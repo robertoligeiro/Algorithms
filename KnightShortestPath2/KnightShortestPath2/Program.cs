1fd5492955a63676cc35c09b2209d09b7923721d
robertoligeiro <robertoligeiro@gmail.com>
2017-01-15 19:14:20 -0800

    Adding Rercursive Algos (string permut and combination) and BFS KnightPath.

---
+++ KnightShortestPath2/KnightShortestPath2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightShortestPath2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resp = ChessKnightWalk.FindShortestPath(new ChessKnightWalk.Pos(0, 0, 0), new ChessKnightWalk.Pos(4, 2, 0));
+        }
+
+        public class ChessKnightWalk
+        {
+            public class Pos
+            {
+                public Tuple<int, int> coords;
+                public int steps { get; set; }
+
+                public Pos(int x, int y, int s)
+                {
+                    coords = new Tuple<int, int>(x, y);
+                    steps = s;
+                }
+            }
+
+            public readonly static int[,] fixedPos = { { -2, -1 }, { -2, 1 }, { -1, 2 }, { 1, 2 }, { 2, 1 }, { 2, -1 }, { 1, -2 }, { -1, -2 }, { -2, -1 } };
+
+            private static void GetNextPos(Tuple<int, int> curr, Dictionary<Tuple<int, int>, Pos> visited, Queue<Tuple<int, int>> path, int step)
+            {
+                for (int i = 0; i <= 8; ++i)
+                {
+                    int x = ChessKnightWalk.fixedPos[i, 0];
+                    int y = ChessKnightWalk.fixedPos[i, 1];
+                    var t = new Tuple<int, int>(curr.Item1 + x, curr.Item2 + y);
+                    if (t.Item1 >= 0 && t.Item1 <= 8 && t.Item2 >= 0 && t.Item2 <= 8 && !visited.ContainsKey(t))
+                    {
+                        visited.Add(t, new Pos(t.Item1, t.Item2, step));
+                        path.Enqueue(t);
+                    }
+                }
+            }
+            public static int FindShortestPath(Pos start, Pos end)
+            {
+                var visited = new Dictionary<Tuple<int, int>, Pos>();
+                var path = new Queue<Tuple<int, int>>();
+                path.Enqueue(start.coords);
+                visited.Add(start.coords, start);
+                while (path.Count > 0)
+                {
+                    var curr = path.Dequeue();
+                    if (curr.Equals(end.coords))
+                    {
+                        return visited[curr].steps;
+                    }
+
+                    var step = visited[curr].steps;
+                    GetNextPos(curr, visited, path, ++step);
+                }
+
+                return -1;
+            }
+        }
+    }
+}
---
+++ RecursiveAlgorithms/RecursiveAlgorithmsStringsPermutCombination/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var combination = StringCombination.DoStringCombination("aabc");
+            var permutation = StringPermutation.DoStringPermutation("aabc");
+        }
+
+        // aabc -> 	AABC, AACB, ABAC, ABCA, ACAB, ACBA, BAAC, BACA, BCAA, CAAB, CABA, CBAA
+		// - Add to response every time loop reaches the end.
+		// - Do recursion starting from 0 each level.
+        class StringPermutation
+        {
+            private static void DoStringPermutationRec(string input, Dictionary<char, int> hist, List<string> resp, char[] curr, int posWrite)
+            {
+                if (posWrite == curr.Count())
+                {
+                    resp.Add(new string(curr));
+                    return;
+                }
+                var localHist = new Dictionary<char,int>(hist);
+
+                for (int i = 0; i < localHist.Count; ++i)
+                {
+                    if (localHist.ElementAt(i).Value > 0)
+                    {
+                        curr[posWrite++] = localHist.ElementAt(i).Key;
+                        localHist[localHist.ElementAt(i).Key]--;
+                        DoStringPermutationRec(input, localHist, resp, curr, posWrite);
+                        curr[--posWrite] = ' ';
+                        localHist[localHist.ElementAt(i).Key]++;
+                    }
+                }
+            }
+
+            public static List<string> DoStringPermutation(string input)
+            {
+                var charHist = new Dictionary<char, int>();
+                var currString = new char[input.Count()];
+                var resp = new List<string>();
+                foreach (var c in input)
+                {
+                    int t;
+                    if (charHist.TryGetValue(c, out t))
+                    {
+                        charHist[c] = ++t;
+                    }
+                    else
+                    {
+                        charHist.Add(c, 1);
+                    }
+                }
+
+                DoStringPermutationRec(input, charHist, resp, currString, 0);
+                return resp;
+            }
+        }
+
+        // AABC -> A, AA, AAB, AABC, AAC, AB, ABC, AC, B, BC, C
+		// - Add each iteration to response.
+        // - Do recursion starting loop from where recursion previously left.
+        class StringCombination
+        {
+            private static void DoStringCombinationRec(string input, Dictionary<char, int> hist, List<string> resp, char[] curr, int posWrite, int posRead)
+            {
+                var localHist = new Dictionary<char, int>(hist);
+
+                for (int i = posRead; i < localHist.Count; ++i)
+                {
+                    if (localHist.ElementAt(i).Value > 0)
+                    {
+                        curr[posWrite++] = localHist.ElementAt(i).Key;
+                        string s = new string(curr);
+                        resp.Add(s);
+                        localHist[localHist.ElementAt(i).Key]--;
+                        DoStringCombinationRec(input, localHist, resp, curr, posWrite, i);
+                        curr[--posWrite] = ' ';
+                        //localHist[localHist.ElementAt(i).Key]++;
+                    }
+                }
+            }
+
+            public static List<string> DoStringCombination(string input)
+            {
+                var charHist = new Dictionary<char, int>();
+                var currString = new char[input.Count()];
+                var resp = new List<string>();
+                foreach (var c in input)
+                {
+                    int t;
+                    if (charHist.TryGetValue(c, out t))
+                    {
+                        charHist[c] = ++t;
+                    }
+                    else
+                    {
+                        charHist.Add(c, 1);
+                    }
+                }
+
+                resp.Add(string.Empty);
+                DoStringCombinationRec(input, charHist, resp, currString, 0, 0);
+                return resp;
+            }
+        }
+    }
+}
