1f204d196951ca9040cd0a5438af0b3f0c7359b6
robertoligeiro <robertoligeiro@gmail.com>
2017-01-18 23:16:04 -0800

    score combinations.

---
+++ DynamicProgramingScoreCombinations/DynamicProgramingScoreCombinations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgramingScoreCombinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = GetNumberScoreCombinations(12, new List<int>() { 2, 3, 7});
+        }
+
+        public static int GetNumberScoreCombinations(int score, List<int> scoreValues)
+        {
+            var resp = new int[score + 1];
+            resp[0] = 1;
+            foreach (var val in scoreValues)
+            {
+                for (int i = val; i <= score; ++i)
+                {
+                    resp[i] += resp[i - val];
+                }
+            }
+
+            return resp[score];
+        }
+    }
+}
