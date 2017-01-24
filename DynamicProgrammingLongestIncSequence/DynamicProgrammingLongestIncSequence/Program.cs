e50b3b97fe668dd8bfc3057eb6b4631564b3ebb0
robertoligeiro <robertoligeiro@gmail.com>
2017-01-23 23:31:56 -0800

    Dynamic Programming, longest increasing sub.

---
+++ DynamicProgrammingLongestIncSequence/DynamicProgrammingLongestIncSequence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgrammingLongestIncSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new int[] {0, -1, 4, 5, 6, 2, 3 };
+            var r = GetLongestIncSequence(a);
+        }
+
+        public static int GetLongestIncSequence(int []a)
+        {
+            int []maxAtIndex = Enumerable.Repeat(1, a.Length).ToArray();
+            for (int i = 1; i < a.Length; ++i)
+            {
+                for (int j = 0; j < i; ++j)
+                {
+                    if (a[i] > a[j])
+                    {
+                        maxAtIndex[i] = Math.Max(maxAtIndex[i], maxAtIndex[j] + 1);
+                    }
+                }
+            }
+
+            return maxAtIndex.Max();
+        }
+    }
+}
