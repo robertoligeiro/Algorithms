199ebec5cf21dd9a40d1de1257f77c60ecd6d67d
robertoligeiro <robertoligeiro@gmail.com>
2017-01-26 22:08:36 -0800

    adding Greedy 3sum.

---
+++ Greedy3sum/Greedy3sum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greedy3sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l = new List<int>() { 11, 2, 5, 7, 3 };
+            bool b = Has3Sum(l, 22);
+        }
+
+        public static bool Has3Sum(List<int> a, int t)
+        {
+            a.Sort();
+            for (int i = 0; i < a.Count; ++i)
+            {
+                if (HasSum(a, i, t-a[i])) return true;
+            }
+            return false;
+        }
+
+        public static bool HasSum(List<int> a, int start, int t)
+        {
+            int left = start;
+            int right = a.Count - 1;
+
+            while (left < right)
+            {
+                if (a[left] + a[right] == t) return true;
+                if (a[left] + a[right] < t)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return false;
+        }
+    }
+}
