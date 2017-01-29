3eb2fb50ef89a9195b05a8440ef46d22dfd89544
robertoligeiro <robertoligeiro@gmail.com>
2017-01-29 15:31:44 -0800

    Greedy Max Rectangle in the skyline (max rectangle in histogram)

---
+++ GreedyLargestRectInSkyline/GreedyLargestRectInSkyline/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreedyLargestRectInSkyline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l = new List<int>() { 2, 1, 2, 3, 1 };
+            var r = GetMaxArea(l);
+        }
+
+        public static int GetMaxArea(List<int> a)
+        {
+            var sValues = new Stack<int>();
+            int i = 0;
+            int top = 0;
+            int maxArea = 0;
+            for (; i < a.Count;)
+            {
+                if (sValues.Count == 0 || sValues.Peek() <= a[i])
+                {
+                    sValues.Push(a[i++]);
+                }
+                else
+                {
+                    while (sValues.Count > 0 && sValues.Peek() >= a[i])
+                    {
+                        top = sValues.Pop();
+                        int area;
+                        if (sValues.Count == 0)
+                        {
+                            area = a[top] * i;
+                            maxArea = maxArea > area ? maxArea : area;
+                        }
+                        else
+                        {
+                            area = a[top] * (i - sValues.Peek() - 1);
+                            maxArea = maxArea > area ? maxArea : area;
+                        }
+                    }
+                }
+            }
+
+            while (sValues.Count > 0)
+            {
+                top = sValues.Pop();
+                int area;
+                if (sValues.Count == 0)
+                {
+                    area = a[top] * i;
+                    maxArea = maxArea > area ? maxArea : area;
+                }
+                else
+                {
+                    area = a[top] * (i - sValues.Peek() - 1);
+                    maxArea = maxArea > area ? maxArea : area;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
