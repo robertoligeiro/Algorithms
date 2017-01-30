5d09eb4cdf2107f967468f7c58cdb69aedad2345
robertoligeiro <robertoligeiro@gmail.com>
2017-01-29 21:25:27 -0800

    Recursion Pow(X,Y)

---
+++ RecursionPowXY/RecursionPowXY/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionPowXY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = Pow(2, 2);
+
+            r = Pow(10, 2);
+            r = Pow(2, 5);
+            r = Pow(2, 10);
+        }
+
+        public static int Pow(int x, uint y)
+        {
+            if (y == 0) return 1;
+            var temp = Pow(x, y / 2);
+
+            if (y % 2 == 0) return temp * temp;
+            else return x * temp * temp;
+        }
+    }
+}
