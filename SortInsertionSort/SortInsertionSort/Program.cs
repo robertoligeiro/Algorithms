66ba7fb5dafe15ee40009e6b02cb159f766ecd28
robertoligeiro <robertoligeiro@gmail.com>
2017-02-01 22:21:58 -0800

    InsertionSort

---
+++ SortInsertionSort/SortInsertionSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortInsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l = new List<int>();
+            var rand = new Random();
+            for (int i = 0; i < 10; ++i)
+            {
+                l.Add(rand.Next(0, 100));
+            }
+
+            InsertionSort(l);
+        }
+
+        public static void InsertionSort(List<int> a)
+        {
+            for (int i = 0; i < a.Count; ++i)
+            {
+                int inv = i;
+                for (int j = inv-1; j >= 0; --j)
+                {
+                    if (a[inv] < a[j])
+                    {
+                        var t = a[inv];
+                        a[inv] = a[j];
+                        a[j] = t;
+                        inv = j;
+                    }
+                    else break;
+                }
+            }
+        }
+    }
+}
