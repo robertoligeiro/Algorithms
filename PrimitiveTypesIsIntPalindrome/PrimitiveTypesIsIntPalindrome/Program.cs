65fe43325fa6d5057344376ed8fc512b9c605924
robertoligeiro <robertoligeiro@gmail.com>
2017-02-11 20:44:34 -0800

    primitive types Reverse digits and IsPalindromeInt.

---
+++ PrimitiveTypesIsIntPalindrome/PrimitiveTypesIsIntPalindrome/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveTypesIsIntPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool b = IsPalindrome(-1);
+            b = IsPalindrome(0);
+            b = IsPalindrome(11);
+            b = IsPalindrome(121);
+            b = IsPalindrome(2112);
+            b = IsPalindrome(123);
+            b = IsPalindrome(21112);
+            b = IsPalindrome(21122);
+        }
+
+        static bool IsPalindrome(int a)
+        {
+            if (a < 0) return false;
+            if (a == 0) return true;
+            var l = new List<char>();
+            while (a > 0)
+            {
+                var dig = a % 10;
+                l.Add(Convert.ToChar(dig));
+                a /= 10;
+            }
+            int i = 0;
+            int j = l.Count-1;
+            while (i <= j)
+            {
+                if (l[i++] != l[j--]) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ PrimitiveTypesReverseDigitis/PrimitiveTypesReverseDigitis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveTypesReverseDigitis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = Reverse(314);
+            r = Reverse(-192);
+        }
+
+        public static int Reverse(int a)
+        {
+            var neg = a < 0 ? true : false;
+            int r = 0;
+            a = Math.Abs(a);
+            while (a > 0)
+            {
+                r = r * 10 + a % 10;
+                a /= 10;
+            }
+
+            return neg?-r:r;
+        }
+    }
+}
