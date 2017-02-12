387a161ce7683196d3f6d83bfc5694213fdc9220
robertoligeiro <robertoligeiro@gmail.com>
2017-02-11 21:11:32 -0800

    PrimitiveTypesGenerateRandom

---
+++ PrimitiveTypesGenerateRandomNumbers/PrimitiveTypesGenerateRandomNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveTypesGenerateRandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l = new List<int>();
+            l = Enumerable.Range(1, 10).ToList();
+            GenerateRandom(l);
+        }
+
+        public static void GenerateRandom(List<int> a)
+        {
+            int len = a.Count;
+            var rand = new Random();
+            for (int i = 0; i < len; ++i)
+            {
+                int r = rand.Next(0, i + 1);
+                int t = a[i];
+                a[i] = a[r];
+                a[r] = t;
+            }
+        }
+    }
+}
