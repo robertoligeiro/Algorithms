76e6a4350ba7a34fac8787c9a083ca53fc009edf
robertoligeiro <robertoligeiro@gmail.com>
2017-02-14 20:34:51 -0800

    arrays, plus one.

---
+++ ArraysPartitionArray/ArraysPartitionArray/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysPartitionArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var a = new List<int>();
+            for (int i = 0; i < 20; ++i)
+            {
+                a.Add(r.Next(0,10));
+            }
+
+            PartitionArray(a, 5);
+        }
+
+        public static void PartitionArray(List<int> a, int p)
+        {
+            int pivot = a[p];
+            int i = 1;
+            int j = a.Count - 1;
+            Swap(0, p, a);
+            while (true)
+            {
+                while (i <= a.Count - 1 && a[i] <= pivot)
+                {
+                    i++;
+                }
+
+                while (j >= 0 && a[j] > pivot)
+                {
+                    j--;
+                }
+
+                if (j < i) break;
+                Swap(i, j, a);
+            }
+
+            Swap(0, j, a);
+        }
+
+        public static void Swap(int i, int j, List<int> a)
+        {
+            var t = a[i];
+            a[i] = a[j];
+            a[j] = t;
+        }
+    }
+}
---
+++ ArraysPlusOne/ArraysPlusOne/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysPlusOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = PlusOne(new List<int>() { 9, 9, 9 });
+            var b = PlusOne(new List<int>() { 0,9});
+        }
+
+        public static List<int> PlusOne(List<int> a)
+        {
+            int sum = 1;
+            for (int i = a.Count - 1; i >= 0; --i)
+            {
+                sum += a[i];
+                if (sum % 10 == 0)
+                {
+                    a[i] = 0;
+                    sum = 1;
+                }
+                else
+                {
+                    a[i] = sum;
+                    sum = 0;
+                    break;
+                }
+            }
+
+            if (sum == 1)
+            {
+                var r = new List<int>();
+                r.Add(1);
+                r.AddRange(a);
+                return r;
+            }
+            return a;
+        }
+    }
+}
