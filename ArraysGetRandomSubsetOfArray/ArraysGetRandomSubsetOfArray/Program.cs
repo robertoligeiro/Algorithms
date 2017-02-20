072b0827ec4f3675de1975bbf4d9b06ab79ad72d
robertoligeiro <robertoligeiro@gmail.com>
2017-02-20 00:57:36 -0800

    Arrays: Get random Subset from Array.

---
+++ ArraysGetRandomSubsetOfArray/ArraysGetRandomSubsetOfArray/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysGetRandomSubsetOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = GetSubset(10, 10);
+        }
+
+        public static List<int> GetSubset(int n, int k)
+        {
+            var dict = new Dictionary<int, int>();
+            var rand = new Random();
+            for (int i = 0; i < k; ++i)
+            {
+                int index = rand.Next(i, n - 1);
+                int v;
+                if (dict.TryGetValue(index, out v))
+                {
+                    dict[index] = i;
+                    dict[i] = v;
+                }else
+                if (dict.TryGetValue(i, out v))
+                {
+                    dict[i] = index;
+                    dict.Add(index, v);
+                }
+                else
+                {
+                    dict.Add(index, i);
+                    if(index != i)
+                        dict.Add(i, index);
+                }
+            }
+
+            var r = new List<int>();
+            for (int i = 0; i < k; ++i)
+            {
+                r.Add(dict[i]);
+            }
+            return r;
+        }
+    }
+}
