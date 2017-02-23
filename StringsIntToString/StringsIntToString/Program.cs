db4e0051304ed1c2924f8a8f6513cc7cee05d40a
robertoligeiro <robertoligeiro@gmail.com>
2017-02-22 23:21:44 -0800

    Strings: IntToString, StringToInt.

---
+++ StringsIntToString/StringsIntToString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsIntToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = IntToString(-345);
+        }
+
+        public static string IntToString(int x)
+        {
+            bool isNeg = x < 0;
+            StringBuilder s = new StringBuilder();
+            x = isNeg ? -x : x;
+            while (x > 0)
+            {
+                var p = x % 10;
+                x /= 10;
+                s.Append(p);
+            }
+
+            if (isNeg) s.Append("-");
+            return new string(s.ToString().Reverse().ToArray());
+        }
+    }
+}
---
+++ StringsStringToInt/StringsStringToInt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsStringToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = StringToInt("-456");
+        }
+
+        public static int StringToInt(string s)
+        {
+            bool isNeg = s[0] == '-';
+            int i = isNeg ? 1 : 0;
+            int acc = 0;
+            for (; i < s.Length; ++i)
+            {
+                acc = acc * 10 + (s[i] - '0');
+            }
+
+            return isNeg ? -acc : acc;
+        }
+    }
+}
