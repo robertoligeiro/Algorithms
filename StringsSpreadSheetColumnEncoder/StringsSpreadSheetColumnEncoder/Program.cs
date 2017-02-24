965a8408baae066381054c116e8ae5e869012354
robertoligeiro <robertoligeiro@gmail.com>
2017-02-23 23:48:47 -0800

    strings: spreadsheet column enconder.

---
+++ StringsSpreadSheetColumnEncoder/StringsSpreadSheetColumnEncoder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsSpreadSheetColumnEncoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = GetColumnEncode("AAA");
+        }
+
+        public static int GetColumnEncode(string s)
+        {
+            var r = 0;
+            for (int i = 0; i < s.Length; ++i)
+            {
+                r = r * 26 + s[i] - 'A' + 1;
+            }
+
+            return r;
+        }
+    }
+}
