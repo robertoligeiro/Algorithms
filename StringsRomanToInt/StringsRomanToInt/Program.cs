dbb064693c900dec7bd0860119e9a31abbf2dbe2
robertoligeiro <robertoligeiro@gmail.com>
2017-02-26 17:40:22 -0800

    strings: Roman to Int.

---
+++ StringsRomanToInt/StringsRomanToInt/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsRomanToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = RomanToInt("lix");
+        }
+
+        public static int RomanToInt(string r)
+        {
+            var rValues = new Dictionary<char, int>() { { 'i', 1 }, { 'v', 5 }, { 'x', 10 },
+                                                        { 'l', 50 }, { 'c', 100 }, { 'd', 500 }, { 'm', 1000 } };
+
+            var acc = rValues[r.LastOrDefault()];
+            int i = r.Length - 2;
+            while (i>=0)
+            {
+                if (rValues[r[i]] >= rValues[r[i+1]])
+                {
+                    acc += rValues[r[i]];
+                }
+                else
+                {
+                    acc -= rValues[r[i]];
+                }
+                i--;
+            }
+            return acc;
+        }
+    }
+}
