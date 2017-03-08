2bfbb29e9705a59de1af0ff34439eb38553048ce
robertoligeiro <robertoligeiro@gmail.com>
2017-03-07 23:54:10 -0800

    Stacks: check if expression {}[]() is well formed.

---
+++ StacksWellFormedExpression/StacksWellFormedExpression/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksWellFormedExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = IsWellFormed("(");
+            b = IsWellFormed("()");
+            b = IsWellFormed("{[()]}");
+            b = IsWellFormed("()[()]{}");
+            b = IsWellFormed("(}");
+            b = IsWellFormed("[()[]{()");
+        }
+
+        public static bool IsWellFormed(string exp)
+        {
+            var s = new Stack<char>();
+            foreach (var c in exp)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    s.Push(c);
+                }
+                else
+                {
+                    if (s.Count == 0) return false;
+                    char top = s.Pop();
+                    if ((c == ')' && top != '(') || (c == ']' && top != '[') || (c == '}' && top != '{')) return false;
+                }
+            }
+            return s.Count == 0;
+        }
+    }
+}
