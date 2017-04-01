be4de1affe8a8cbcd4e439d5f3c08bd4195b90e8
robertoligeiro <robertoligeiro@gmail.com>
2017-04-01 14:55:02 -0700

    add variables.

--- CSharpPearls/CSharpPearls/Program.cs
+++ CSharpPearls/CSharpPearls/Program.cs
@@ -14,9 +14,29 @@ namespace CSharpPearls
             var i = 200;
             SuperPower(t, i);
             Console.WriteLine("power: {0}, i: {1}", t.power, i);
+
+            Node n0 = new Node { val = 0 };
+            Node n1 = new Node { val = 1 };
+            n0.next = n1;
+            WalkOnList(n0);
+            Console.WriteLine("n0.val: {0}, n1.val: {1}", n0.val, n1.val);
+
             Console.ReadKey();
         }
 
+        public class Node
+        {
+            public int val;
+            public Node next;
+        }
+
+        public static void WalkOnList(Node r)
+        {
+            while (r != null)
+            {
+                r = r.next;
+            }
+        }
         public class MyType
         {
             public int power;
@@ -26,6 +46,8 @@ namespace CSharpPearls
         {
             t.power += 1000;
             i += 1000;
+            var tt = new MyType { power = 20000 };
+            t = tt;
         }
     }
 }
