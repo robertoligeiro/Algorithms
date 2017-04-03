76cc12bd50eb94ddfd7f027c72b6e9e166cd1dd3
robertoligeiro <robertoligeiro@gmail.com>
2017-04-02 21:44:19 -0700

    sorting: find the smallest in a cycled sorted array.

---
+++ SortingFindSmallestInCycledSortedArray/SortingFindSmallestInCycledSortedArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingFindSmallestInCycledSortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int FindSmallest(List<int> a)
+        {
+            var left = 0;
+            var right = a.Count - 1;
+            while (left < right)
+            {
+                var mid = left + (right - left) / 2;
+                if (a[mid] > a[right])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+
+            return left;
+        }
+    }
+}
