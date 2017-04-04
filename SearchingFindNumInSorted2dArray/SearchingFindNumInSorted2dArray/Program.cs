5f03071752a57d9a4211a3c8331048dc522b58f8
robertoligeiro <robertoligeiro@gmail.com>
2017-04-03 22:22:55 -0700

    searching: find element in a sorted 2d array.

---
+++ SearchingFindNumInSorted2dArray/SearchingFindNumInSorted2dArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchingFindNumInSorted2dArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new int[6][]
+            {
+                new int[]{ -1, 2, 4, 4, 6},
+                new int[]{ 1, 5, 5, 9, 21},
+                new int[]{ 3, 6, 6, 9, 22},
+                new int[]{ 3, 6, 8, 10, 24},
+                new int[]{ 6, 8, 9, 12, 25},
+                new int[]{ 8, 10, 12, 13, 40}
+            };
+
+            var b = FindNum(a, 7); // false
+            b = FindNum(a, 8); // true
+            b = FindNum(a, 40); // true
+        }
+
+        public static bool FindNum(int[][] a, int n)
+        {
+            var col = a[0].Length - 1;
+            var row = 0;
+            while (row < a.GetLength(0) && col >= 0)
+            {
+                if (a[row][col] == n) return true;
+                if (a[row][col] < n)
+                {
+                    row++;
+                }
+                else
+                {
+                    col--;
+                }
+            }
+            return false;
+        }
+    }
+}
