d7839bfd13ec507a30f4e8ad18caa87b1b14a40a
robertoligeiro <robertoligeiro@gmail.com>
2017-04-05 09:55:31 -0700

    Searching: find duplicated element in array 0 - n.

---
+++ SearchingFindDuplicateElementInArray/SearchingFindDuplicateElementInArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchingFindDuplicateElementInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = FindDuplicate(new List<int>() { 0, 3, 1, 2, 2 });
+        }
+
+        public static int FindDuplicate(List<int> a)
+        {
+            var xorArray = 0;
+            for (int i = 0; i < a.Count; ++i)
+            {
+                xorArray ^= a[i];
+            }
+
+            return xorArray;
+        }
+    }
+}
