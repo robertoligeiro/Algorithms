ea1c041319264bbad15be6d7ecb97106ddceb494
robertoligeiro <robertoligeiro@gmail.com>
2017-04-05 09:47:45 -0700

    Searching: finding missing element in an array 1 - N. (using xor techinique)

---
+++ SearchingFindMissingElementInArray/SearchingFindMissingElementInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchingFindMissingElementInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = FindMissing(new List<int>() { 5,0,3,1,2});
+        }
+
+        public static int FindMissing(List<int> a)
+        {
+            var xorArray = 0;
+            var xorArrayFull = 0;
+            for (int i = 0; i <= a.Count; ++i)
+            {
+                xorArrayFull ^= i;
+                if(i < a.Count)
+                    xorArray ^= a[i];
+            }
+
+            return xorArrayFull ^ xorArray;
+        }
+    }
+}
