4d2af584a25be240efd294e6923959c623497a64
robertoligeiro <robertoligeiro@gmail.com>
2017-04-06 08:53:56 -0700

    Hash: check if word can be muted to a palindrome.

---
+++ HashesTestForPalindromicPermutations/HashesTestForPalindromicPermutations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashesTestForPalindromicPermutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = HasPalindrome("level"); //true
+            b = HasPalindrome("edified"); //true
+            b = HasPalindrome("foaraboofob"); //true
+            b = HasPalindrome("roatort"); //true
+            b = HasPalindrome("efdified"); //true
+            b = HasPalindrome("edifierd"); //false
+        }
+
+        public static bool HasPalindrome(string s)
+        {
+            var charCount = new Dictionary<char, int>();
+            foreach (var c in s)
+            {
+                var count = 0;
+                if (charCount.TryGetValue(c, out count))
+                {
+                    count++;
+                    charCount[c] = count;
+                }
+                else
+                {
+                    charCount.Add(c, 1);
+                }
+            }
+            var countOdds = 0;
+            foreach (var i in charCount.Values)
+            {
+                if (i % 2 != 0 && countOdds++ == 1) return false;
+            }
+            return true;
+        }
+    }
+}
