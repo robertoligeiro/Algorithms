df3e70dcd914ada2f0d59c602651194074b708f4
robertoligeiro <robertoligeiro@gmail.com>
2017-04-06 08:44:40 -0700

    Hash: partition list of words in anagrams groups.

---
+++ HashsPartitioningIntoAnagrams/HashsPartitioningIntoAnagrams/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashsPartitioningIntoAnagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var l = new List<string>() { "debitcard", "elvis", "silent", "badcredit", "lives", "freedom", "listen", "levis", "money" };
+            var r = PartitionIntoAnagrams(l);
+        }
+
+        public static List<List<string>> PartitionIntoAnagrams(List<string> a)
+        {
+            var anagrams = new Dictionary<string, List<string>>();
+            foreach (var s in a)
+            {
+                var sortedS = new string(s.OrderBy(c => c).ToArray());
+                List<string> l;
+                if (anagrams.TryGetValue(sortedS, out l))
+                {
+                    l.Add(s);
+                }
+                else
+                {
+                    anagrams.Add(sortedS, new List<string>() { s });
+                }
+            }
+            var resp = new List<List<string>>();
+            foreach (var v in anagrams.Values)
+            {
+                if (v.Count > 1)
+                {
+                    resp.Add(v);
+                }
+            }
+            return resp;
+        }
+    }
+}
