32ce4ebf39717bd42a7ee83772a5cef9c6a02ba5
robertoligeiro <robertoligeiro@gmail.com>
2017-04-16 21:26:47 -0700

    Sorting: Merge two sorted arrays where one array has room for both. (EPI 14.02)

---
+++ SortingMergeToSortedArraysInPlace/SortingMergeToSortedArraysInPlace/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingMergeToSortedArraysInPlace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[7] { 5, 13, 17 , 0, 0, 0, 0};
+            int[] b = new int[4] { 3, 7, 11, 19 };
+            Merge(a, 3, b, 4);
+        }
+
+        public static void Merge(int[] a, int sizeA, int[] b, int sizeB)
+        {
+            var nextWrite = sizeA + sizeB - 1;
+            sizeA--;sizeB--;
+            while (sizeA >= 0 && sizeB >= 0)
+            {
+                if (a[sizeA] > b[sizeB]) a[nextWrite] = a[sizeA--];
+                else a[nextWrite] = b[sizeB--];
+                nextWrite--;
+            }
+            while (sizeB >= 0)
+                a[nextWrite--] = b[sizeB--];
+        }
+    }
+}
