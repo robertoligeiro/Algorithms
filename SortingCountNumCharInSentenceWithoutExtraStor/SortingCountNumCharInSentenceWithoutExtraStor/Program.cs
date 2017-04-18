b8f3f22fc1438fbcfade47c8eeea995a6eb38b3b
robertoligeiro <robertoligeiro@gmail.com>
2017-04-17 20:45:07 -0700

    Count frequencies of char in a sentence without using extra storage. (EPI 14.3)

---
+++ SortingCountNumCharInSentenceWithoutExtraStor/SortingCountNumCharInSentenceWithoutExtraStor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingCountNumCharInSentenceWithoutExtraStor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountNumberCharWithoutExtraStorage("gfghghghtabca");
+        }
+
+        public static void CountNumberCharWithoutExtraStorage(string s)
+        {
+            var sortedS = new string(s.OrderBy(c => c).ToArray());
+            var prev = sortedS[0];
+            var count = 1;
+            for(int i = 1; i < sortedS.Length; ++i)
+            {
+                if (sortedS[i] == prev) count++;
+                else
+                {
+                    Console.WriteLine("char:{0}, count:{1}", prev, count);
+                    prev = sortedS[i];
+                    count = 1;
+                }
+            }
+            Console.WriteLine("char:{0}, count:{1}", prev, count);
+        }
+    }
+}
