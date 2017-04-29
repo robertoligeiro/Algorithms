1102073cbb6ec7930ee6b14b07cae4035f326729
robertoligeiro <robertoligeiro@gmail.com>
2017-04-29 15:42:43 -0700

    Recursion: Generate well formed pairs of parenthesis.

---
+++ RecursionGenerateAllWellFormedParens/RecursionGenerateAllWellFormedParens/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionGenerateAllWellFormedParens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rBook = BookSolution(5);
+        }
+
+
+        public static List<string> BookSolution(int n_pairs)
+        {
+            var result = new List<string>();
+            BookSolutionRecursion(n_pairs, n_pairs, "", result);
+            return result;
+        }
+
+        public static void BookSolutionRecursion(int l_needed, int r_needed, string prefix, List<string> r)
+        {
+            if (l_needed == 0 && r_needed == 0)
+            {
+                r.Add(prefix);
+                return;
+            }
+
+            if (l_needed > 0)
+            {
+                BookSolutionRecursion(l_needed - 1, r_needed, prefix + "(", r);
+            }
+            if (l_needed < r_needed)
+            {
+                BookSolutionRecursion(l_needed, r_needed - 1, prefix + ")", r);
+            }
+        }
+    }
+}
