ef75e1f4996215a201e571a7a051fe1407da2411
robertoligeiro <robertoligeiro@gmail.com>
2017-04-29 15:59:41 -0700

    Recursion: get string palindrome decompositions (EPI 16.7)

---
+++ RecursionStringPalindromeDecompositions/RecursionStringPalindromeDecompositions/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionStringPalindromeDecompositions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = GetPalDeco("0204451881");
+        }
+
+        public static List<List<string>> GetPalDeco(string s)
+        {
+            var resp = new List<List<string>>();
+            var parc = new List<string>();
+            GetPalDeco(s, resp, parc);
+            return resp;
+        }
+
+        public static void GetPalDeco(string s, List<List<string>> r, List<string> p)
+        {
+            if (s.Length == 0)
+            {
+                r.Add(new List<string>(p));
+                return;
+            }
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                var t = s.Substring(0, i + 1);
+                if (IsPalindrome(t))
+                {
+                    var localP = new List<string>(p);
+                    localP.Add(t);
+                    GetPalDeco(s.Substring(i + 1, s.Length - i - 1), r, localP);
+                }
+            }
+        }
+
+        public static bool IsPalindrome(string s)
+        {
+            for (int i = 0, j = s.Length - 1; i < j; ++i, --j)
+            {
+                if (s[i] != s[j]) return false;
+            }
+            return true;
+        }
+    }
+}
