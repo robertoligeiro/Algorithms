9db7a8989ce0e0c7c8685faf455de2192e7c3ec3
robertoligeiro <robertoligeiro@gmail.com>
2017-05-06 12:21:26 -0700

    LeetCode: Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.

---
+++ LeetCodePickOneSumLeafsInTree/LeetCodePickOneSumLeafsInTree/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePickOneSumLeafsInTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class TreeNode
+        {
+             public int val;
+             public TreeNode left;
+             public TreeNode right;
+             public TreeNode(int x) { val = x; }
+        }
+
+        public class Solution
+        {
+            public int SumNumbers(TreeNode root)
+            {
+                var leafs = new List<int>();
+                GetSumToLeafs(root, 0, 0, leafs);
+                var tot = 0;
+                foreach (var i in leafs) tot += i;
+                return tot;
+            }
+
+            public void GetSumToLeafs(TreeNode r, int acc, int mult, List<int> l)
+            {
+                if (r == null) return;
+
+                var lAcc = acc * mult + r.val;
+                if (r.left == null && r.right == null)
+                {
+                    l.Add(lAcc);
+                    return;
+                }
+                mult = 10;
+                if (r.left != null) GetSumToLeafs(r.left, lAcc, mult, l);
+                if (r.right != null) GetSumToLeafs(r.right, lAcc, mult, l);
+            }
+        }
+    }
+}
