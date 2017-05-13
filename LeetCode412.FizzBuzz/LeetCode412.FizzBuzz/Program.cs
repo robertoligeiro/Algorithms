ae9c6fb8f408754beb873b12eed20bcac8f39578
robertoligeiro <robertoligeiro@gmail.com>
2017-05-13 12:30:13 -0700

    LeetCode: 412 Fizz Buzz

---
+++ LeetCode412.FizzBuzz/LeetCode412.FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode412.FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var r = s.FizzBuzz(16);
+        }
+        public class Solution
+        {
+            public IList<string> FizzBuzz(int n)
+            {
+                var r = new List<string>();
+                for (int i = 1; i <= n; ++i)
+                {
+                    if (i % 3 == 0 && i % 5 == 0) r.Add("FizzBuzz");
+                    else if (i % 3 == 0) r.Add("Fizz");
+                    else if (i % 5 == 0) r.Add("Buzz");
+                    else r.Add(i.ToString());
+                }
+                return r;
+            }
+        }
+    }
+}
