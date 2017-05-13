70c1a1bc09bd212835a5cca766cb74a8bf7c1191
robertoligeiro <robertoligeiro@gmail.com>
2017-05-12 23:08:19 -0700

    LeetCode: 338. Counting Bits

---
+++ LeetCode338.CountingBits/LeetCode338.CountingBits/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode338.CountingBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var r = s.CountBits(5);
+        }
+
+        public class Solution
+        {
+            public int[] CountBits(int num)
+            {
+                var r = new List<int>();
+                for (int i = 0; i <= num; ++i)
+                {
+                    r.Add(GetBits(i));
+                }
+                return r.ToArray();
+            }
+
+            public int GetBits(int n)
+            {
+                var count = 0;
+                while (n > 0)
+                {
+                    if ((n & 1) == 1) count++;
+                    n = n >> 1;
+                }
+                return count;
+            }
+        }
+    }
+}
