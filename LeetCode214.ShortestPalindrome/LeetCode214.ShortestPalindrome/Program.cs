4e0c215d47845800c3a02a6788c137f7a687b35c
robertoligeiro <robertoligeiro@gmail.com>
2017-05-17 22:27:29 -0700

    LeetCode 214. Shortest Palindrome

---
+++ LeetCode214.ShortestPalindrome/LeetCode214.ShortestPalindrome/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode214.ShortestPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            //var r = s.ShortestPalindrome("aaaba");
+            var r = s.ShortestPalindrome("aacecaaa"); //aaaaacecaaa, aaacecaaa
+        }
+
+        public class Solution
+        {
+            public string ShortestPalindrome(string s)
+            {
+                var sReversed = new string(s.ToCharArray().Reverse().ToArray());
+                var n = s.Length;
+                int l;
+                for (l = n; l >= 0; --l)
+                {
+                    if (s.Substring(0, l) == sReversed.Substring(n - l)) break;
+                }
+
+                return sReversed.Substring(0, n - l) + s;
+            }
+        }
+    }
+}
