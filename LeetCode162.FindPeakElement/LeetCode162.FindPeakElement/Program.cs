598fd2fc891f122e0d1c3268e889c76a144e6e45
robertoligeiro <robertoligeiro@gmail.com>
2017-05-23 20:44:10 -0700

    LeetCode162.FindPeakElement

---
+++ LeetCode162.FindPeakElement/LeetCode162.FindPeakElement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode162.FindPeakElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public class Solution
+        {
+            public int FindPeakElement(int[] nums)
+            {
+                int low = 0;
+                int high = nums.Length - 1;
+
+                while (low < high)
+                {
+                    int mid1 = (low + high) / 2;
+                    int mid2 = mid1 + 1;
+                    if (nums[mid1] < nums[mid2])
+                        low = mid2;
+                    else
+                        high = mid1;
+                }
+                return low;
+            }
+        }
+    }
+}
