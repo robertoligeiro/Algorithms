087e3558ddd90bd7869e38ad83496c6082941f98
robertoligeiro <robertoligeiro@gmail.com>
2017-06-04 14:23:15 -0700

    cracking the code: make change for value. 8.11, 362.

---
+++ DynamicProgrammingCoins/DynamicProgrammingCoins/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProgrammingCoins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = MakeChange(100);
+        }
+
+        public static int MakeChange(int n)
+        {
+            var coins = new int[] { 25, 10, 5, 1 };
+            return MakeChange(n, coins, 0);
+        }
+
+        public static int MakeChange(int amount, int[] coins, int index)
+        {
+            if (index == coins.Length) return 1;
+            var cointAmount = coins[index];
+            var ways = 0;
+            for (int i = 0; i * cointAmount <= amount; ++i)
+            {
+                var remaining = amount - (i * cointAmount);
+                ways += MakeChange(remaining, coins, index + 1);
+            }
+            return ways;
+        }
+    }
+}
