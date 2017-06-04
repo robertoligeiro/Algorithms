b6a8fa6559722253ba7403dfc669550997cf1e71
robertoligeiro <robertoligeiro@gmail.com>
2017-06-04 14:41:12 -0700

    factorial

---
+++ RecursionCalcFactorial/RecursionCalcFactorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionCalcFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = Fact(4);
+        }
+
+        public static int Fact(int n)
+        {
+            if (n == 1) return 1;
+            return n * Fact(n - 1);
+        }
+    }
+}
