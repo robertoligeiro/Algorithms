8b566ecc8880dff060b0f691a9a42d58360d5213
robertoligeiro <robertoligeiro@gmail.com>
2017-06-06 00:32:05 -0700

    Find turning point of a flipped array.

---
+++ SearchingFindTurnOfFlippedSortedArrat/SearchingFindTurnOfFlippedSortedArrat/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchingFindTurnOfFlippedSortedArrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = FindTurn(new List<int> { 5,6,7,8,9,1,2,3,4}); //5
+            r = FindTurn(new List<int> { 5, 1, 2, 3, 4 }); //1
+            r = FindTurn(new List<int> { 5, 6, 7, 8, 9, 1 }); //5
+        }
+
+        public static int FindTurn(List<int> a)
+        {
+            var l = 0;
+            var r = a.Count - 1;
+            while (l < r)
+            {
+                var mid = l + (r - l) / 2;
+                if (a[mid] > a[r])
+                {
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid;
+                }
+            }
+            return r;
+        }
+    }
+}
