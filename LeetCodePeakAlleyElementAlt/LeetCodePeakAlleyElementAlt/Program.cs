3504ac9caa2e87fdb8bd086e00da01ce56b0fe26
robertoligeiro <robertoligeiro@gmail.com>
2017-06-08 09:06:31 -0700

    Sort and array into alleys and peaks, cracking the code interview 10.11, pg 416.

---
+++ LeetCodePeakAlleyElementAlt/LeetCodePeakAlleyElementAlt/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePeakAlleyElementAlt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new List<int>() { 5, 3, 1, 2, 3 };
+           SortToPeakAndAlley(a);
+        }
+
+        public static void SortToPeakAndAlley(List<int> a)
+        {
+            for (int i = 1; i < a.Count - 1; i += 2)
+            {
+                var biggestIndex = BiggestIndex(a, i - 1, i + 1);
+                if (biggestIndex != i)
+                {
+                    var t = a[i];
+                    a[i] = a[biggestIndex];
+                    a[biggestIndex] = t;
+                }
+            }
+        }
+
+        public static int BiggestIndex(List<int> a, int l, int r)
+        {
+            int biggest = 0;
+            if (a[l] > a[l + 1]) biggest = l;
+            else biggest = l + 1;
+            if (a[biggest] < a[r]) biggest = r;
+            return biggest;
+        }
+    }
+}
