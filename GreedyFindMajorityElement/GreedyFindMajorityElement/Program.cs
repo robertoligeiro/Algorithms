8945020d8f583fcaf31eb396c51515caa34aaf9a
robertoligeiro <robertoligeiro@gmail.com>
2017-06-11 13:34:29 -0700

    EPI 18.6, pg 338

---
+++ GreedyFindMajorityElement/GreedyFindMajorityElement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreedyFindMajorityElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = MajorityElement(new List<char>() { 'b', 'a', 'c', 'a', 'a', 'b', 'a', 'a', 'c', 'a' });
+        }
+
+        public static char MajorityElement(List<char> a)
+        {
+            var count = 1;
+            char element = a[0];
+            for(int i = 1; i < a.Count; ++i)
+            {
+                if (a[i] == a[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    if (--count == 0)
+                    {
+                        element = a[i];
+                        count = 1;
+                    }
+                }
+            }
+
+            return element;
+        }
+    }
+}
