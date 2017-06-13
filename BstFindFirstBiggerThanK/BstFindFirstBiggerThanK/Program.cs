4dafe36c903cf87eacefad9c2e3cc7ecc7c55cf6
robertoligeiro <robertoligeiro@gmail.com>
2017-06-12 22:56:31 -0700

    Find first Largest in BST: better recursive solution.

--- BstFindFirstBiggerThanK/BstFindFirstBiggerThanK/Program.cs
+++ BstFindFirstBiggerThanK/BstFindFirstBiggerThanK/Program.cs
@@ -28,36 +28,33 @@ namespace BstFindFirstBiggerThanK
             n2.right = n4;
 
             var b = FindLargestkBookSol(n0, 1); //3
+            var b1 = FindLargestKMySol(n0, 1); //3
+
             var n5 = new TreeNode() { val = 8 };
             n4.left = n5;
             b = FindLargestkBookSol(n0, 5); //7
+            b1 = FindLargestKMySol(n0, 5); //7
+
             b = FindLargestkBookSol(n0, 7); //8
+            b1 = FindLargestKMySol(n0, 7); //8
+
             b = FindLargestkBookSol(n0, 8); //9;
+            b1 = FindLargestKMySol(n0, 8); //9
+
             b = FindLargestkBookSol(n0, 9); //null;
+            b1 = FindLargestKMySol(n0, 9); //null
         }
         public static TreeNode FindLargestKMySol(TreeNode r, int k)
         {
-            var sparents = new Stack<TreeNode>();
-            while (r != null)
+            if (r == null) return null;
+            if (r.val > k)
             {
-                if (r.val == k) break;
-                sparents.Push(r);
-                if (r.val > k) r = r.left;
-                else r = r.right; 
+                var ret = FindLargestKMySol(r.left, k);
+                if (ret == null) return r;
+                return ret;
             }
 
-            if (r == null) return null;
-            if (r.right != null) //left most child on right tree
-            {
-                r = r.right;
-                while (r.left != null) r = r.left;
-                return r;
-            }
-            while (sparents.Count > 0 && sparents.Peek().left != r)
-            {
-                r = sparents.Pop();
-            }
-            return sparents.Count > 0 ? sparents.Peek():null;
+            return FindLargestKMySol(r.right, k);
         }
 
         public static TreeNode FindLargestkBookSol(TreeNode r, int k)
