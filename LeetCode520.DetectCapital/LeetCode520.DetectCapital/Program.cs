316720811408bacdd3d0e0e21fd708dd91a856bb
robertoligeiro <robertoligeiro@gmail.com>
2017-06-16 21:36:09 -0700

    LeetCode 520. Detect Capital

---
+++ LeetCode520.DetectCapital/LeetCode520.DetectCapital/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode520.DetectCapital
+{
+    //https://leetcode.com/problems/detect-capital/#/descriptions
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class Solution
+        {
+            public bool DetectCapitalUse(string word)
+            {
+                var firstCap = false;
+                var secondCap = false;
+                for (var i = 0; i < word.Length; ++i)
+                {
+                    if (i == 0 && char.IsUpper(word[i])) firstCap = true;
+                    else if (i == 1 && char.IsUpper(word[i]))
+                    {
+                        if (!firstCap) return false;
+                        secondCap = true;
+                    }
+                    else if (firstCap && secondCap && char.IsLower(word[i])) return false;
+                    else if (firstCap && !secondCap && char.IsUpper(word[i])) return false;
+                    else if (!firstCap && char.IsUpper(word[i])) return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
