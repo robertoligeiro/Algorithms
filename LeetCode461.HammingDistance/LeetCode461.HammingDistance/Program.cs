1da8742c0fc423f9e8abdc55ea8208e4d5a440c8
robertoligeiro <robertoligeiro@gmail.com>
2017-06-26 08:46:24 -0700

    LeetCode 461. HammingDistance

---
+++ LeetCode461.HammingDistance/LeetCode461.HammingDistance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode461.HammingDistance
+{
+    class Program
+    {
+        /https://leetcode.com/problems/hamming-distance/#/description
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var r = s.HammingDistance(1, 4);
+        }
+        public class Solution
+        {
+            public int HammingDistance(int x, int y)
+            {
+                var xor = x ^ y;
+                var count = 0;
+                while (xor > 0)
+                {
+                    count += (xor & 1); 
+                    xor >>= 1;
+                }
+                return count;
+            }
+        }
+    }
+}
