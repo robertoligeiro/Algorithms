2f523f840c546e4fd28dd6292402eceb6cf88036
robertoligeiro <robertoligeiro@gmail.com>
2017-06-26 08:25:21 -0700

    LeetCode: Construct Binary Tree from Preorder and Inorder Traversal

---
+++ 105.ConstructBinTreefromPreAndInOrderTrav/105.ConstructBinTreefromPreAndInOrderTrav/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _105.ConstructBinTreefromPreAndInOrderTrav
+{
+    //https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/#/description
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var r = s.BuildTree(new int[] { 5, 3, 1, 4, 7 }, new int[] { 1, 3, 4, 5, 7 });
+        }
+
+  public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+  }
+
+        public class Solution
+        {
+            public TreeNode BuildTree(int[] preorder, int[] inorder)
+            {
+                var sPre = new Stack<int>();
+                for (var i = preorder.Length - 1; i >= 0; --i) sPre.Push(preorder[i]);
+                var inL = new List<int>(inorder);
+                return BuildTree(sPre, inL);
+            }
+
+            private TreeNode BuildTree(Stack<int> pre, List<int> ino)
+            {
+                if (ino.Count == 0) return null;
+                var node = new TreeNode(pre.Pop());
+                var index = ino.IndexOf(node.val);
+                var left = new List<int>();
+                if (index > 0)
+                {
+                    left.AddRange(ino.GetRange(0, index));
+                }
+                var right = new List<int>();
+                if (index < ino.Count - 1)
+                {
+                    right.AddRange(ino.GetRange(index + 1, ino.Count - index - 1));
+                }
+                node.left = BuildTree(pre, left);
+                node.right = BuildTree(pre, right);
+                return node;
+            }
+        }
+    }
+}
