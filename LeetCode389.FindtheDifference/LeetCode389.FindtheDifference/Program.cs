4c2405021357d6811bad70e6c36f82846072b807
robertoligeiro <robertoligeiro@gmail.com>
2017-06-26 07:54:46 -0700

    leetcode: 389. Find the Difference

---
+++ LeetCode389.FindtheDifference/LeetCode389.FindtheDifference/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode389.FindtheDifference
+{
+    class Program
+    {
+        //https://leetcode.com/problems/find-the-difference/#/description
+        static void Main(string[] args)
+        {
+        }
+        public class Solution
+        {
+            public char FindTheDifference(string s, string t)
+            {
+                var h = new Dictionary<char,int>();
+                foreach (var c in s)
+                {
+                    var count = 0;
+                    if (h.TryGetValue(c, out count))
+                    {
+                        h[c] = ++count;
+                    }
+                    else h.Add(c, 1);
+                }
+                foreach (var c in t)
+                {
+                    var count = 0;
+                    if (h.TryGetValue(c, out count))
+                    {
+                        if (--count == 0)
+                        {
+                            h.Remove(c);
+                        }
+                        else h[c] = count;
+                    }
+                    else return c;
+                }
+
+                return h.FirstOrDefault().Key;
+            }
+        }
+    }
+}
