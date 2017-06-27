77371758d1f5575dbea5dd4a11170fc93b5ee691
robertoligeiro <robertoligeiro@gmail.com>
2017-06-27 08:07:12 -0700

    l

---
+++ LeetCode464.CanIWin/LeetCode464.CanIWin/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode464.CanIWin
+{
+    //https://leetcode.com/problems/can-i-win/#/description
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var r = s.CanIWin(10, 11); //false
+            r = s.CanIWin(6, 11); //true
+        }
+
+        public class Solution
+        {
+            public bool CanIWin(int maxChoosableInteger, int desiredTotal)
+            {
+                if (maxChoosableInteger >= desiredTotal) return true;
+                if (maxChoosableInteger + 1 >= desiredTotal) return false;
+                if (maxChoosableInteger * (maxChoosableInteger + 1) / 2 < desiredTotal) return false;
+                return canWin(desiredTotal);
+            }
+
+            private bool canWin(int total)
+            {
+                for (int i = 20; i >= 1; i--)
+                {
+                        int totalNext = total - i;
+                        if (totalNext <= 0) return true;
+                        else if(canWin(totalNext))
+                        {
+                            return true;
+                        }
+                }
+
+                return false;
+            }
+        }
+    }
+}
