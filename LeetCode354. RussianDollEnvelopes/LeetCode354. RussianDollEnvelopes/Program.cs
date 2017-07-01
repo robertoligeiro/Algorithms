bc1e452fff6214dc1a4a2f19c14393cc4afdddfe
robertoligeiro <robertoligeiro@gmail.com>
2017-06-30 18:46:07 -0700

    LeetCode354.RussianDollEnvelopes

--- LeetCode354. RussianDollEnvelopes/LeetCode354. RussianDollEnvelopes/Program.cs
+++ LeetCode354. RussianDollEnvelopes/LeetCode354. RussianDollEnvelopes/Program.cs
@@ -8,12 +8,15 @@ namespace LeetCode354.RussianDollEnvelopes
 {
     class Program
     {
+        //https://leetcode.com/problems/russian-doll-envelopes/#/description
+        //solution based on:https://leetcode.com/problems/russian-doll-envelopes/#/solutions 
         static void Main(string[] args)
         {
             var s = new Solution();
-            var r = s.MaxEnvelopes(new int[,] { { 5, 4 }, { 6, 4 }, { 6, 7 }, { 2, 3 } });
-            r = s.MaxEnvelopes(new int[,] { { 5, 4 }, { 1, 5 }, { 6, 4 }, { 6, 7 }, { 2, 3 } });
-            r = s.MaxEnvelopes(new int[,] { { 2, 100}, { 3, 200}, { 4, 300}, { 5, 500}, { 5, 400}, { 5, 250}, { 6, 370},{ 6, 360},{ 7, 380} });
+            //var r = s.MaxEnvelopes(new int[,] { { 5, 4 }, { 6, 4 }, { 6, 7 }, { 2, 3 } });
+            var r = s.MaxEnvelopes(new int[,] { { 4,5 }, { 4,6 }, { 6, 7 }, { 2, 3 }, { 1, 1 } });
+            //r = s.MaxEnvelopes(new int[,] { { 5, 4 }, { 1, 5 }, { 6, 4 }, { 6, 7 }, { 2, 3 } });
+            //var r = s.MaxEnvelopes(new int[,] { { 2, 100}, { 3, 200}, { 4, 300}, { 5, 500}, { 5, 400}, { 5, 250}, { 6, 370},{ 6, 360},{ 7, 380} });
         }
 
         public class Solution
@@ -26,28 +29,44 @@ namespace LeetCode354.RussianDollEnvelopes
                 {
                     lTuples.Add(new Tuple<int, int>(envelopes[i, 0], envelopes[i, 1]));
                 }
-                lTuples.Sort();
-                var usedIndex = new HashSet<int>();
-                for (int i = 0; i < lTuples.Count; ++i)
+                lTuples.Sort(CompareEvents);
+
+                return LengthOfLIS(lTuples.Select(x => x.Item2).ToArray());
+            }
+
+            private int LengthOfLIS(int[] nums)
+            {
+                if (nums.Length == 0) return 0;
+                var max = 1;
+                var increaseCounterArray = Enumerable.Repeat(1, nums.Length).ToArray();
+                var i = 1;
+                while (i < nums.Length)
                 {
-                    //if (usedIndex.Add(i))
-                    //{
-                        var s = new Stack<Tuple<int, int>>();
-                        s.Push(lTuples[i]);
-                        for (int j = i + 1; j < lTuples.Count; ++j)
+                    var j = 0;
+                    while (j < i)
+                    {
+                        if (nums[j] < nums[i])
                         {
-                            if (s.Peek().Item1 < lTuples[j].Item1 && s.Peek().Item2 < lTuples[j].Item2)
-                            {
-                                s.Push(lTuples[j]);
-                                usedIndex.Add(j);
-                            }
+                            increaseCounterArray[i] = Math.Max(increaseCounterArray[i], increaseCounterArray[j] + 1);
+                            max = Math.Max(max, increaseCounterArray[i]);
                         }
-
-                        max = Math.Max(max, s.Count);
-                    //}
+                        ++j;
+                    }
+                    ++i;
                 }
-
                 return max;
+            }
+
+            private static int CompareEvents(Tuple<int, int> a, Tuple<int, int> b)
+            {
+                if (a.Item1 > b.Item1) return 1;
+                if (a.Item1 < b.Item1) return -1;
+                if (a.Item1 == b.Item1)
+                {
+                    if (a.Item2 > b.Item2) return -1;
+                    if (a.Item2 < b.Item2) return 1;
+                }
+                return 1;
             }
         }
     }
