b59aeb3ca4abab809e45086aee436232105c4c3b
robertoligeiro <robertoligeiro@gmail.com>
2017-06-30 18:32:58 -0700

    LeetCode300.LongestIncreasingSubsequence

---
+++ LeetCode300.LongestIncreasingSubsequence/LeetCode300.LongestIncreasingSubsequence/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode300.LongestIncreasingSubsequence
+{
+    class Program
+    {
+        //https://leetcode.com/problems/longest-increasing-subsequence/#/description
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var r = s.LengthOfLIS(new int[] { 10, 9, 2, 5, 3, 7, 101, 18 });
+        }
+
+        public class Solution
+        {
+            public int LengthOfLIS(int[] nums)
+            {
+                if (nums.Length == 0) return 0;
+                var max = 1;
+                var increaseCounterArray = Enumerable.Repeat(1, nums.Length).ToArray();
+                var i = 1;
+                while (i < nums.Length)
+                {
+                    var j = 0;
+                    while (j < i)
+                    {
+                        if (nums[j] < nums[i])
+                        {
+                            increaseCounterArray[i] = Math.Max(increaseCounterArray[i], increaseCounterArray[j] + 1);
+                            max = Math.Max(max, increaseCounterArray[i]);
+                        }
+                        ++j;
+                    }
+                    ++i;
+                }
+                return max;
+            }
+        }
+    }
+}
---
+++ LeetCode354. RussianDollEnvelopes/LeetCode354. RussianDollEnvelopes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode354.RussianDollEnvelopes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var r = s.MaxEnvelopes(new int[,] { { 5, 4 }, { 6, 4 }, { 6, 7 }, { 2, 3 } });
+            r = s.MaxEnvelopes(new int[,] { { 5, 4 }, { 1, 5 }, { 6, 4 }, { 6, 7 }, { 2, 3 } });
+            r = s.MaxEnvelopes(new int[,] { { 2, 100}, { 3, 200}, { 4, 300}, { 5, 500}, { 5, 400}, { 5, 250}, { 6, 370},{ 6, 360},{ 7, 380} });
+        }
+
+        public class Solution
+        {
+            public int MaxEnvelopes(int[,] envelopes)
+            {
+                var max = 0;
+                var lTuples = new List<Tuple<int, int>>();
+                for (int i = 0; i < envelopes.GetLength(0); ++i)
+                {
+                    lTuples.Add(new Tuple<int, int>(envelopes[i, 0], envelopes[i, 1]));
+                }
+                lTuples.Sort();
+                var usedIndex = new HashSet<int>();
+                for (int i = 0; i < lTuples.Count; ++i)
+                {
+                    //if (usedIndex.Add(i))
+                    //{
+                        var s = new Stack<Tuple<int, int>>();
+                        s.Push(lTuples[i]);
+                        for (int j = i + 1; j < lTuples.Count; ++j)
+                        {
+                            if (s.Peek().Item1 < lTuples[j].Item1 && s.Peek().Item2 < lTuples[j].Item2)
+                            {
+                                s.Push(lTuples[j]);
+                                usedIndex.Add(j);
+                            }
+                        }
+
+                        max = Math.Max(max, s.Count);
+                    //}
+                }
+
+                return max;
+            }
+        }
+    }
+}
