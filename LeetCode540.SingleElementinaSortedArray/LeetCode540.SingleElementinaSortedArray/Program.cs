4d99aad1247ea71ea50b674f7f68560f4b848cf7
robertoligeiro <robertoligeiro@gmail.com>
2017-07-07 21:06:42 -0700

    LeetCode540.SingleElementinaSortedArray

---
+++ LeetCode540.SingleElementinaSortedArray/LeetCode540.SingleElementinaSortedArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode540.SingleElementinaSortedArray
+{
+    class Program
+    {
+        //https://leetcode.com/problems/single-element-in-a-sorted-array/#/description
+        // solution: https://leetcode.com/problems/single-element-in-a-sorted-array/#/solutions
+        static void Main(string[] args)
+        {
+        }
+        public class Solution
+        {
+            public int SingleNonDuplicate(int[] nums)
+            {
+                var l = 0;
+                var r = nums.Length - 1;
+                while (l < r)
+                {
+                    var mid = l + (r - l) / 2;
+                    if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1])
+                    {
+                        return nums[mid];
+                    }
+                    if (nums[mid] == nums[mid + 1] && mid % 2 == 0)
+                    {
+                        l = mid + 1;
+                    }
+                    else if (nums[mid] == nums[mid - 1] && mid % 2 == 1)
+                    {
+                        l = mid + 1;
+                    }
+                    else
+                    {
+                        r = mid - 1;
+                    }
+                }
+
+                return nums[l];
+            }
+        }
+    }
+}
