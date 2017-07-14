26b3abb4629bcf8fc9856ba6d13fe871aad568d0
robertoligeiro <robertoligeiro@gmail.com>
2017-07-14 09:16:58 -0700

    sudoku cleaner

--- SodokuSolver/SodokuSolver/Program.cs
+++ SodokuSolver/SodokuSolver/Program.cs
@@ -8,83 +8,92 @@ namespace SodokuSolver
 {
     class Program
     {
-        public static class SodokuSolver
+        static void Main(string[] args)
         {
-            public static bool SolveSodoku(int[][] board)
-            {
-                return SolveSodokuRec(board);
-            }
+            int [,] sudoku = new int[,] { 
+                { 7, 6, 0, 8, 0, 3, 0, 0, 2 }, 
+                { 0, 2, 5, 0, 0, 9, 7, 0, 6 }, 
+                { 0, 0, 1, 0, 6, 0, 0, 0, 3 }, 
+                { 0, 9, 0, 0, 0, 0, 8, 3, 1 }, 
+                { 2, 0, 0, 0, 0, 0, 0, 0, 4 }, 
+                { 4, 5, 3, 0, 0, 0, 0, 6, 0 }, 
+                { 1, 0, 0, 0, 7, 0, 3, 0, 0 }, 
+                { 5, 0, 2, 3, 0, 0, 6, 1, 0 }, 
+                { 8, 0, 0, 1, 0, 5, 0, 4, 7 } };
 
-            private static bool SolveSodokuRec(int[][] board)
+            var s = new SudokuSolver(sudoku);
+            var r = s.Solve();
+        }
+
+        public class SudokuSolver
+        {
+            public int[,] sudoku { get; set; }
+            public SudokuSolver(int[,] board)
             {
-                for (int r = 0; r <= 8; ++r)
+                sudoku = board;
+            }
+            public bool Solve()
+            {
+                for (int r = 0; r < this.sudoku.GetLength(0); ++r)
                 {
-                    for (int c = 0; c <= 8; ++c)
+                    for (int c = 0; c < this.sudoku.GetLength(1); ++c)
                     {
-                        if (board[r][c] == 0)
+                        if (this.sudoku[r, c] == 0)
                         {
-                            for (int x = 1; x <= 9; ++x)
+                            for (int v = 1; v <= 9; ++v)
                             {
-                                board[r][c] = x;
-                                if (IsValidSodoku(board, r, c))
+                                this.sudoku[r, c] = v;
+                                if (this.CanPut(r, c))
                                 {
-                                    if (SolveSodokuRec(board))
+                                    if (this.Solve())
                                     {
                                         return true;
                                     }
                                 }
                             }
-                            board[r][c] = 0;
+                            this.sudoku[r, c] = 0;
                             return false;
                         }
                     }
                 }
                 return true;
             }
-
-            private static bool IsValidSodoku(int[][] board, int row, int col)
+            private bool CanPut(int row, int col)
+            {
+                return this.CanPutRow(row) && this.CanPutCol(col) && this.CanPutBox(row, col);
+            }
+            private bool CanPutRow(int row)
             {
                 var h = new HashSet<int>();
-                for (int i = 0; i <= 8; ++i)
+                for (int i = 0; i < this.sudoku.GetLength(0); ++i)
                 {
-                    if (board[row][i] != 0 && !h.Add(board[row][i])) return false;
+                    if (this.sudoku[row, i] != 0 && !h.Add(this.sudoku[row, i])) return false;
                 }
-
-                h.Clear();
-                for (int i = 0; i <= 8; ++i)
-                {
-                    if (board[i][col] != 0 && !h.Add(board[i][col])) return false;
-                }
-
-                h.Clear();
-                if (row <= 2) row = 0;
-                else
-                if (row > 2 && row <= 5 ) row = 3;
-                else
-                if (row > 5) row = 6;
-
-                if (col <= 2) col = 0;
-                else
-                if (col > 2 && col <= 5) col = 3;
-                else
-                if (col > 5) col = 6;
-                for (int i = row; i <= row + 2; ++i)
-                {
-                    for (int j = col; j <= col + 2; ++j)
-                    {
-                        if (board[i][j] != 0 && !h.Add(board[i][j])) return false;
-                    }
-                }
-
                 return true;
             }
-        }
-
-        static void Main(string[] args)
-        {
-            int [][]board = new int[9][] {new int[] { 7, 6, 0, 8, 0, 3, 0, 0, 2 }, new int[] { 0, 2, 5, 0, 0, 9, 7, 0, 6 }, new int[] { 0, 0, 1, 0, 6, 0, 0, 0, 3 }, new int[] { 0, 9, 0, 0, 0, 0, 8, 3, 1 }, new int[] { 2, 0, 0, 0, 0, 0, 0, 0, 4 }, new int[] { 4, 5, 3, 0, 0, 0, 0, 6, 0 }, new int[] { 1, 0, 0, 0, 7, 0, 3, 0, 0 }, new int[] { 5, 0, 2, 3, 0, 0, 6, 1, 0 }, new int[] { 8, 0, 0, 1, 0, 5, 0, 4, 7 } };
-
-            var b = SodokuSolver.SolveSodoku(board);
+            private bool CanPutCol(int col)
+            {
+                var h = new HashSet<int>();
+                for (int i = 0; i < this.sudoku.GetLength(1); ++i)
+                {
+                    if (this.sudoku[i,col] != 0 && !h.Add(this.sudoku[i, col])) return false;
+                }
+                return true;
+            }
+            private bool CanPutBox(int row, int col)
+            {
+                var h = new HashSet<int>();
+                var rowStart = row - row % 3;
+                var colStart = col - col % 3;
+                for (int r = 0; r < 3; ++r)
+                {
+                    for (int c = 0; c < 3; ++c)
+                    {
+                        if (this.sudoku[rowStart + r, colStart + c] != 0 && !h.Add(this.sudoku[rowStart + r, colStart + c])) return false;
+                    }
+                }
+                return true;
+            }
         }
     }
 }
