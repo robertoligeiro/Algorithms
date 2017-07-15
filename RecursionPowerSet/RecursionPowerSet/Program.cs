7a0617bfcc2f1a76509eaeaf5697376d8812dc5b
robertoligeiro <robertoligeiro@gmail.com>
2017-07-15 11:02:56 -0700

    fix comb method to avoid copying dictionary locally.

--- RecursionPowerSet/RecursionPowerSet/Program.cs
+++ RecursionPowerSet/RecursionPowerSet/Program.cs
@@ -10,24 +10,42 @@ namespace RecursionPowerSet
     {
         static void Main(string[] args)
         {
-            var r = GetSubsets(new List<int>() { 1, 2, 3, 4 });
+            var r = ArrayComb(new List<int>() { 1, 2, 3, 4 });
+            r = ArrayComb(new List<int>() { 1, 1, 2 });
         }
 
-        public static List<List<int>> GetSubsets(List<int> a)
+        public static List<List<int>> ArrayComb(List<int> a)
         {
+            var m = new Dictionary<int, int>();
+            foreach (var i in a)
+            {
+                var count = 0;
+                if (m.TryGetValue(i, out count))
+                {
+                    m[i] = ++count;
+                }
+                else m.Add(i, 1);
+            }
             var resp = new List<List<int>>();
             var parc = new List<int>();
-            GetSubsets(a, resp, parc, 0);
+            ArrayComb(m, resp, parc, 0);
             return resp;
         }
-        public static void GetSubsets(List<int> a, List<List<int>> resp, List<int> parc, int start)
+
+        public static void ArrayComb(Dictionary<int, int> m, List<List<int>> r, List<int> parc, int pos)
         {
-            resp.Add(new List<int>(parc));
-            for (int i = start; i < a.Count; ++i)
+            r.Add(new List<int>(parc));
+            if (pos == m.Count) return;
+            for (int i = pos; i < m.Count; ++i)
             {
-                parc.Add(a[i]);
-                GetSubsets(a, resp, parc, i + 1);
-                parc.RemoveAt(parc.Count - 1);
+                if (m.ElementAt(i).Value > 0)
+                {
+                    parc.Add(m.ElementAt(i).Key);
+                    m[m.ElementAt(i).Key]--;
+                    ArrayComb(m, r, parc, i);
+                    parc.RemoveAt(parc.Count - 1);
+                    m[m.ElementAt(i).Key]++;
+                }
             }
         }
     }
