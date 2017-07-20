6c85ce91fb9f00b3c3727cbf1075f83dae15bdbc
robertoligeiro <robertoligeiro@gmail.com>
2017-07-20 08:50:13 -0700

    fix serialization to use pre-order.

--- LeetCode297.SerializeandDeserializeBinaryTree/LeetCode297.SerializeandDeserializeBinaryTree/Program.cs
+++ LeetCode297.SerializeandDeserializeBinaryTree/LeetCode297.SerializeandDeserializeBinaryTree/Program.cs
@@ -14,15 +14,55 @@ namespace LeetCode297.SerializeandDeserializeBinaryTree
             var c = new Codec();
             var root = c.deserialize("5,3,7,1,null,6,null,null,null,null,null");
             var s = c.serialize(root);
+
+            var root1 = TreeSerialization.Deserialize("5,3,1,null,null,null,7,6,null,null,null");
+            var s1 = TreeSerialization.Serialize(root1);
         }
 
-  public class TreeNode {
+        public class TreeNode {
       public int val;
       public TreeNode left;
       public TreeNode right;
       public TreeNode(int x) { val = x; }
   }
+        public static class TreeSerialization
+        {
+            public static string Serialize(TreeNode n)
+            {
+                var resp = new List<string>();
+                SerializePreOrder(n, resp);
+                return string.Join(",", resp);
+            }
 
+            private static void SerializePreOrder(TreeNode n, List<string> s)
+            {
+                if (n == null)
+                {
+                    s.Add("null");
+                    return;
+                }
+                s.Add(n.val.ToString());
+                SerializePreOrder(n.left, s);
+                SerializePreOrder(n.right, s);
+            }
+
+            public static TreeNode Deserialize(string tree)
+            {
+                var nodes = tree.Split(',');
+                var q = new Queue<string>(nodes);
+                return DeserializePreOrder(q);
+            }
+
+            private static TreeNode DeserializePreOrder(Queue<string> q)
+            {
+                var n = q.Dequeue();
+                if (n == "null") return null;
+                var node = new TreeNode(int.Parse(n));
+                node.left = DeserializePreOrder(q);
+                node.right = DeserializePreOrder(q);
+                return node;
+            }
+        }
         public class Codec
         {
 
