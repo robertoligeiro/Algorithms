dc6e7789bac727022aaec84b68e7e901cfbcf4b9
robertoligeiro <robertoligeiro@gmail.com>
2017-07-23 09:31:25 -0700

    LeetCode 208. ImplementTrie.

---
+++ LeetCode208.ImplementTrie/LeetCode208.ImplementTrie/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode208.ImplementTrie
+{
+    class Program
+    {
+        //https://leetcode.com/problems/implement-trie-prefix-tree/#/description
+        static void Main(string[] args)
+        {
+            var trie = new Trie();
+            trie.Insert("test");
+            trie.Insert("roberto");
+            trie.Insert("ana");
+            trie.Insert("tests");
+            trie.Insert("tesla");
+            var b = trie.Search("roberto"); //true
+            b = trie.Search("robert"); //false
+            b = trie.StartsWith("robert"); //true
+            b = trie.Search("ana"); //true
+            b = trie.Search("test"); //true
+            b = trie.Search("tests"); //true
+            b = trie.Search("testss"); //false
+            b = trie.StartsWith("testss"); //false
+        }
+
+        public class TrieNode
+        {
+            public int IsEnd = 0;
+            public Dictionary<char, TrieNode> map = new Dictionary<char, TrieNode>();
+        }
+        public class Trie
+        {
+            private TrieNode head = new TrieNode() { IsEnd = 1 };
+
+            /** Initialize your data structure here. */
+            public Trie()
+            {
+
+            }
+
+            /** Inserts a word into the trie. */
+            public void Insert(string word)
+            {
+                var curr = this.head;
+                foreach (var c in word)
+                {
+                    TrieNode next;
+                    if (!curr.map.TryGetValue(c, out next))
+                    {
+                        next = new TrieNode();
+                        curr.map.Add(c, next);
+                    }
+                    curr = next;
+                }
+                curr.IsEnd++;
+            }
+
+            /** Returns if the word is in the trie. */
+            public bool Search(string word)
+            {
+                return SearchPath(word, true);
+            }
+
+            /** Returns if there is any word in the trie that starts with the given prefix. */
+            public bool StartsWith(string prefix)
+            {
+                return SearchPath(prefix, false);
+            }
+
+            private bool SearchPath(string word, bool checkEnd)
+            {
+                var curr = this.head;
+                foreach (var c in word)
+                {
+                    TrieNode next;
+                    if (!curr.map.TryGetValue(c, out next)) return false;
+                    curr = next;
+                }
+
+                if (checkEnd) return curr.IsEnd > 0;
+                return true;
+            }
+        }
+
+    }
+}
---
+++ LeetCode208.ImplementTrie/LeetCode208.ImplementTrie/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LeetCode208.ImplementTrie")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode208.ImplementTrie")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f3acdc3d-5bdc-4a7c-bbe4-e163abd73964")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
