266a6617cf259fca76403e5b3c593611782fa437
robertoligeiro <robertoligeiro@gmail.com>
2017-07-26 07:38:00 -0700

    LeetCode11.ContainerWithMostWater

---
+++ LeetCode11.ContainerWithMostWater/LeetCode11.ContainerWithMostWater/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode11.ContainerWithMostWater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public class Solution
+        {
+            //https://leetcode.com/problems/container-with-most-water/#/description
+            public int MaxArea(int[] height)
+            {
+                var max = 0;
+                var dist = height.Length - 1;
+                var left = 0;
+                var right = height.Length - 1;
+                while (left < right)
+                {
+                    if (height[left] == height[right])
+                    {
+                        max = Math.Max(max, height[left] * dist);
+                        left++;
+                        right--;
+                        dist -= 2;
+                    }
+                    else if (height[left] > height[right])
+                    {
+                        max = Math.Max(max, height[right] * dist);
+                        right--;
+                        dist--;
+                    }
+                    else
+                    {
+                        max = Math.Max(max, height[left] * dist);
+                        left++;
+                        dist--;
+                    }
+                }
+                return max;
+            }
+        }
+    }
+}
