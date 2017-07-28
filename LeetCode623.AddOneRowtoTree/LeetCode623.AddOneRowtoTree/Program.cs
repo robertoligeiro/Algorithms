30152d0184da597c35d4ed9d1c051bee636e64f4
robertoligeiro <robertoligeiro@gmail.com>
2017-07-28 09:18:46 -0700

    LeetCode623.AddOneRowtoTree

---
+++ LeetCode623.AddOneRowtoTree/LeetCode623.AddOneRowtoTree/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode623.AddOneRowtoTree
+{
+    class Program
+    {
+        //https://leetcode.com/problems/add-one-row-to-tree/tabs/description
+        static void Main(string[] args)
+        {
+        }
+        public class TreeNode {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+        public class Solution
+        {
+            public TreeNode AddOneRow(TreeNode root, int v, int d)
+            {
+                if (d == 1) return new TreeNode(v) { left = root };
+                var l = 1;
+                var q1 = new Queue<TreeNode>();
+                var q2 = new Queue<TreeNode>();
+                q1.Enqueue(root);
+                while (q1.Count > 0 || q2.Count > 0)
+                {
+                    if (l == d - 1)
+                    {
+                        AddOneRow(q1, v);
+                        return root;
+                    }
+                    MoveFromQueues(q1, q2);
+                    l++;
+                    if (l == d - 1)
+                    {
+                        AddOneRow(q2, v);
+                        return root;
+                    }
+                    MoveFromQueues(q2, q1);
+                    l++;
+                }
+                return root;
+            }
+            private void MoveFromQueues(Queue<TreeNode> from, Queue<TreeNode> to)
+            {
+                while (from.Count > 0)
+                {
+                    var curr = from.Dequeue();
+                    if (curr.left != null) to.Enqueue(curr.left);
+                    if (curr.right != null) to.Enqueue(curr.right);
+                }
+            }
+            private void AddOneRow(Queue<TreeNode> q, int vaule)
+            {
+                while (q.Count > 0)
+                {
+                    var curr = q.Dequeue();
+                    var left = curr.left;
+                    var rigth = curr.right;
+                    curr.left = new TreeNode(vaule) { left = left };
+                    curr.right = new TreeNode(vaule) { right = rigth };
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode623.AddOneRowtoTree/LeetCode623.AddOneRowtoTree/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LeetCode623.AddOneRowtoTree")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode623.AddOneRowtoTree")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3a2d800c-91f1-4ef2-92b7-8df771d6b11a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
