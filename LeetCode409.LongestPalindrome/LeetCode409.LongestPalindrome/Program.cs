784606f008d5b95329fae11413b4506447fc2b5a
robertoligeiro <robertoligeiro@gmail.com>
2017-07-29 12:55:23 -0700

    LeetCode409.LongestPalindrome

---
+++ LeetCode409.LongestPalindrome/LeetCode409.LongestPalindrome/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode409.LongestPalindrome
+{
+    //https://leetcode.com/problems/longest-palindrome/description/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var r = s.LongestPalindrome("abccccdd");
+        }
+        public class Solution
+        {
+            public int LongestPalindrome(string s)
+            {
+                if (string.IsNullOrEmpty(s)) return 0;
+                var h = new HashSet<char>();
+                var countPairs = 0;
+                foreach (var c in s)
+                {
+                    if (!h.Add(c))
+                    {
+                        h.Remove(c);
+                        countPairs++;
+                    }
+                }
+
+                if (h.Count == 0) return countPairs * 2;
+                return (countPairs * 2) + 1;
+            }
+        }
+    }
+}
