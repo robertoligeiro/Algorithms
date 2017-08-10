90f0c1c884a496ef23b7bdeafc7b8a0fe053a176
robertoligeiro <robertoligeiro@gmail.com>
2017-08-10 08:09:37 -0700

    LeetCode551.Student_Attendance_Record_I

---
+++ LeetCode551. Student Attendance Record I/LeetCode551. Student Attendance Record I/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode551.Student_Attendance_Record_I
+{
+    class Program
+    {
+        //https://leetcode.com/submissions/detail/113318002/    
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var b = s.CheckRecord("PPALLP");
+            b = s.CheckRecord("PPALLL");
+        }
+
+        public class Solution
+        {
+            public bool CheckRecord(string s)
+            {
+                var countA = 0;
+                var countL = 0;
+                foreach (var c in s)
+                {
+                    if (c == 'A') countA++;
+                    if (c == 'L') countL++;
+                    else countL = 0;
+                    if (countA >= 2 || countL > 2) return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
