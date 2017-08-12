12ef0f683eb079dd2f6d445b9af9d33803dbe90d
robertoligeiro <robertoligeiro@gmail.com>
2017-08-11 18:16:18 -0700

    LeetCode654.Maximum_Binary_Tree

---
+++ LeetCode654. Maximum Binary Tree/LeetCode654. Maximum Binary Tree/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode654.Maximum_Binary_Tree
+{
+    class Program
+    {
+        //https://leetcode.com/problems/maximum-binary-tree/description/
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var r = s.ConstructMaximumBinaryTree(new int[] { 3, 2, 1, 6, 0, 5 });
+        }
+
+        public class TreeNode {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+        public class Solution
+        {
+            public TreeNode ConstructMaximumBinaryTree(int[] nums)
+            {
+                return ConstructMaximumBinaryTree(new List<int>(nums));
+            }
+            public TreeNode ConstructMaximumBinaryTree(List<int> nums)
+            {
+                if (nums == null || nums.Count == 0) return null;
+
+                var index = nums.IndexOf(nums.Max());
+                var n = new TreeNode(nums[index]);
+                n.left = ConstructMaximumBinaryTree(nums.GetRange(0, index));
+                n.right = ConstructMaximumBinaryTree(nums.GetRange(index + 1, nums.Count - index - 1));
+                return n;
+            }
+        }
+    }
+}
