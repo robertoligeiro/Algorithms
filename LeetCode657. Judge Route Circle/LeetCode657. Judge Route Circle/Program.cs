aa5aecd519f4d2c3a3cfe66adcaf7b506d48ef77
robertoligeiro <robertoligeiro@gmail.com>
2017-08-16 21:03:25 -0700

    LeetCode657.Judge_Route_Circle

---
+++ LeetCode657. Judge Route Circle/LeetCode657. Judge Route Circle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode657.Judge_Route_Circle
+{
+    class Program
+    {
+        //https://leetcode.com/problems/judge-route-circle/description/
+        static void Main(string[] args)
+        {
+        }
+        public class Solution
+        {
+            public bool JudgeCircle(string moves)
+            {
+                var x = 0;
+                var y = 0;
+                foreach (var c in moves)
+                {
+                    switch(c)
+                    {
+                        case 'U':
+                            y--;
+                            break;
+                        case 'D':
+                            y++;
+                            break;
+                        case 'L':
+                            x--;
+                            break;
+                        case 'R':
+                            x++;
+                            break;
+                    }
+                }
+                return x == 0 && y == 0;
+            }
+        }
+    }
+}
