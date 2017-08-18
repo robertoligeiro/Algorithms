895003e42889484340e90c42e1071f8f56091e85
robertoligeiro <robertoligeiro@gmail.com>
2017-08-17 23:43:10 -0700

    InterviewBloombergScreen

---
+++ InterviewBloombergScreen/InterviewBloombergScreen/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewBloombergScreen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = 0;
+            r = FindNonRepeated(new List<int>() { 1 }); //1
+            r = FindNonRepeated(new List<int>() { 1, 1, 2 }); //2
+            r = FindNonRepeated(new List<int>() { 1, 2, 2 }); // 1
+            r = FindNonRepeated(new List<int>() { 1, 1, 2, 2, 3 }); //3
+            r = FindNonRepeated(new List<int>() { 1, 1, 2, 2, 3, 3, 4, 5, 5 }); //4
+            r = FindNonRepeated(new List<int>() { 1, 1, 2, 3, 3, 4, 4, 5, 5 }); //2
+            r = FindNonRepeated(new List<int>() { 1, 2, 2, 3, 3, 4, 4, 5, 5 }); // 1
+            r = FindNonRepeated(new List<int>() { 1, 1, 2, 2, 3, 3, 4, 4, 5 }); //5
+            r = FindNonRepeated(new List<int>() { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6 }); //6
+            r = FindNonRepeated(new List<int>() { 1, 1, 2, 2, 3, 3, 4, 4, 5, 6, 6 }); //5
+        }
+
+        public static int FindNonRepeated(List<int> nums)
+        {
+            return FindNonRepeated(nums, 0, nums.Count - 1);
+        }
+        public static int FindNonRepeated(List<int> nums, int l, int r)
+        {
+            if (l > r) throw new ArgumentException("non repeated doesn't exist");
+            if (l == r) return nums[l];
+            var mid = l + (r - l) / 2;
+            if (mid % 2 == 0)
+            {
+                if (nums[mid] == nums[mid + 1])
+                {
+                    return FindNonRepeated(nums, mid + 2, r);
+                }
+                return FindNonRepeated(nums, l, mid);
+            }
+
+            if (nums[mid] == nums[mid - 1])
+            {
+                return FindNonRepeated(nums, mid + 1, r);
+            }
+            else
+            {
+                return FindNonRepeated(nums, l, mid - 1);
+            }
+        }
+    }
+}
