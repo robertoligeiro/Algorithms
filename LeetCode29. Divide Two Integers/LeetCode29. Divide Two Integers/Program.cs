fb76257024b5b0c12827771b2cac1c8ad964efc7
robertoligeiro <robertoligeiro@gmail.com>
2017-08-18 09:10:26 -0700

    LeetCode29.Divide_Two_Integers

---
+++ LeetCode29. Divide Two Integers/LeetCode29. Divide Two Integers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode29.Divide_Two_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var snaive = new SolutionNaive();
+            var r = snaive.Divide(8, 2);
+
+            var s = new Solution();
+            var r1 = s.Divide(8, 2);
+        }
+        public class SolutionNaive
+        {
+            public int Divide(int dividend, int divisor)
+            {
+                var resp = 0;
+                while (dividend >= divisor)
+                {
+                    dividend -= divisor;
+                    resp++;
+                }
+                return resp;
+            }
+        }
+
+        public class Solution
+        {
+            public int Divide(int dividend, int divisor)
+            {
+                var resp = 0;
+                while (dividend >= divisor)
+                {
+                    var tempDivisor = divisor;
+                    var mult = 1;
+                    while (dividend >= (tempDivisor << 1))
+                    {
+                        tempDivisor <<= 1;
+                        mult <<= 1;
+                    }
+                    dividend -= tempDivisor;
+                    resp += mult;
+                }
+                return resp;
+            }
+        }
+    }
+}
