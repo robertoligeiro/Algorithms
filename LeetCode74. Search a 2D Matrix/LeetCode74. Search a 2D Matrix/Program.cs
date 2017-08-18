121f654db82af9c898bd7ee9d515b72f26521ef7
robertoligeiro <robertoligeiro@gmail.com>
2017-08-18 09:27:43 -0700

    LeetCode74.Search_a_2D_Matrix

---
+++ LeetCode74. Search a 2D Matrix/LeetCode74. Search a 2D Matrix/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode74.Search_a_2D_Matrix
+{
+    class Program
+    {
+        //https://leetcode.com/problems/search-a-2d-matrix/description/
+        static void Main(string[] args)
+        {
+        }
+        public class Solution
+        {
+            public bool SearchMatrix(int[,] matrix, int target)
+            {
+                var row = 0;
+                var col = matrix.GetLength(1) - 1;
+                while (row < matrix.GetLength(0) && col >= 0)
+                {
+                    if (matrix[row, col] == target) return true;
+                    if (matrix[row, col] > target) col--;
+                    else row++;
+                }
+                return false;
+            }
+        }
+    }
+}
