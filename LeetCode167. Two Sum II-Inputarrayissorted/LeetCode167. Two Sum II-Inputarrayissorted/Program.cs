98d808c4f7c7c117178be7436d4d201c153c3952
robertoligeiro <robertoligeiro@gmail.com>
2017-08-19 22:17:06 -0700

    LeetCode167.Two_Sum_II_Inputarrayissorted

---
+++ LeetCode167. Two Sum II-Inputarrayissorted/LeetCode167. Two Sum II-Inputarrayissorted/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode167.Two_Sum_II_Inputarrayissorted
+{
+    //https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var r = s.TwoSum(new int[] { 2,7,11,15}, 9);
+        }
+        public class Solution
+        {
+            public int[] TwoSum(int[] numbers, int target)
+            {
+                var l = 0;
+                var r = numbers.Length - 1;
+                while (l < r)
+                {
+                    var num = numbers[l] + numbers[r];
+                    if (num == target) return new int[] { l+1, r+1 };
+                    if (num > target) r--;
+                    else l++;
+                }
+                return null;
+            }
+        }
+    }
+}
