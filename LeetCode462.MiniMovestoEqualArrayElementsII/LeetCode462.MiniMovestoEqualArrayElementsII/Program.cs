badcb9a18d677749c04116f002462cc44249ef83
robertoligeiro <robertoligeiro@gmail.com>
2017-09-01 17:33:11 -0700

    LeetCodeMiniMovestoEqualArrayElementsII

---
+++ LeetCode462.MiniMovestoEqualArrayElementsII/LeetCode462.MiniMovestoEqualArrayElementsII/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode462.MiniMovestoEqualArrayElementsII
+{
+    class Program
+    {
+        //https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/description/
+        static void Main(string[] args)
+        {
+        }
+        public class Solution
+        {
+            public int MinMoves2(int[] nums)
+            {
+                var l = new List<int>(nums);
+                l.Sort();
+                var i = 0;
+                var j = l.Count - 1;
+                var count = 0;
+                while (i < j)
+                {
+                    count += (l[j--] - l[i++]);
+                }
+                return count;
+            }
+        }
+    }
+}
