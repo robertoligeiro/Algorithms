6cfb1d29eacf25cde3920db8f2176f833dd2d88c
robertoligeiro <robertoligeiro@gmail.com>
2017-09-07 20:53:41 -0700

    RelativeRanks

--- LeetCode506.RelativeRanks/LeetCode506.RelativeRanks/Program.cs
+++ LeetCode506.RelativeRanks/LeetCode506.RelativeRanks/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace LeetCode506.RelativeRanks
 {
+    //https://leetcode.com/problems/relative-ranks/description/
     class Program
     {
         static void Main(string[] args)
@@ -18,19 +19,40 @@ namespace LeetCode506.RelativeRanks
         {
             public string[] FindRelativeRanks(int[] nums)
             {
+                var map = new List<Tuple<int, int>>();
+                for (int i = 0; i < nums.Length; ++i)
+                {
+                    map.Add(new Tuple<int,int>(nums[i], i));
+                }
+                map.Sort();
+                var resp = new string[nums.Length];
+                if(map.Count > 0) resp[map.ElementAt(map.Count - 1).Item2] = "Gold Medal";
+                if (map.Count > 1) resp[map.ElementAt(map.Count - 2).Item2] = "Silver Medal";
+                if (map.Count > 2) resp[map.ElementAt(map.Count - 3).Item2] = "Bronze Medal";
+                for (int i = map.Count - 4; i >= 0; --i)
+                {
+                    resp[map[i].Item2] = (map.Count - i).ToString();
+                }
+                return resp;
+            }
+        }
+        public class Solution2
+        {
+            public string[] FindRelativeRanks(int[] nums)
+            {
                 var heap = new List<int>(nums);
                 heap.Sort(Compare);
                 var m = new Dictionary<int, int>();
                 for (int i = 0; i < heap.Count; ++i)
                 {
-                    m.Add(heap[i], i+1);
+                    m.Add(heap[i], i + 1);
                 }
 
                 var resp = new List<string>();
                 foreach (var i in nums)
                 {
                     if (m[i] == 1) resp.Add("Gold Medal");
-                    else if(m[i] == 2) resp.Add("Silver Medal");
+                    else if (m[i] == 2) resp.Add("Silver Medal");
                     else if (m[i] == 3) resp.Add("Bronze Medal");
                     else resp.Add(m[i].ToString());
                 }
@@ -43,5 +65,6 @@ namespace LeetCode506.RelativeRanks
                 return -1;
             }
         }
+
     }
 }
