43cf20626f5095d4bb5d618f73afa4b2e042f231
robertoligeiro <robertoligeiro@gmail.com>
2017-09-08 22:34:33 -0700

    LeetCode239.Sliding_Window_Maximum fix to use deque only.

--- LeetCode239. Sliding Window Maximum/LeetCode239. Sliding Window Maximum/Program.cs
+++ LeetCode239. Sliding Window Maximum/LeetCode239. Sliding Window Maximum/Program.cs
@@ -12,10 +12,51 @@ namespace LeetCode239.Sliding_Window_Maximum
         static void Main(string[] args)
         {
             var s = new Solution();
-            var r = s.MaxSlidingWindow(new int[] { 1, 3, -1, -3, 5, 3, 6, 7 }, 3);
+            //var r = s.MaxSlidingWindow(new int[] { 1, 3, -1, -3, 5, 3, 6, 7 }, 3);
             //var r = s.MaxSlidingWindow(new int[] { 4,-2 }, 2);
+            var r = s.MaxSlidingWindow(new int[] { 1}, 1);
+            var r = s.MaxSlidingWindow(new int[] { 1 , -1}, 1);
         }
+
         public class Solution
+        {
+            public int[] MaxSlidingWindow(int[] nums, int k)
+            {
+                if (nums == null || nums.Length == 0 || k == 0) return new int[0];
+                var deque = new LinkedList<Tuple<int,int>>();
+                var max = int.MinValue;
+                for (int i = 0; i < k; ++i)
+                {
+                    deque.AddLast(new Tuple<int, int>(nums[i], i));
+                    max = Math.Max(max, nums[i]);
+                }
+                var resp = new List<int>();
+                for (int i = k; i <= nums.Length; ++i)
+                {
+                    while (deque.Count > 0 && deque.First().Item1 < max)
+                    {
+                        deque.RemoveFirst();
+                    }
+                    resp.Add(max);
+
+                    if (deque.Count > 0 && deque.First().Item2 == i - k)
+                    {
+                        deque.RemoveFirst();
+                        max = int.MinValue;
+                        if(deque.Count > 0)
+                            max = deque.Max().Item1;
+                    }
+                    if (i < nums.Length)
+                    {
+                        deque.AddLast(new Tuple<int, int>(nums[i], i));
+                        max = Math.Max(max, nums[i]);
+                    }
+                }
+                return resp.ToArray();
+            }
+        }
+
+        public class Solution2
         {
             public int[] MaxSlidingWindow(int[] nums, int k)
             {
