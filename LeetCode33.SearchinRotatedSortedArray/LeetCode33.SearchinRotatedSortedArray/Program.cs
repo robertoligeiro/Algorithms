b469b2f954bace3ea1565937fd722ce4951817a5
robertoligeiro <robertoligeiro@gmail.com>
2017-09-10 23:43:43 -0700

    fix SearchinRotatedSortedArray

--- LeetCode33.SearchinRotatedSortedArray/LeetCode33.SearchinRotatedSortedArray/Program.cs
+++ LeetCode33.SearchinRotatedSortedArray/LeetCode33.SearchinRotatedSortedArray/Program.cs
@@ -27,49 +27,33 @@ namespace LeetCode33.SearchinRotatedSortedArray
         {
             public int Search(int[] nums, int target)
             {
-                if (nums == null || nums.Length == 0) return -1;
-                var l = 0;
-                var r = nums.Length - 1;
-                int mid = 0;
-                while (l < r)
-                {
-                    mid = l + (r - l) / 2;
-                    if (nums[mid] > nums[r])
-                    {
-                        l = mid + 1;
-                    }
-                    else
-                    {
-                        r = mid;
-                    }
-                }
-                int turn = l;
-                if (turn > 0 && target >= nums[0] && target <= nums[turn - 1])
-                {
-                    l = 0; r = turn - 1;
-                }
-                else
-                {
-                    l = turn;
-                    r = nums.Length - 1;
-                }
-
+                return Search(nums, target, 0, nums.Length - 1);
+            }
+            private int Search(int[] nums, int target, int l, int r)
+            {
+                if (l > r) return -1;
                 while (l <= r)
                 {
-                    mid = l + (r - l) / 2;
-                    if (nums[mid] == target)
+                    var mid = l + (r - l) / 2;
+                    if (nums[mid] == target) return mid;
+                    if (nums[mid] < nums[r])
                     {
-                        return mid;
+                        if (nums[mid] < target && nums[r] >= target)
+                        {
+                            l = mid + 1;
+                        }
+                        else
+                        {
+                            r = mid - 1;
+                        }
                     }
                     else
-                    if (nums[mid] > target)
                     {
-                        r = mid - 1;
+                        var ret = Search(nums, target, l, mid - 1);
+                        if (ret != -1) return ret;
+                        return Search(nums, target, mid + 1, r);
                     }
-                    else l = mid + 1;
                 }
-
-
                 return -1;
             }
         }
