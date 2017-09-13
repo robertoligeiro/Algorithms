ae8cc8da7ca46106ddd50123d581fb18523b1173
robertoligeiro <robertoligeiro@gmail.com>
2017-09-12 22:27:38 -0700

    LeetCode380.Insert_Delete_GetRandom_O_1_

---
+++ LeetCode380. Insert Delete GetRandom O(1)/LeetCode380. Insert Delete GetRandom O(1)/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode380.Insert_Delete_GetRandom_O_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomizedSet randomSet = new RandomizedSet();
+            // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+            var b = randomSet.Insert(1);
+
+            // Returns false as 2 does not exist in the set.
+            b = randomSet.Remove(2);
+
+            // Inserts 2 to the set, returns true. Set now contains [1,2].
+            b = randomSet.Insert(2);
+
+            // getRandom should return either 1 or 2 randomly.
+            var r = randomSet.GetRandom();
+
+            // Removes 1 from the set, returns true. Set now contains [2].
+            b = randomSet.Remove(1);
+
+            // 2 was already in the set, so return false.
+            b = randomSet.Insert(2);
+
+            // Since 2 is the only number in the set, getRandom always return 2.
+            r = randomSet.GetRandom();
+        }
+        public class RandomizedSet
+        {
+            private HashSet<int> elements;
+
+            private Random rand;
+            /** Initialize your data structure here. */
+            public RandomizedSet()
+            {
+                elements = new HashSet<int>();
+                rand = new Random();
+            }
+
+            /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+            public bool Insert(int val)
+            {
+                return elements.Add(val);
+            }
+
+            /** Removes a value from the set. Returns true if the set contained the specified element. */
+            public bool Remove(int val)
+            {
+                return elements.Remove(val);
+            }
+
+            /** Get a random element from the set. */
+            public int GetRandom()
+            {
+                var index = rand.Next(0, elements.Count);
+                return elements.ElementAt(index);
+            }
+        }
+    }
+}
---
+++ LeetCode380. Insert Delete GetRandom O(1)/LeetCode380. Insert Delete GetRandom O(1)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LeetCode380. Insert Delete GetRandom O(1)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode380. Insert Delete GetRandom O(1)")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b4f23dcc-ee10-4548-a7d7-b161b6b7a78f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
