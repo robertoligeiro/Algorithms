f1b331c5d0ffd8453297befb897fbaf029ebc320
robertoligeiro <robertoligeiro@gmail.com>
2017-09-19 08:40:18 -0700

    LeetCode432.AllOoneDataStructure

---
+++ LeetCode432.AllOoneDataStructure/LeetCode432.AllOoneDataStructure/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode432.AllOoneDataStructure
+{
+    class Program
+    {
+        //https://leetcode.com/problems/all-oone-data-structure/description/
+        //no c# on leetcode
+        static void Main(string[] args)
+        {
+        }
+    }
+}
