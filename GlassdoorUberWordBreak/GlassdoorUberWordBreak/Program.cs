90aed79c6dc3b2875e39f320fb85ac1f3095567e
robertoligeiro <robertoligeiro@gmail.com>
2017-09-20 21:29:19 -0700

    GlassdoorUberWordBreak

---
+++ GlassdoorUberWordBreak/GlassdoorUberWordBreak/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlassdoorUberWordBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = WordBreak("leetcode", new List<string>() { "leet", "code" });
+        }
+
+        public static bool WordBreak(string s, List<string> words)
+        {
+            var m = new Dictionary<string, int>();
+            foreach (var w in words)
+            {
+                var count = 0;
+                if (m.TryGetValue(w, out count)) m[w] = ++count;
+                else m.Add(w, 1);
+            }
+            return WordBreak(s, 0, m);
+        }
+
+        public static bool WordBreak(string s, int start, Dictionary<string, int> m)
+        {
+            if (s.Length == start) return true;
+            for (int i = start; i <= s.Length; ++i)
+            {
+                var key = s.Substring(start, i - start);
+                var count = 0;
+                if (m.TryGetValue(key, out count) && count > 0)
+                {
+                    m[key] = --count;
+                    if (WordBreak(s, i, m)) return true;
+                    m[key] = ++count;
+                }
+            }
+            return false;
+        }
+    }
+}
