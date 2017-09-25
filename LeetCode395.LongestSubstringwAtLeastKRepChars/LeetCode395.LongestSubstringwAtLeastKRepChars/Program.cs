86cda2e8b13ba74ac0842a19d77eeeda4ca73099
robertoligeiro <robertoligeiro@gmail.com>
2017-09-24 20:35:17 -0700

    LeetCode395.LongestSubstringwAtLeastKRepChars

--- LeetCode395.LongestSubstringwAtLeastKRepChars/LeetCode395.LongestSubstringwAtLeastKRepChars/Program.cs
+++ LeetCode395.LongestSubstringwAtLeastKRepChars/LeetCode395.LongestSubstringwAtLeastKRepChars/Program.cs
@@ -11,11 +11,11 @@ namespace LeetCode395.LongestSubstringwAtLeastKRepChars
         static void Main(string[] args)
         {
             var s = new Solution();
-            //var r = s.LongestSubstring("aaabb", 3);
-            //var r = s.LongestSubstring("aaabbb", 3);
-            //var r = s.LongestSubstring("bbaaacbd", 3);
-            var r = s.LongestSubstring("abcabcabcdabcabc", 3);
-            //var r = s.LongestSubstring("aacbbbdc", 2);
+            var r = s.LongestSubstring("aaabb", 3); //3
+            r = s.LongestSubstring("aaabbb", 3); //3
+            r = s.LongestSubstring("bbaaacbd", 3); //3
+            r = s.LongestSubstring("abcabcabcdabcabc", 3); //9
+            r = s.LongestSubstring("aacbbdbc", 2); //3
         }
 
         public class Solution
@@ -42,7 +42,7 @@ namespace LeetCode395.LongestSubstringwAtLeastKRepChars
 
             public void PopulateHist(string s, int k, List<char> b, List<char> h)
             {
-                var hist = new Dictionary< char, int>(); 
+                var hist = new Dictionary<char, int>();
                 foreach (var c in s)
                 {
                     var count = 0;
@@ -57,5 +57,44 @@ namespace LeetCode395.LongestSubstringwAtLeastKRepChars
                 }
             }
         }
+
+        // Solution 2 - code is cleaner, but is worse is perf.
+        public class Solution2
+        {
+            public int LongestSubstring(string s, int k)
+            {
+                if (string.IsNullOrEmpty(s)) return 0;
+                var hist = new Dictionary<char, int>();
+                foreach (var c in s)
+                {
+                    var count = 0;
+                    if (hist.TryGetValue(c, out count)) { hist[c] = ++count; }
+                    else hist.Add(c, 1);
+                }
+                return LongestSubstring(s, k, hist);
+            }
+
+            private int LongestSubstring(string s, int k, Dictionary<char, int> m)
+            {
+                if (string.IsNullOrWhiteSpace(s)) return 0;
+                var max = s.Length;
+                foreach (var t in m)
+                {
+                    if (t.Value < k)
+                    {
+                        var index = s.IndexOf(t.Key);
+                        if (index != -1)
+                        {
+                            var left = LongestSubstring(s.Substring(0, index), k);
+                            var right = LongestSubstring(s.Substring(index + 1, s.Length - index - 1), k);
+                            max = Math.Max(left, right);
+                        }
+                    }
+                }
+                return max;
+            }
+        }
+
+
     }
 }
