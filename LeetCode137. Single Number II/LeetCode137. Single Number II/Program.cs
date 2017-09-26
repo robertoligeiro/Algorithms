8a9316726be27f3fa1459aad650e4124288da6b4
robertoligeiro <robertoligeiro@gmail.com>
2017-09-25 18:57:30 -0700

    LeetCode137.Single_Number_II

---
+++ LeetCode137. Single Number II/LeetCode137. Single Number II/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode137.Single_Number_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class Solution
+        {
+            public int SingleNumber(int[] nums)
+            {
+                var isNot = new HashSet<int>();
+                var canBe = new HashSet<int>();
+                foreach (var i in nums)
+                {
+                    if (!isNot.Add(i)) canBe.Remove(i);
+                    else canBe.Add(i);
+                }
+                return canBe.First();
+            }
+        }
+    }
+}
