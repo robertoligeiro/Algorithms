bd99134ae3d569975532aecd56bfead99fe01e38
robertoligeiro <robertoligeiro@gmail.com>
2017-10-02 13:09:43 -0700

    LeetCode448.FindAllNumbersDisappearedArray

---
+++ LeetCode448.FindAllNumbersDisappearedArray/LeetCode448.FindAllNumbersDisappearedArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode448.FindAllNumbersDisappearedArray
+{
+    class Program
+    {
+        //https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var r = s.FindDisappearedNumbers(new int[] { 4,3,2,7,8,2,3,1});
+            //var r = s.FindDisappearedNumbers(new int[] { 1, 1 });
+        }
+
+        public class Solution
+        {
+            public IList<int> FindDisappearedNumbers(int[] nums)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    //get the index since numbers are 1 - N
+                    // do Math.Abs cause number could be seen before, hence it as marked negative
+                    var index = Math.Abs(nums[i]) - 1;
+                    if (nums[index] > 0)
+                    {
+                        nums[index] = -nums[index];
+                    }
+                }
+                // resp = all numbers that are not negative
+                var resp = new List<int>();
+                for (int i = 0; i < nums.Length; ++i)
+                {
+                    if (nums[i] > 0) resp.Add(i + 1);
+                }
+                return resp;
+            }
+        }
+    }
+}
---
+++ LeetCode448.FindAllNumbersDisappearedArray/LeetCode448.FindAllNumbersDisappearedArray/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LeetCode448.FindAllNumbersDisappearedArray")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode448.FindAllNumbersDisappearedArray")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ab263037-8eeb-4dc8-a9d3-68f72b7a25da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
