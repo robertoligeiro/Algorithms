94000b548fc35300e6d63e40c71ba8d7fefd4575
robertoligeiro <robertoligeiro@gmail.com>
2017-10-03 10:33:46 -0700

    LeetCode49.Group_Anagrams

---
+++ LeetCode49. Group Anagrams/LeetCode49. Group Anagrams/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode49.Group_Anagrams
+{
+    class Program
+    {
+        //https://leetcode.com/problems/group-anagrams/description/
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var r = s.GroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" });
+        }
+        public class Solution
+        {
+            public IList<IList<string>> GroupAnagrams(string[] strs)
+            {
+                var map = new Dictionary<string, List<string>>();
+                foreach (var s in strs)
+                {
+                    var sorted = new string(s.OrderBy(c => c).ToArray());
+                    List<string> l;
+                    if (!map.TryGetValue(sorted, out l))
+                    {
+                        map.Add(sorted, new List<string>() { s });
+                    }
+                    else l.Add(s);
+                }
+                var resp = new List<IList<string>>();
+                foreach (var l in map.Values)
+                {
+                    resp.Add(l);
+                }
+                return resp;
+            }
+        }
+    }
+}
