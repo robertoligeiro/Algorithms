5f72c834682149abb3e3467eb3fc9710c3389f62
robertoligeiro <robertoligeiro@gmail.com>
2017-10-04 09:14:09 -0700

    LeetCode477.Total_Hamming_Distance

---
+++ LeetCode477. Total Hamming Distance/LeetCode477. Total Hamming Distance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode477.Total_Hamming_Distance
+{
+    class Program
+    {
+        //https://leetcode.com/problems/total-hamming-distance/description/
+        //solution: https://leetcode.com/problems/total-hamming-distance/discuss/
+        static void Main(string[] args)
+        {
+        }
+
+        public class Solution
+        {
+            public int TotalHammingDistance(int[] nums)
+            {
+                var numsCount = nums.Length;
+                var hDist = 0;
+                for (int i = 0; i < 32; ++i)
+                {
+                    var totBits = 0;
+                    foreach (var num in nums)
+                    {
+                        totBits += (num >> i) & 1;
+                    }
+                    hDist += totBits * (numsCount - totBits);
+                }
+                return hDist;
+            }
+        }
+    }
+}
