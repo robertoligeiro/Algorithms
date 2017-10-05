9ac7b918281861de10b01a6b7d556673f7b75781
robertoligeiro <robertoligeiro@gmail.com>
2017-10-05 09:30:31 -0700

    fix LeetCode576.Out_of_Boundary_Paths

--- LeetCode576. Out of Boundary Paths/LeetCode576. Out of Boundary Paths/Program.cs
+++ LeetCode576. Out of Boundary Paths/LeetCode576. Out of Boundary Paths/Program.cs
@@ -13,29 +13,23 @@ namespace LeetCode576.Out_of_Boundary_Paths
         {
             var s = new Solution();
             //var r = s.FindPaths(2,2,2,0,0);
-            var r = s.FindPaths(1, 3, 3, 0, 1);
+            //var r = s.FindPaths(1, 3, 3, 0, 1);
+            var r = s.FindPaths(2, 3, 8, 1, 0);
         }
 
         public class Solution
         {
             public int FindPaths(int m, int n, int N, int i, int j)
             {
-                return FindPaths(m, n, N, i, j, new Dictionary<Tuple<int, int>, int>());
-            }
-
-            private int FindPaths(int m, int n, int N, int i, int j, Dictionary<Tuple<int, int>, int> memo)
-            {
                 if (i == m || j == n || i < 0 || j < 0) return 1;
                 if (N == 0) return 0;
                 var curr = new Tuple<int, int>(i,j);
                 var count = 0;
-                if (!memo.TryGetValue(curr, out count))
-                {
-                    count = FindPaths(m, n, N-1, i + 1, j, memo) + 
-                            FindPaths(m, n, N-1, i - 1, j, memo) + 
-                            FindPaths(m, n, N-1, i, j + 1, memo) + 
-                            FindPaths(m, n, N - 1, i, j - 1, memo);
-                }
+                count = FindPaths(m, n, N-1, i + 1, j) + 
+                        FindPaths(m, n, N-1, i - 1, j) + 
+                        FindPaths(m, n, N-1, i, j + 1) + 
+                        FindPaths(m, n, N - 1, i, j - 1);
+
                 return count;
             }
         }
