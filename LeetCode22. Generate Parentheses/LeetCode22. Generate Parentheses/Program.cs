72eb18184fa64edc43aa48d925b6f73cc67a7a31
robertoligeiro <robertoligeiro@gmail.com>
2017-10-07 22:12:55 -0700

    LeetCode22.Generate_Parentheses

---
+++ LeetCode22. Generate Parentheses/LeetCode22. Generate Parentheses/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode22.Generate_Parentheses
+{
+    class Program
+    {
+        //https://leetcode.com/problems/generate-parentheses/description/
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            var r = s.GenerateParenthesis(3);
+        }
+        public class Solution
+        {
+            public IList<string> GenerateParenthesis(int n)
+            {
+                var resp = new List<string>();
+                GenerateParenthesis(n, n, resp, string.Empty);
+                return resp;
+            }
+            private void GenerateParenthesis(int left, int right, List<string> resp, string parc)
+            {
+                if (left == 0 && right == 0)
+                {
+                    resp.Add(parc);
+                    return;
+                }
+
+                if (left > 0)
+                {
+                    GenerateParenthesis(left - 1, right, resp, parc + "(");
+                }
+                if (right > left)
+                {
+                    GenerateParenthesis(left, right - 1, resp, parc + ")");
+                }
+            }
+        }
+    }
+}
