d32de52cdc9f3a75ba509f07a3c93a6f1d09e95b
robertoligeiro <robertoligeiro@gmail.com>
2017-10-20 18:38:11 -0700

    441. Arranging Coins

---
+++ LeetCode441. Arranging Coins/LeetCode441. Arranging Coins/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode441.Arranging_Coins
+{
+	/https://leetcode.com/problems/arranging-coins/description/
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.ArrangeCoins(5);
+		}
+		public class Solution
+		{
+			public int ArrangeCoins(int n)
+			{
+				var count = 1;
+				var resp = 0;
+				while (n - count >= 0)
+				{
+					n -= (count++);
+					resp++;
+				}
+				return resp;
+			}
+		}
+	}
+}
