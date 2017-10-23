49e93416815e3462d685ca59fe6133dea51b8628
robertoligeiro <robertoligeiro@gmail.com>
2017-10-23 09:06:07 -0700

    LeetCode39.Combination_Sum

---
+++ InterviewBilloSnap/InterviewBilloSnap/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewBilloSnap
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var board = new int[,] { 
+				{ 1, 2, 2 },
+				{ 1, 3, 4 },
+				{ 2, 4, 4 }
+			};
+			var r = Solution.GetClusterGreatCount(board);
+		}
+
+		public class Solution
+		{
+			public static int GetClusterGreatCount(int[,] board)
+			{
+				var max = 0;
+				for (var r = 0; r < board.GetLength(0); ++r)
+				{
+					for (var c = 0; c < board.GetLength(1); ++c)
+					{
+						if (board[r, c] != -1)
+						{
+							max =  Math.Max(max, GetClusterCount(board, new Tuple<int, int>(r, c), board[r, c]));
+						}
+					}
+				}
+				return max;
+			}
+			public static int GetClusterCount(int[,] board, Tuple<int, int> curr, int val)
+			{
+				var clusterCount = 0;
+				var q = new Queue<Tuple<int, int>>();
+				var v = new HashSet<Tuple<int, int>>();
+				q.Enqueue(curr);
+				while (q.Count > 0)
+				{
+					curr = q.Dequeue();
+					if (v.Add(curr) && board[curr.Item1, curr.Item2] == val)
+					{
+						clusterCount++;
+						board[curr.Item1, curr.Item2] = -1;
+						GetAdj(board, curr, q);
+					}
+				}
+				return clusterCount;
+			}
+
+			public static void GetAdj(int[,] board, Tuple<int, int> curr, Queue<Tuple<int, int>> q)
+			{
+				var left = curr.Item2 - 1;
+				var right = curr.Item2 + 1;
+				var up = curr.Item1 - 1;
+				var down = curr.Item1 + 1;
+				if (left >= 0) q.Enqueue(new Tuple<int, int>(curr.Item1, left));
+				if (right < board.GetLength(1)) q.Enqueue(new Tuple<int, int>(curr.Item1, right));
+				if (up >= 0) q.Enqueue(new Tuple<int, int>(up, curr.Item2));
+				if (down < board.GetLength(0)) q.Enqueue(new Tuple<int, int>(down, curr.Item2));
+			}
+		}
+	}
+}
---
+++ LeetCode39. Combination Sum/LeetCode39. Combination Sum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode39.Combination_Sum
+{
+	class Program
+	{
+		//https://leetcode.com/problems/combination-sum/description/
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.CombinationSum(new int[] { 2,3,6,7}, 7);
+		}
+
+		public class Solution
+		{
+			public IList<IList<int>> CombinationSum(int[] candidates, int target)
+			{
+				var parcial = new List<int>();
+				var resp = new List<IList<int>>();
+				CombinationSum(candidates, target, parcial, resp, 0);
+				return resp;
+			}
+			private void CombinationSum(int[] candidates, int target, List<int> parcial, List<IList<int>> resp, int start)
+			{
+				if (target < 0) return;
+				if (target == 0)
+				{
+					resp.Add(new List<int>(parcial));
+					return;
+				}
+				for (int i = start; i < candidates.Length; ++i)
+				{
+					parcial.Add(candidates[i]);
+					CombinationSum(candidates, target - candidates[i], parcial, resp,i);
+					parcial.RemoveAt(parcial.Count - 1);
+				}
+			}
+		}
+	}
+}
