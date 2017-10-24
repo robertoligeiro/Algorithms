e90d994e64a7ae49a1a2f4a1ef08cbaeb56fe22d
robertoligeiro <robertoligeiro@gmail.com>
2017-10-23 19:56:49 -0700

    LeetCode674.LongestContinuousIncSubseq

---
+++ LeetCode674.LongestContinuousIncSubseq/LeetCode674.LongestContinuousIncSubseq/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode674.LongestContinuousIncSubseq
+{
+	class Program
+	{
+		//https://leetcode.com/problems/longest-continuous-increasing-subsequence/description/
+		static void Main(string[] args)
+		{
+		}
+		public class Solution
+		{
+			public int FindLengthOfLCIS(int[] nums)
+			{
+				if (nums == null || nums.Length == 0) return 0;
+				var count = 1;
+				var maxCount = 1;
+				for (int i = 1; i < nums.Length; ++i)
+				{
+					if (nums[i] > nums[i - 1]) count++;
+					else
+					{
+						maxCount = Math.Max(maxCount, count);
+						count = 1;
+					}
+				}
+				return Math.Max(maxCount, count);
+			}
+		}
+	}
+}
