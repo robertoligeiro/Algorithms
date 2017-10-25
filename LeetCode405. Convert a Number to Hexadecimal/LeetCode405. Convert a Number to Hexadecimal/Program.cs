d4ca6b259f2d179e5d9c596d661a26106d2ace74
robertoligeiro <robertoligeiro@gmail.com>
2017-10-24 20:46:26 -0700

    LeetCode405.Convert_a_Number_to_Hexadecimal

---
+++ LeetCode405. Convert a Number to Hexadecimal/LeetCode405. Convert a Number to Hexadecimal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode405.Convert_a_Number_to_Hexadecimal
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.ToHex(-1);
+		}
+
+		public class Solution
+		{
+			public string ToHex(int num)
+			{
+				if (num == 0) return "0";
+				var hex = "0123456789abcdef";
+				long lnum = num;
+				if (num < 0) lnum = long.MaxValue + num + 1;
+				var resp = string.Empty;
+				var count = 0;
+				while (lnum > 0 && count++ < 8)
+				{
+					resp = hex[((int)lnum & 0xf)] + resp;
+					lnum >>= 4;
+				}
+				return resp;
+			}
+		}
+	}
+}
