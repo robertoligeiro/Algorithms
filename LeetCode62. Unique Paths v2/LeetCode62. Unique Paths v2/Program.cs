4fa6fbb84c2c60910e2e00444e82637e60b218bc
robertoligeiro <robertoligeiro@gmail.com>
2017-10-26 19:31:59 -0700

    LeetCode62.Unique_Paths_v2

---
+++ LeetCode62. Unique Paths v2/LeetCode62. Unique Paths v2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode62.Unique_Paths_v2
+{
+	//https://leetcode.com/problems/unique-paths/description/
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.UniquePaths(3,3);
+		}
+
+		class Solution
+		{
+			public int UniquePaths(int m, int n)
+			{
+				var memo = new int[m, n];
+				return UniquePaths(0, 0, m, n, memo);
+			}
+			private int UniquePaths(int r, int c, int m, int n, int[,] memo)
+			{
+				if (r >= m || c >= n) return 0;
+				if (r == m-1 && c == n-1) return 1;
+				if (memo[r, c] != 0) return memo[r, c];
+				var count = UniquePaths(r + 1, c, m, n, memo);
+				count += UniquePaths(r, c + 1, m, n, memo);
+				memo[r, c] = count;
+				return count;
+			}
+		}
+	}
+}
