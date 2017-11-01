b41e370f677b4d0817af1866044bcee7ac1d4239
robertoligeiro <robertoligeiro@gmail.com>
2017-10-31 21:09:24 -0700

    LeetCode116.PopulatingNextRightPointersNode

---
+++ LeetCode116.PopulatingNextRightPointersNode/LeetCode116.PopulatingNextRightPointersNode/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode116.PopulatingNextRightPointersNode
+{
+	class Program
+	{
+		//https://leetcode.com/problems/populating-next-right-pointers-in-each-node/description/
+		// doesn't have c# option, did in c++ in leetcode.
+		static void Main(string[] args)
+		{
+		}
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode next;
+			public TreeNode(int x) { val = x; }
+		}
+
+		public class Solution
+		{
+			public void Connect(TreeNode root)
+			{
+				if (root == null) return;
+				var curr = root;
+				while (curr != null)
+				{
+					if (curr.left == null) break;
+					curr.left.next = curr.right;
+					if (curr.next != null)
+					{
+						curr.right.next = curr.next.left;
+					}
+					curr = curr.next;
+				}
+				Connect(curr.left);
+			}
+		}
+	}
+}
