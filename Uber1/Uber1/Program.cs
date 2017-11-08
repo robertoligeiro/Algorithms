c9193fe1836b6e00b3dcc3b4ca070064250c3913
robertoligeiro <robertoligeiro@gmail.com>
2017-11-07 21:28:16 -0800

    uber interview loop 1

---
+++ Uber1/Uber1/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uber1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var game = new MineGame(5, 6);
+			Console.ReadKey();
+			game.PrintBoard(true);
+			Console.ReadKey();
+			game.Open(1,1);
+			Console.ReadKey();
+			game.Open(2, 0);
+			Console.ReadKey();
+		}
+
+		public class MineGame
+		{
+			private int[,] board;
+			private bool[,] boardOutput;
+
+			public MineGame(int size, int mineCount)
+			{
+				board = new int[size, size];
+				boardOutput = new bool[size, size];
+				var rand = new Random(1);
+				while (mineCount > 0)
+				{
+					var r = rand.Next(0, size);
+					var c = rand.Next(0, size);
+					if (board[r, c] != -1)
+					{
+						board[r, c] = -1;
+						mineCount--;
+					}
+				}
+
+				SetNumbers(this.board);
+				PrintBoard(false);
+			}
+
+			public void Open(int r, int c)
+			{
+				Console.WriteLine();
+				if (this.board[r, c] == -1)
+				{
+					Console.WriteLine("GameOver");
+					return;
+				}
+
+
+				var q = new Queue<Tuple<int, int>>();
+				var visited = new HashSet<Tuple<int, int>>();
+				this.boardOutput[r, c] = true;
+				GetNeibouhs(r, c, q);
+
+				while (q.Count > 0)
+				{
+					var curr = q.Dequeue();
+					if (visited.Add(curr))
+					{
+						if (this.board[curr.Item1, curr.Item2] != -1)
+						{
+							this.boardOutput[curr.Item1, curr.Item2] = true;
+						}
+						if (this.board[curr.Item1, curr.Item2] == 0)
+						{
+							this.GetNeibouhs(curr.Item1, curr.Item2, q);
+						}
+					}
+				}
+
+				PrintBoard(true);
+			}
+
+			private void GetNeibouhs(int r, int c, Queue<Tuple<int,int>> q)
+			{
+				for (int cR = r - 1; cR <= (r + 1); ++cR)
+				{
+					for (int cC = c - 1; cC <= (c + 1); ++cC)
+					{
+						if ((cR >= 0 && cR < board.GetLength(0)) && (cC >= 0 && cC < board.GetLength(1)))
+						{
+							q.Enqueue(new Tuple<int, int>(cR, cC));
+						}
+					}
+				}
+			}
+			private void SetNumbers(int[,] board)
+			{
+				for (int r = 0; r < board.GetLength(0); ++r)
+				{
+					for (int c = 0; c < board.GetLength(1); ++c)
+					{
+						if (board[r, c] == -1) continue;
+						var countMines = 0;
+						for (int cR = r - 1; cR <= (r+1); ++cR)
+						{
+							for (int cC = c - 1; cC <= (c + 1); ++cC)
+							{
+								if ((cR >= 0 && cR < board.GetLength(0)) && (cC >= 0 && cC < board.GetLength(1)))
+								{
+									if (board[cR, cC] == -1) countMines++;
+								}
+							}
+						}
+
+						board[r, c] = countMines;
+					}
+				}
+			}
+
+			public void PrintBoard(bool printQuestionMark)
+			{
+				for (int r = 0; r < board.GetLength(0); ++r)
+				{
+					var col = string.Empty;
+					for (int c = 0; c < board.GetLength(1); ++c)
+					{
+						if (!printQuestionMark)
+						{
+							if (board[r, c] == -1)
+							{
+								col += ("* ");
+							}
+							else
+								col += (board[r, c] + " ");
+						}
+						else
+						{
+							if (this.boardOutput[r, c])
+							{
+								col += (board[r, c] + " ");
+							}else col += ("? ");
+						}
+					}
+					Console.WriteLine(col);
+				}
+			}
+		}
+	}
+}
---
+++ Uber2/Uber2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uber2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var quadTree = new QuadTree();
+			quadTree.Add(1,1);
+			quadTree.Add(1, 6);
+			quadTree.Add(6, 1);
+			quadTree.Add(6, 6);
+			quadTree.Add(7, 7);
+		}
+
+		public class QuadTreeNode
+		{
+			public int startX;
+			public int startY;
+			public int endX;
+			public int endY;
+			public Tuple<int, int> value;
+			public List<QuadTreeNode> childs = new List<QuadTreeNode>();
+		}
+		public class QuadTree
+		{
+			public int endX = 10;
+			public int endY = 10;
+			public QuadTreeNode root = new QuadTreeNode() { startX = 0, startY = 0, endX = 10, endY = 10};
+
+			public void Add(int x, int y)
+			{
+				Add(x,y, this.root);
+			}
+
+			private void Add(int x, int y, QuadTreeNode q)
+			{
+				if (q.value == null && q.childs.Count == 0)
+				{
+					q.value = new Tuple<int, int>(x, y);
+				}
+				else
+				{
+					var nodeId = 0;
+					if (q.childs.Count == 0)
+					{
+						var halfX = (q.endX - q.startX) / 2;
+						var halfY = (q.endY - q.startY) / 2;
+						var q0 = new QuadTreeNode() { startX = q.startX, startY = q.startY, endX = halfX, endY = halfY };
+						var q1 = new QuadTreeNode() { startX = q.startX, startY = halfY, endX = halfX, endY = q.endY };
+						var q2 = new QuadTreeNode() { startX = halfX, startY = q.startY, endX = q.endX, endY = halfY };
+						var q3 = new QuadTreeNode() { startX = halfX, startY = halfY, endX = q.endX, endY = q.endY };
+						q.childs.Add(q0);
+						q.childs.Add(q1);
+						q.childs.Add(q2);
+						q.childs.Add(q3);
+						nodeId = FindQuadNode(q.value.Item1, q.value.Item2, q);
+						q.childs[nodeId].value = new Tuple<int, int>(q.value.Item1, q.value.Item2);
+						q.value = null;
+					}
+					nodeId = FindQuadNode(x, y, q);
+					Add(x, y, q.childs[nodeId]);
+				}
+			}
+
+			private int FindQuadNode(int x, int y, QuadTreeNode q)
+			{
+				var halfX = (q.endX - q.startX) / 2;
+				var halfY = (q.endY - q.startY) / 2;
+				if (x <= halfX && y <= halfY) return 0;
+				if (x <= halfX && y > halfY) return 1;
+				if (x > halfX && y <= halfY) return 2;
+				if (x > halfX && y > halfY) return 3;
+				return -1;
+			}
+		}
+	}
+}
