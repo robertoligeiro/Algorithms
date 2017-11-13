a307aea7a1e89b0f0209cb1cd634744b04796503
robertoligeiro <robertoligeiro@gmail.com>
2017-11-13 09:20:46 -0800

    LeetCode69.Sqrt_x_

---
+++ LeetCode69. Sqrt(x)/LeetCode69. Sqrt(x)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode69.Sqrt_x_
+{
+	//https://leetcode.com/problems/sqrtx/description/
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			//var r = s.MySqrt(2147395599);
+			var r = s.MySqrt(1);
+		}
+
+		public class Solution
+		{
+			public int MySqrt(int x)
+			{
+				if (x == 1) return 1;
+				long l = 1;
+				long r = x/2;
+				while (l <= r)
+				{
+					long mid = l + (r - l) / 2;
+					long sqr = mid * mid;
+					if (sqr == x) return (int)mid;
+					if (sqr < x) l = mid + 1;
+					else r = mid - 1;
+				}
+				return (int)r;
+			}
+		}
+	}
+}
---
+++ LeetCode69. Sqrt(x)/LeetCode69. Sqrt(x)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LeetCode69. Sqrt(x)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode69. Sqrt(x)")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f9bc0d2c-5ad2-403d-9fd4-d789f6117f34")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
