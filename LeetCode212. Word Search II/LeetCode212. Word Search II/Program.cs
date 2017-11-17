c981b51bbfebba78e4de8ea7074f128c49c0ef5b
robertoligeiro <robertoligeiro@gmail.com>
2017-11-16 20:29:20 -0800

    LeetCode212.Word_Search_II

--- LeetCode212. Word Search II/LeetCode212. Word Search II/Program.cs
+++ LeetCode212. Word Search II/LeetCode212. Word Search II/Program.cs
@@ -11,103 +11,142 @@ namespace LeetCode212.Word_Search_II
         //https://leetcode.com/problems/word-search-ii/description/
         static void Main(string[] args)
         {
-            var s = new Solution();
-            var b = new char[,]
-            {
-              {'o','a','a','n'},
-              {'e','t','a','e'},
-              {'i','h','k','r'},
-              {'i','f','l','v'}
-            };
-            var r = s.FindWords(b, new string[] { "oath", "pea", "eat", "rain" });
-            //var b = new char[,] { { 'a', 'b' }, { 'c', 'd' } };
-            //var r = s.FindWords(b, new string[] { "ab", "cb", "ad", "bd", "ac", "ca", "da", "bc", "db", "adcb", "dabc", "abb", "acb" });
+			//var s = new Solution1();
+			//var b = new char[,]
+			//{
+			//  {'o','a','a','n'},
+			//  {'e','t','a','e'},
+			//  {'i','h','k','r'},
+			//  {'i','f','l','v'}
+			//};
+			//var r = s.FindWords(b, new string[] { "oath", "pea", "eat", "rain" });
+			//var b = new char[,] { { 'a', 'b' }, { 'c', 'd' } };
+			//var r = s.FindWords(b, new string[] { "ab", "cb", "ad", "bd", "ac", "ca", "da", "bc", "db", "adcb", "dabc", "abb", "acb" });
 
-            //var b = new char[,] { { 'a' } };
-            //var r = s.FindWords(b, new string[] { "a" });
-        }
+			//var b = new char[,] { { 'a' } };
+			//var r = s.FindWords(b, new string[] { "a" });
+			var s = new Solution();
+			var b = new char[,]
+			{
+			  {'a','b'},
+			  {'a','a'},
+			};
+			var r = s.FindWords(b, new string[] { "aba", "baa", "bab", "aaab", "aaa", "aaaa", "aaba" });
+		}
 
-        public class Solution
-        {
-            public IList<string> FindWords(char[,] board, string[] words)
-            {
-                var m = new Dictionary<char, HashSet<string>>();
-                foreach (var w in words)
-                {
-                    HashSet<string> l;
-                    if (m.TryGetValue(w[0], out l))
-                    {
-                        l.Add(w);
-                    }
-                    else
-                    {
-                        m[w[0]] = new HashSet<string>() { w };
-                    }
-                }
+		public class TrieNode
+		{
+			public bool isEnd = false;
+			public Dictionary<char, TrieNode> map = new Dictionary<char, TrieNode>();
+		}
 
-                var resp = new HashSet<string>();
-                for (int row = 0; row < board.GetLength(0); ++row)
-                {
-                    for (int col = 0; col < board.GetLength(1); ++col)
-                    {
-                        HashSet<string> lWords;
-                        if (m.TryGetValue(board[row, col], out lWords))
-                        {
-                            var word = new StringBuilder();
-                            word.Append(board[row, col]);
-                            var t = new Tuple<int, int>(row, col);
-                            FindWord(board, word, t, new HashSet<Tuple<int, int>>() { t }, lWords, resp);
-                        }
-                    }
-                }
-                return resp.ToList();
-            }
+		public class Trie
+		{
+			private TrieNode root = new TrieNode();
 
-            private bool FindWord(char[,] board, StringBuilder parc, Tuple<int, int> pos, HashSet<Tuple<int, int>> visited, HashSet<string> words, HashSet<string> found)
-            {
-                if (parc.Length > 0 && words.Contains(parc.ToString()))
-                {
-                    found.Add(parc.ToString());
-                }
+			public void AddWord(string w)
+			{
+				var curr = this.root;
+				foreach (var c in w)
+				{
+					TrieNode next;
+					if (!curr.map.TryGetValue(c, out next))
+					{
+						next = new TrieNode();
+						curr.map.Add(c, next);
+					}
+					curr = next;
+				}
+				curr.isEnd = true;
+			}
 
-                var adj = GetAdj(board, pos);
-                foreach (var t in adj)
-                {
-                    if (visited.Add(t))
-                    {
-                        parc.Append(board[t.Item1, t.Item2]);
-                        if (WordsPrefixCheck(words, parc.ToString()))
-                        {
-                            FindWord(board, parc, t, visited, words, found);
-                        }
-                        parc.Remove(parc.Length - 1, 1);
-                        visited.Remove(t);
-                    }
-                }
-                return false;
-            }
+			public bool HasPrefix(List<char> parc, ref bool isEnd)
+			{
+				var curr = this.root;
+				foreach (var c in parc)
+				{
+					TrieNode next;
+					if (!curr.map.TryGetValue(c, out next)) return false;
+					curr = next;
+				}
+				isEnd = curr.isEnd;
+				return true;
+			}
+		}
+		public class Solution
+		{
+			private Trie trie = new Trie();
+			public IList<string> FindWords(char[,] board, string[] words)
+			{
+				var m = new Dictionary<char, int>();
+				var hWords = new HashSet<string>();
+				
+				foreach (var w in words)
+				{
+					hWords.Add(w);
+					trie.AddWord(w);
+					var size = 0;
+					if (m.TryGetValue(w[0], out size))
+					{
+						m[w[0]] = Math.Max(size, w.Length);
+					}
+					else
+					{
+						m.Add(w[0], w.Length);
+					}
+				}
 
-            private bool WordsPrefixCheck(HashSet<string> words, string prefix)
-            {
-                foreach (var w in words)
-                {
-                    if (w.StartsWith(prefix)) return true;
-                }
-                return false;
-            }
-            private List<Tuple<int, int>> GetAdj(char[,] board, Tuple<int, int> pos)
-            {
-                var up = pos.Item1 - 1;
-                var down = pos.Item1 + 1;
-                var left = pos.Item2 - 1;
-                var right = pos.Item2 + 1;
-                var resp = new List<Tuple<int, int>>();
-                if (up >= 0) resp.Add(new Tuple<int, int>(up, pos.Item2));
-                if (down < board.GetLength(0)) resp.Add(new Tuple<int, int>(down, pos.Item2));
-                if (left >= 0) resp.Add(new Tuple<int, int>(pos.Item1, left));
-                if (right < board.GetLength(1)) resp.Add(new Tuple<int, int>(pos.Item1, right));
-                return resp;
-            }
-        }
+				var resp = new HashSet<string>();
+				for (int row = 0; row < board.GetLength(0); ++row)
+				{
+					for (int col = 0; col < board.GetLength(1); ++col)
+					{
+						if (m.ContainsKey(board[row, col]))
+						{
+							var p = new Tuple<int, int>(row, col);
+							var v = new HashSet<Tuple<int, int>>() { p };
+							FindWord(board, hWords, p, v, m[board[row, col]], new List<char>() { board[row, col] }, resp);
+						}
+					}
+				}
+
+				return resp.ToList();
+			}
+
+			private void FindWord(char[,] board, HashSet<string> words, Tuple<int, int> pos, HashSet<Tuple<int, int>> visited, int maxSize, List<char> parc, HashSet<string> found)
+			{
+				var isEnd = false;
+				if (parc.Count > maxSize || !this.trie.HasPrefix(parc, ref isEnd)) return;
+				if (isEnd)
+				{
+					found.Add(new string(parc.ToArray()));
+				}
+				var adj = GetAdj(board, pos);
+				foreach (var a in adj)
+				{
+					if (visited.Add(a))
+					{
+						parc.Add(board[a.Item1, a.Item2]);
+						FindWord(board, words, a, visited, maxSize, parc, found);
+						parc.RemoveAt(parc.Count - 1);
+						visited.Remove(a);
+					}
+				}
+			}
+
+			private List<Tuple<int, int>> GetAdj(char[,] board, Tuple<int, int> pos)
+			{
+				var up = pos.Item1 - 1;
+				var down = pos.Item1 + 1;
+				var left = pos.Item2 - 1;
+				var right = pos.Item2 + 1;
+				var resp = new List<Tuple<int, int>>();
+				if (up >= 0) resp.Add(new Tuple<int, int>(up, pos.Item2));
+				if (down < board.GetLength(0)) resp.Add(new Tuple<int, int>(down, pos.Item2));
+				if (left >= 0) resp.Add(new Tuple<int, int>(pos.Item1, left));
+				if (right < board.GetLength(1)) resp.Add(new Tuple<int, int>(pos.Item1, right));
+				return resp;
+			}
+		}
     }
 }
--- LeetCode669. Trim a Binary Search Tree/LeetCode669. Trim a Binary Search Tree/Program.cs
+++ LeetCode669. Trim a Binary Search Tree/LeetCode669. Trim a Binary Search Tree/Program.cs
@@ -28,6 +28,8 @@ namespace LeetCode669.Trim_a_Binary_Search_Tree
 			public TreeNode TrimBST(TreeNode root, int L, int R)
 			{
 				if (root == null) return null;
+				if (root.val < L) return TrimBST(root.right, L, R);
+				if (root.val > R) return TrimBST(root.left, L, R);
 				root.left = TrimBST(root.left, L, R);
 				root.right = TrimBST(root.right, L, R);
 				if (root.val < L || root.val > R)
