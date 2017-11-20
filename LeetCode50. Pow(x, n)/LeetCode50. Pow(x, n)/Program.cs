59669e3580a11af3bf0172278330e207305a2bfb
robertoligeiro <robertoligeiro@gmail.com>
2017-11-20 09:39:33 -0800

    LeetCode50.Pow_x__n_

---
+++ LeetCode50. Pow(x, n)/LeetCode50. Pow(x, n)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode50.Pow_x__n_
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.MyPow(2, -2147483648);
+		}
+		public class Solution
+		{
+			public double MyPow(double x, int n)
+			{
+				if (x == 1) return 1;
+				if (x == -1 && n == int.MinValue) return 1;
+				if (n == int.MinValue) return 0;
+				var isNeg = false;
+				if (n < 0)
+				{
+					isNeg = true;
+					n = -n;
+				}
+				var ret = MyPowHelper(x, n);
+				if (isNeg) return 1 / ret;
+				return ret;
+			}
+			public double MyPowHelper(double x, int n)
+			{
+				if (n == 0) return 1;
+				var ret = MyPow(x, n / 2);
+				if (n % 2 == 0) return ret * ret;
+				return x * ret * ret;
+			}
+		}
+	}
+}
