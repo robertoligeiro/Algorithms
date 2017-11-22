a6c0f83e41dc943862cd3e6e370cb010d1e533fd
robertoligeiro <robertoligeiro@gmail.com>
2017-11-21 20:28:31 -0800

    LeetCode686.Repeated_String_Match

---
+++ LeetCode686. Repeated String Match/LeetCode686. Repeated String Match/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode686.Repeated_String_Match
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+		public class Solution
+		{
+			public int RepeatedStringMatch(string A, string B)
+			{
+				var sb = new StringBuilder(;
+				int resp = 0;
+				for (; sb.Length < B.Length; ++resp)
+				{
+					sb.Append(A);
+				}
+				if (sb.ToString().IndexOf(B) >= 0) return resp;
+				sb.Append(A);
+				if (sb.ToString().IndexOf(B) >= 0) return ++resp;
+				return -1;
+			}
+		}
+	}
+}
