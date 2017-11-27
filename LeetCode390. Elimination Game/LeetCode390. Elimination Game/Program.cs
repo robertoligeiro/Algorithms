169e18ba75a913526dabf25521292817dd75a760
robertoligeiro <robertoligeiro@gmail.com>
2017-11-27 09:18:35 -0800

    LeetCode390.Elimination_Game

---
+++ LeetCode390. Elimination Game/LeetCode390. Elimination Game/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode390.Elimination_Game
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.LastRemaining(9);
+		}
+
+		public class Solution
+		{
+			public int LastRemaining(int n)
+			{
+				return LeftToRight(n);
+			}
+
+			private int LeftToRight(int n)
+			{
+				if (n <= 2) return n;
+				return 2 * RightToLeft(n / 2);
+			}
+
+			private int RightToLeft(int n)
+			{
+				if (n <= 2) return 1;
+				if (n % 2 == 1) return 2 * LeftToRight(n / 2);
+				return 2 * LeftToRight(n / 2) - 1;
+			}
+		}
+	}
+}
