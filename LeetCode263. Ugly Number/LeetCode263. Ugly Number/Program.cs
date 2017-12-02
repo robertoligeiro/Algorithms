0fcaf5ac719f1aa0c153cbc041d4a84dac0341d8
robertoligeiro <robertoligeiro@gmail.com>
2017-12-01 23:47:48 -0800

    LeetCode263.Ugly_Number

---
+++ LeetCode263. Ugly Number/LeetCode263. Ugly Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode263.Ugly_Number
+{
+	//https://leetcode.com/submissions/detail/130455218/
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+		public class Solution
+		{
+			public bool IsUgly(int num)
+			{
+				if (num <= 0) return false;
+				if (num == 1) return true;
+				if (num % 2 == 0) return IsUgly(num / 2);
+				if (num % 3 == 0) return IsUgly(num / 3);
+				if (num % 5 == 0) return IsUgly(num / 5);
+				return false;
+			}
+		}
+	}
+}
