5eaa83ea1108555ee6166400141d602b31b29005
robertoligeiro <robertoligeiro@gmail.com>
2017-12-04 19:52:58 -0800

    LeetCode295.Find_Median_from_Data_Stream

--- LeetCode295. Find Median from Data Stream/LeetCode295. Find Median from Data Stream/Program.cs
+++ LeetCode295. Find Median from Data Stream/LeetCode295. Find Median from Data Stream/Program.cs
@@ -22,48 +22,189 @@ namespace LeetCode295.Find_Median_from_Data_Stream
 			//r = m.FindMedian();
 			//m.AddNum(4);
 			//r = m.FindMedian();
-			m.AddNum(12);
-			r = m.FindMedian();
-			m.AddNum(10);
-			r = m.FindMedian();
-			m.AddNum(13);
-			r = m.FindMedian();
-			m.AddNum(11);
-			r = m.FindMedian();
-			m.AddNum(5);
-			r = m.FindMedian();
-			m.AddNum(15);
-			r = m.FindMedian();
-			m.AddNum(1);
-			r = m.FindMedian();
-			m.AddNum(11);
-			r = m.FindMedian();
-			m.AddNum(6);
-			r = m.FindMedian();
-			m.AddNum(17);
-			r = m.FindMedian();
+
+			//m.AddNum(12);
+			//r = m.FindMedian();
+			//m.AddNum(10);
+			//r = m.FindMedian();
+			//m.AddNum(13);
+			//r = m.FindMedian();
+			//m.AddNum(11);
+			//r = m.FindMedian();
+			//m.AddNum(5);
+			//r = m.FindMedian();
+			//m.AddNum(15);
+			//r = m.FindMedian();
+			//m.AddNum(1);
+			//r = m.FindMedian();
+			//m.AddNum(11);
+			//r = m.FindMedian();
+			//m.AddNum(6);
+			//r = m.FindMedian();
+			//m.AddNum(17);
+			//r = m.FindMedian();
+			//m.AddNum(14);
+			//r = m.FindMedian();
+			//m.AddNum(8);
+			//r = m.FindMedian();
+			//m.AddNum(17);
+			//r = m.FindMedian();
+			//m.AddNum(6);
+			//r = m.FindMedian();
+			//m.AddNum(4);
+			//r = m.FindMedian();
+			//m.AddNum(16);
+			//r = m.FindMedian();
+			//m.AddNum(8);
+			//r = m.FindMedian();
+			//m.AddNum(10);
+			//r = m.FindMedian();
+			//m.AddNum(2);
+			//r = m.FindMedian();
+			//m.AddNum(12);
+			//r = m.FindMedian();
+			//m.AddNum(0);
+			//r = m.FindMedian();
+
+			m.AddNum(78);
 			m.AddNum(14);
-			r = m.FindMedian();
+			m.AddNum(50);
+			m.AddNum(20);
+			m.AddNum(13);
+			m.AddNum(9);
+			m.AddNum(25);
 			m.AddNum(8);
-			r = m.FindMedian();
-			m.AddNum(17);
-			r = m.FindMedian();
+			m.AddNum(13);
+			m.AddNum(37);
+			m.AddNum(29);
+			m.AddNum(33);
+			m.AddNum(55);
+			m.AddNum(52);
 			m.AddNum(6);
-			r = m.FindMedian();
-			m.AddNum(4);
-			r = m.FindMedian();
-			m.AddNum(16);
-			r = m.FindMedian();
-			m.AddNum(8);
-			r = m.FindMedian();
-			m.AddNum(10);
-			r = m.FindMedian();
-			m.AddNum(2);
-			r = m.FindMedian();
+			m.AddNum(17);
+			m.AddNum(65);
+			m.AddNum(23);
+			m.AddNum(74);
+			m.AddNum(43);
+			m.AddNum(5);
+			m.AddNum(29);
+			m.AddNum(29);
+			m.AddNum(72);
+			m.AddNum(7);
+			m.AddNum(13);
+			m.AddNum(56);
+			m.AddNum(21);
+			m.AddNum(31);
+			m.AddNum(66);
+			m.AddNum(69);
+			m.AddNum(69);
+			m.AddNum(74);
 			m.AddNum(12);
-			r = m.FindMedian();
+			m.AddNum(77);
+			m.AddNum(23);
+			m.AddNum(10);
+			m.AddNum(6);
+			m.AddNum(27);
+			m.AddNum(63);
+			m.AddNum(77);
+			m.AddNum(21);
+			m.AddNum(40);
+			m.AddNum(10);
+			m.AddNum(19);
+			m.AddNum(59);
+			m.AddNum(35);
+			m.AddNum(40);
+			m.AddNum(44);
+			m.AddNum(4);
+			m.AddNum(15);
+			m.AddNum(29);
+			m.AddNum(63);
+			m.AddNum(27);
+			m.AddNum(46);
+			m.AddNum(56);
 			m.AddNum(0);
-			r = m.FindMedian();
+			m.AddNum(60);
+			m.AddNum(72);
+			m.AddNum(35);
+			m.AddNum(54);
+			m.AddNum(50);
+			m.AddNum(14);
+			m.AddNum(29);
+			m.AddNum(62);
+			m.AddNum(24);
+			m.AddNum(18);
+			m.AddNum(79);
+			m.AddNum(16);
+			m.AddNum(19);
+			m.AddNum(8);
+			m.AddNum(77);
+			m.AddNum(10);
+			m.AddNum(21);
+			m.AddNum(66);
+			m.AddNum(42);
+			m.AddNum(76);
+			m.AddNum(14);
+			m.AddNum(58);
+			m.AddNum(20);
+			m.AddNum(0);
+		}
+	}
+	public class MedianFinder
+	{
+		private LinkedList<int> items = new LinkedList<int>();
+
+		public void AddNum(int num)
+		{
+			if (items.Count == 0 || num < items.First.Value)
+			{
+				items.AddFirst(num);
+			}
+			else if (num > items.Last.Value)
+			{
+				items.AddLast(num);
+			}
+			else
+			{
+				var l = 0;
+				var r = items.Count - 1;
+				while (l <= r)
+				{
+					var mid = l + (r - l) / 2;
+					if (items.ElementAt(mid) <= num)
+					{
+						l = mid + 1;
+					}
+					else r = mid - 1;
+				}
+
+				var el = items.Find(items.ElementAt(r));
+				var val = items.ElementAt(r);
+
+				for (; el.Next != null && el.Value == el.Next.Value;)
+				{
+					el = el.Next;
+				}
+				items.AddAfter(el, num);
+
+				//for (var it = items.First; it != null; it = it.Next)
+				//{
+				//	if (it.Value >= num)
+				//	{
+				//		items.AddBefore(it, num);
+				//		break;
+				//	}
+				//}
+			}
+		}
+
+		public double FindMedian()
+		{
+			var mid = items.Count / 2;
+			if (items.Count % 2 != 0)
+			{
+				return items.ElementAt(mid);
+			}
+			return (double)(items.ElementAt(mid) + items.ElementAt(mid - 1)) / 2;
 		}
 	}
 	public class MedianItem
@@ -108,7 +249,7 @@ namespace LeetCode295.Find_Median_from_Data_Stream
 			return ret.Key;
 		}
 	}
-	public class MedianFinder
+	public class MedianFinder2
 	{
 		private MedianItem maxItems = new MedianItem();
 		private MedianItem minItems = new MedianItem();
@@ -153,24 +294,4 @@ namespace LeetCode295.Find_Median_from_Data_Stream
 			return (double)(maxItems.Min() + minItems.Max()) / 2;
 		}
 	}
-
-	//simple, just using sort, also time exceeds even before the solution above.
-	//public class MedianFinder
-	//{
-	//	private List<int> items = new List<int>();
-	//	private int count = 0;
-	//	public void AddNum(int num)
-	//	{
-	//		count++;
-	//		items.Add(num);
-	//		items.Sort();
-	//	}
-
-	//	public double FindMedian()
-	//	{
-	//		var mid = (items.Count - 1)/2;
-	//		if ((items.Count - 1) % 2 == 0) return items[mid];
-	//		return (double)(items[mid] + items[mid + 1]) / 2;
-	//	}
-	//}
 }
---
+++ MinMaxHeap/MinMaxHeap/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMaxHeap
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var heap = new PriorityQueue(false);
+			heap.AddNum(5);
+			heap.AddNum(15);
+			heap.AddNum(51);
+			heap.AddNum(2);
+			heap.AddNum(4);
+			heap.AddNum(6);
+			while (heap.items.Count > 0)
+			{
+				var he = heap.GetTop();
+			}
+		}
+		public class PriorityQueue
+		{
+			public List<int> items = new List<int>();
+			public bool isMin;
+			public PriorityQueue(bool isMin)
+			{
+				this.isMin = isMin;
+			}
+			public void AddNum(int num)
+			{
+				items.Add(num);
+				var curr = items.Count - 1;
+				var parent = curr / 2;
+				while (parent >= 0)
+				{
+					if (curr != 0 && curr % 2 == 0) parent--;
+					if ((this.isMin && items[curr] < items[parent]) ||
+						(!this.isMin && items[curr] > items[parent]))
+					{
+						var t = items[curr];
+						items[curr] = items[parent];
+						items[parent] = t;
+						curr = parent;
+						parent = curr / 2;
+					}
+					else break;
+				}
+			}
+			public int GetTop()
+			{
+				var ret = items[0];
+				items[0] = items[items.Count - 1];
+				items.RemoveAt(items.Count - 1);
+				var parent = 0;
+				var curr = 1;
+				while (curr < items.Count)
+				{
+					if (curr + 1 < items.Count &&
+						((this.isMin && items[curr] > items[curr + 1]) ||
+						(!this.isMin && items[curr] < items[curr + 1])))
+					{
+						++curr;
+					}
+					if ((this.isMin && items[curr] < items[parent]) ||
+						(!this.isMin && items[curr] > items[parent]))
+					{
+						var t = items[parent];
+						items[parent] = items[curr];
+						items[curr] = t;
+						parent = curr;
+						curr = (curr * 2) + 1;
+					}
+					else break;
+				}
+				return ret;
+			}
+		}
+	}
+}
