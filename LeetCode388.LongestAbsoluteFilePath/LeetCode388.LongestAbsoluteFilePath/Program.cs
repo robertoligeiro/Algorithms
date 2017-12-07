50fde34f474742b7fbc21a0b3d3abab12c3a8fd1
robertoligeiro <robertoligeiro@gmail.com>
2017-12-06 20:39:37 -0800

    LeetCode388.LongestAbsoluteFilePath

--- LeetCode388.LongestAbsoluteFilePath/LeetCode388.LongestAbsoluteFilePath/Program.cs
+++ LeetCode388.LongestAbsoluteFilePath/LeetCode388.LongestAbsoluteFilePath/Program.cs
@@ -6,15 +6,47 @@ using System.Threading.Tasks;
 
 namespace LeetCode388.LongestAbsoluteFilePath
 {
-    class Program
+	//https://leetcode.com/problems/longest-absolute-file-path/description/
+	class Program
     {
         static void Main(string[] args)
         {
             var s = new Solution();
             //var r = s.LengthLongestPath("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext");
             var r = s.LengthLongestPath("a.txt");
-        }
-        public class Solution
+			//var r = s.LengthLongestPath("dir\n        file.txt");
+		}
+
+		public class Solution
+		{
+			public int LengthLongestPath(string input)
+			{
+				var chuncks = input.Split('\n');
+				var maxSoFar = chuncks[0].IndexOf('.') > 0 ? chuncks[0].Length : 0;
+				var path = new List<string>();
+				path.Add(chuncks[0]);
+				for (int i = 1; i < chuncks.Length; ++i)
+				{
+					var chunck = chuncks[i];
+					int localLevel = chunck.LastIndexOf('\t')+1;
+					chunck = chuncks[i].Substring(localLevel);
+					if (localLevel < path.Count)
+					{
+						path.RemoveRange(localLevel, path.Count - localLevel);
+					}
+					path.Add(chunck);
+					int isFile = chunck.IndexOf('.');
+					if (isFile > 0)
+					{
+						var pathString = string.Join("/", path);
+						maxSoFar = Math.Max(maxSoFar, pathString.Length);
+					}
+				}
+				return maxSoFar;
+			}
+		}
+
+		public class Solution1
         {
             public int LengthLongestPath(string input)
             {
