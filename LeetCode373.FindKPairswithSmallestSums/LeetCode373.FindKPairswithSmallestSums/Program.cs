0b200d11f4dd7ad338813f2c9f16ee2c40ac55c8
robertoligeiro <robertoligeiro@gmail.com>
2018-01-02 10:36:32 -0800

    fix for smallest pairs problem -  373. Find K Pairs with Smallest Sums.

--- LeetCode373.FindKPairswithSmallestSums/LeetCode373.FindKPairswithSmallestSums/Program.cs
+++ LeetCode373.FindKPairswithSmallestSums/LeetCode373.FindKPairswithSmallestSums/Program.cs
@@ -12,64 +12,62 @@ namespace LeetCode373.FindKPairswithSmallestSums
         static void Main(string[] args)
         {
             var s = new Solution();
-            var r = s.KSmallestPairs(new int[] { 1, 1, 2 }, new int[] { 1, 2, 3 }, 10);
-            r = s.KSmallestPairs(new int[] {  }, new int[] {  }, 2);
+
+			var r = s.KSmallestPairs(new int[] { 1, 1, 2 }, new int[] { 1, 2, 3 }, 10);
+
+			r = s.KSmallestPairs(new int[] {  }, new int[] {  }, 2);
         }
 
-        public class Solution
-        {
-            public class Pair : IComparable
-            {
-                public int sum;
-                public int[] nums;
+		public class Solution
+		{
+			public IList<int[]> KSmallestPairs(int[] nums1, int[] nums2, int k)
+			{
+				var resp = new List<int[]>();
+				if (nums1.Length == 0 || nums2.Length == 0 || k <= 0) return resp;
 
-                public int CompareTo(object obj)
-                {
-                    var p2 = obj as Pair;
-                    return this.sum.CompareTo(p2.sum);
-                }
-            }
-            public IList<int[]> KSmallestPairs(int[] nums1, int[] nums2, int k)
-            {
-                if (nums1 == null || nums2 == null || nums1.Length == 0 || nums2.Length == 0) return new List<int[]>();
-                var first = nums1[0] <= nums2[0] ? nums1 : nums2;
-                var second = nums1[0] <= nums2[0] ? nums2 : nums1;
-                var resp = new List<Pair>();
-                for (int i = 0; i < first.Length; ++i)
-                {
-                    for (int j = 0; j < second.Length; ++j)
-                    {
-                        if (resp.Count > k && (first[i] >= resp.Last().sum || second[j] >= resp.Last().sum)) continue;
-                        var p = new Pair() { sum = first[i] + second[j], nums = new int[] { first[i], second[j] } };
-                        if (resp.Count < k)
-                        {
-                            resp.Add(p);
-                        }
-                        else
-                        {
-                            resp.Sort();
-                            if (resp.Last().sum > p.sum)
-                            {
-                                resp.RemoveAt(resp.Count - 1);
-                                resp.Add(p);
-                            }
-                        }
-                    }
-                }
-                var respPair = resp.Select(x => x.nums).ToList();
-                respPair.Sort(ComparePairs);
-                return respPair;
-            }
+				var sorted = new SortedDictionary<int, HashSet<Tuple<int, int>>>();
+				sorted.Add(nums1[0] + nums2[0], new HashSet<Tuple<int, int>>() { new Tuple<int, int>(0, 0) });
+				while (k-- > 0 && sorted.Count > 0)
+				{
+					var t = sorted.First();
+					var indexes = t.Value.First();
 
-            private int ComparePairs(int[] p1, int[] p2)
-            {
-                var p0 = p1[0].CompareTo(p2[0]);
-                if (p0 == 0)
-                {
-                    return p1[1].CompareTo(p2[1]);
-                }
-                return p0;
-            }
-        }
+					//clean-up
+					t.Value.Remove(indexes);
+
+					resp.Add(new int[] { nums1[indexes.Item1], nums2[indexes.Item2] });
+
+					if (indexes.Item1 + 1 < nums1.Length)
+					{
+						var newindexes = new Tuple<int, int>(indexes.Item1 + 1, indexes.Item2);
+						var val = nums1[newindexes.Item1] + nums2[newindexes.Item2];
+						AddToSorted(sorted, val, newindexes);
+					}
+
+					if (indexes.Item2 + 1 < nums2.Length)
+					{
+						var newindexes = new Tuple<int, int>(indexes.Item1, indexes.Item2 + 1);
+						var val = nums1[newindexes.Item1] + nums2[newindexes.Item2];
+						AddToSorted(sorted, val, newindexes);
+					}
+
+					if (t.Value.Count == 0) sorted.Remove(t.Key);
+				}
+				return resp;
+			}
+
+			public void AddToSorted(SortedDictionary<int, HashSet<Tuple<int, int>>> s, int val, Tuple<int, int> t)
+			{
+				HashSet<Tuple<int, int>> h;
+				if (s.TryGetValue(val, out h))
+				{
+					h.Add(t);
+				}
+				else
+				{
+					s.Add(val, new HashSet<Tuple<int, int>>() { t });
+				}
+			}
+		}
     }
 }
