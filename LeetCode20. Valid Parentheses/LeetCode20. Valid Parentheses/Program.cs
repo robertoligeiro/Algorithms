56d388542257bb167f0009a27773893d7ca53f0d
robertoligeiro <robertoligeiro@gmail.com>
2018-01-13 07:57:09 -0800

    LeetCode20.Valid_Parentheses

---
+++ LeetCode20. Valid Parentheses/LeetCode20. Valid Parentheses/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode20.Valid_Parentheses
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+		public class Solution
+		{
+			public bool IsValid(string s)
+			{
+				var st = new Stack<char>();
+				foreach (var c in s)
+				{
+					if (c == '(')
+						st.Push(')');
+					else if (c == '{')
+						st.Push('}');
+					else if (c == '[')
+						st.Push(']');
+					else if (st.Count == 0 || st.Pop() != c)
+						return false;
+				}
+				return st.Count == 0;
+			}
+		}
+	}
+}
