0d90da059dc7ee5e97fd3cdf093942faef9d37fb
robertoligeiro <robertoligeiro@gmail.com>
2018-01-13 08:22:51 -0800

    move zeros

---
+++ LeetCode283. Move Zeroes/LeetCode283. Move Zeroes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode283.Move_Zeroes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		public class Solution
+		{
+			public void MoveZeroes(int[] nums)
+			{
+				var nextWrite = 0;
+				for (int i = 0; i < nums.Length; ++i)
+				{
+					if (nums[i] != 0) nums[nextWrite++] = nums[i];
+				}
+				for (int i = nextWrite; i < nums.Length; ++i)
+				{
+					nums[i] = 0;
+				}
+			}
+		}
+	}
+}
