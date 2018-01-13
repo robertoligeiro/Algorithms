65a6a71bac81174ca2de63b210e2d121fab1d38c
robertoligeiro <robertoligeiro@gmail.com>
2018-01-13 12:29:57 -0800

    LeetCode122.BestTimetoBuyandSellStockII

---
+++ LeetCode122.BestTimetoBuyandSellStockII/LeetCode122.BestTimetoBuyandSellStockII/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode122.BestTimetoBuyandSellStockII
+{
+	class Program
+	{
+		//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.maxProfit(new int[] { 1, 7, 2, 3, 6, 7, 6, 7 });
+		}
+		public class Solution
+		{
+			public int maxProfit(int[] prices)
+			{
+				var resp = 0;
+				for (int i = 1; i < prices.Length; ++i)
+				{
+					if (prices[i] > prices[i - 1]) resp += prices[i] - prices[i - 1];
+				}
+				return resp;
+			}
+		}
+	}
+}
