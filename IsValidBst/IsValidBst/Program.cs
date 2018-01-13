1a6262e361c5ec29a0995e04e4eed0f4c83a4dd9
robertoligeiro <robertoligeiro@gmail.com>
2018-01-13 13:38:29 -0800

    IsValidBst again!

---
+++ IsValidBst/IsValidBst/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsValidBst
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int x) { val = x; }
+		}
+
+		public class Solution
+		{
+			public bool IsValidBST(TreeNode r)
+			{
+				TreeNode prev = null;
+				return IsValidBST(r, ref prev);
+			}
+
+			private bool IsValidBST(TreeNode n, ref TreeNode prev)
+			{
+				if (n == null) return true;
+				if (!IsValidBST(n.left, ref prev)) return false;
+				if (prev != null && prev.val >= n.val) return false;
+				prev = n;
+				return IsValidBST(n.right, ref prev);
+			}
+		}
+	}
+}
