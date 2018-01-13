040e9c10c380f10da7dcee09a94ce044917558b3
robertoligeiro <robertoligeiro@gmail.com>
2018-01-13 11:57:19 -0800

    LeetCode206.Reverse_Linked_List

---
+++ LeetCode206. Reverse Linked List/LeetCode206. Reverse Linked List/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode206.Reverse_Linked_List
+{
+	class Program
+	{
+		//https://leetcode.com/problems/reverse-linked-list/description/
+		static void Main(string[] args)
+		{
+		}
+
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int x) { val = x; }
+		}
+		public class Solution
+		{
+			public ListNode ReverseList(ListNode head)
+			{
+				if (head == null) return null;
+				ListNode prev = null;
+				var curr = head;
+				while (curr != null)
+				{
+					var next = curr.next;
+					curr.next = prev;
+					prev = curr;
+					curr = next;
+				}
+				return prev;
+			}
+		}
+	}
+}
