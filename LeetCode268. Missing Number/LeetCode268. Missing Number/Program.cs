cce7be28f62c5c208fec3f0d27101df4ef027b20
robertoligeiro <robertoligeiro@gmail.com>
2018-01-14 13:31:08 -0800

    missing number.

---
+++ LeetCode268. Missing Number/LeetCode268. Missing Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode268.Missing_Number
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+		public class Solution
+		{
+			public int MissingNumber(int[] nums)
+			{
+				var sum = 0;
+				var sumArray = 0;
+				for (int i = 0; i <= nums.Length; ++i)
+				{
+					if (i < nums.Length) sumArray += nums[i];
+					sum += i;
+				}
+				return sum - sumArray;
+			}
+		}
+	}
+}
