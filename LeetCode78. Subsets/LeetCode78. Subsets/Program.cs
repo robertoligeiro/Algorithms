bd0772d5a4e94210e364fa6ba6b1678a5313be8e
robertoligeiro <robertoligeiro@gmail.com>
2018-01-14 13:36:43 -0800

    LeetCode78.Subsets

---
+++ LeetCode78. Subsets/LeetCode78. Subsets/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode78.Subsets
+{
+	class Program
+	{
+		//https://leetcode.com/problems/subsets/description/
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.Subsets(new int[] { 1,2,3});
+		}
+		public class Solution
+		{
+			public IList<IList<int>> Subsets(int[] nums)
+			{
+				var resp = new List<IList<int>>();
+				var parc = new List<int>();
+				Subsets(nums, resp, parc, 0);
+				return resp;
+			}
+
+			private void Subsets(int[] nums, List<IList<int>> resp, List<int> parc, int pos)
+			{
+				resp.Add(new List<int>(parc));
+				if (pos == nums.Length) return;
+				for (int i = pos; i < nums.Length; ++i)
+				{
+					parc.Add(nums[i]);
+					Subsets(nums, resp, parc, i + 1);
+					parc.RemoveAt(parc.Count - 1);
+				}
+			}
+		}
+	}
+}
