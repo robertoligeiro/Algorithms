ac9e30a4fc277c4f6abe11daab947ac3f2bc7753
robertoligeiro <robertoligeiro@gmail.com>
2018-01-16 08:55:59 -0800

    pathsII

--- LeetCode63. Unique Paths II/LeetCode63. Unique Paths II/Program.cs
+++ LeetCode63. Unique Paths II/LeetCode63. Unique Paths II/Program.cs
@@ -12,10 +12,41 @@ namespace LeetCode63.Unique_Paths_II
 		static void Main(string[] args)
 		{
 			var board = new int[,] { { 0,0,0}, { 0,1,0}, { 0,0,0} };
-			var s = new Solution();
+			var s = new Solutionold();
 			var r = s.UniquePathsWithObstacles(board);
 		}
 		public class Solution
+		{
+			public int UniquePathsWithObstacles(int[,] obstacleGrid)
+			{
+				var memo = new int[obstacleGrid.GetLength(0), obstacleGrid.GetLength(1)];
+				return UniquePathsWithObstacles(obstacleGrid, 0, 0, memo);
+			}
+
+			public int UniquePathsWithObstacles(int[,] obstacleGrid, int r, int c, int[,] memo)
+			{
+				if (r == obstacleGrid.GetLength(0) - 1 && 
+					c == obstacleGrid.GetLength(1) - 1 &&
+					obstacleGrid[obstacleGrid.GetLength(0) - 1,obstacleGrid.GetLength(1) - 1] != 1
+					) return 1;
+
+				if (memo[r, c] != 0) return memo[r, c];
+				var down = 0;
+				if (r + 1 < obstacleGrid.GetLength(0) && obstacleGrid[r + 1, c] != 1)
+				{
+					down = UniquePathsWithObstacles(obstacleGrid, r + 1, c, memo);
+				}
+				var right = 0;
+				if (c + 1 < obstacleGrid.GetLength(1) && obstacleGrid[r, c + 1] != 1)
+				{
+					right = UniquePathsWithObstacles(obstacleGrid, r, c + 1, memo);
+				}
+				memo[r, c] = down + right;
+				return memo[r, c];
+			}
+		}
+
+		public class Solutionold
 		{
 			public int UniquePathsWithObstacles(int[,] obstacleGrid)
 			{
