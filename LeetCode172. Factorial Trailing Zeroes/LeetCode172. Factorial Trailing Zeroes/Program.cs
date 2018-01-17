f5df500f6bca4a12853fe9657e8d67400d745671
robertoligeiro <robertoligeiro@gmail.com>
2018-01-17 07:00:50 -0800

    LeetCode172.Factorial_Trailing_Zeroes

---
+++ LeetCode172. Factorial Trailing Zeroes/LeetCode172. Factorial Trailing Zeroes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode172.Factorial_Trailing_Zeroes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		public class Solution
+		{
+			public int TrailingZeroes(int n)
+			{
+				var resp = 0;
+				while (n >= 5)
+				{
+					n /= 5;
+					resp += n;
+				}
+				return resp;
+			}
+		}
+	}
+}
