9e5c013e7347afef3e0ba7d0038708f7fbbc1abd
robertoligeiro <robertoligeiro@gmail.com>
2018-01-17 07:23:37 -0800

    LeetCode101.Symmetric_Tree

---
+++ LeetCode101. Symmetric Tree/LeetCode101. Symmetric Tree/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode101.Symmetric_Tree
+{
+	class Program
+	{
+		//https://leetcode.com/problems/symmetric-tree/description/
+		static void Main(string[] args)
+		{
+		}
+
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int x) { val = x; }
+		}
+
+		public class Solution
+		{
+			public bool IsSymmetric(TreeNode root)
+			{
+				if (root == null) return true;
+				return IsSymmetric(root.left, root.right);
+			}
+
+			private bool IsSymmetric(TreeNode l, TreeNode r)
+			{
+				if (l == null && r == null) return true;
+				if (l == null || r == null) return false;
+				if (l.val != r.val) return false;
+				return IsSymmetric(l.left, r.right) && IsSymmetric(l.right, r.left);
+			}
+		}
+	}
+}
---
+++ LeetCode101. Symmetric Tree/LeetCode101. Symmetric Tree/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LeetCode101. Symmetric Tree")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode101. Symmetric Tree")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("339b747e-a829-4282-bf46-1d6bc3e6f75c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
