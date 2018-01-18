060ee621c6c27c78241c76a8fb1797e6ff8f16f4
robertoligeiro <robertoligeiro@gmail.com>
2018-01-18 07:33:10 -0800

    LeetCode113.Path_Sum_II

---
+++ LeetCode113. Path Sum II/LeetCode113. Path Sum II/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode113.Path_Sum_II
+{
+	class Program
+	{
+		//https://leetcode.com/problems/path-sum-ii/description/
+		static void Main(string[] args)
+		{
+		}
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int x) { val = x; }
+		}
+		public class Solution
+		{
+			public IList<IList<int>> PathSum(TreeNode root, int sum)
+			{
+				var resp = new List<IList<int>>();
+				var path = new List<int>();
+				PathSum(root, resp, path, sum);
+				return resp;
+			}
+			private void PathSum(TreeNode n, List<IList<int>> resp, List<int> path, int sum)
+			{
+				if (n == null) return;
+				sum -= n.val;
+				if (n.left == null && n.right == null)
+				{
+					if (sum == 0)
+					{
+						var p = new List<int>(path);
+						p.Add(n.val);
+						resp.Add(p);
+					}
+					return;
+				}
+
+				path.Add(n.val);
+				PathSum(n.left, resp, path, sum);
+				PathSum(n.right, resp, path, sum);
+				path.RemoveAt(path.Count - 1);
+			}
+		}
+	}
+}
