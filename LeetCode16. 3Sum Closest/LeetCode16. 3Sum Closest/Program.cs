98ce55bd2c1dd130c7659da1723ff88d4253e8ab
robertoligeiro <robertoligeiro@gmail.com>
2018-01-18 19:01:26 -0800

    LeetCode16._3Sum_Closest

---
+++ LeetCode16. 3Sum Closest/LeetCode16. 3Sum Closest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode16._3Sum_Closest
+{
+	class Program
+	{
+		//https://leetcode.com/problems/3sum-closest/description/
+		static void Main(string[] args)
+		{
+		}
+
+		public class Solution
+		{
+			public int ThreeSumClosest(int[] nums, int target)
+			{
+				nums = nums.OrderBy(x => x).ToArray();
+				var minDiff = int.MaxValue;
+				var respSum = 0;
+				var i = 0;
+				var last = nums.Length - 1;
+				while (i < last)
+				{
+					var l = i + 1;
+					var r = last;
+					while (l < r)
+					{
+						var sum = nums[i] + nums[l] + nums[r];
+						var diff = Math.Abs(target - sum);
+						if (diff < minDiff)
+						{
+							respSum = sum;
+							minDiff = diff;
+						}
+						if (diff == 0) return respSum;
+						if (sum > target)
+						{
+							r--;
+						}
+						else
+						{
+							l++;
+						}
+					}
+					++i;
+				}
+				return respSum;
+			}
+		}
+	}
+}
