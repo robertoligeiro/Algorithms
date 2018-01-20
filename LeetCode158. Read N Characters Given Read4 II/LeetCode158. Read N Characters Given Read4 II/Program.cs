2638b0b5d7a33137d374e483ef5f14ecce955df3
robertoligeiro <robertoligeiro@gmail.com>
2018-01-19 17:22:27 -0800

    LeetCode158.Read_N_Characters_Given_Read4_II

---
+++ LeetCode158. Read N Characters Given Read4 II/LeetCode158. Read N Characters Given Read4 II/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode158.Read_N_Characters_Given_Read4_II
+{
+	//https://leetcode.com/problems/read-n-characters-given-read4-ii-call-multiple-times/description/
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			char[] b = new char[4];
+			var r = s.Read(b, 2);
+			r = s.Read(b, 1);
+		}
+
+		public class Reader4
+		{
+			public int Read4(char[] buff)
+			{
+				Array.Copy("ab".ToArray(), buff, 2);
+				return 2;
+			}
+		}
+		public class Solution : Reader4
+		{
+			/**
+			 * @param buf Destination buffer
+			 * @param n   Maximum number of characters to read
+			 * @return    The number of characters read
+			 */
+
+			private char[] read4Buffer = new char[4];
+			private int charsAvailable = 0;
+			private int nextAvailable = 0;
+
+			public int Read(char[] buf, int n)
+			{
+				var index = 0;
+				while (n > 0)
+				{
+					if (this.charsAvailable > 0)
+					{
+						buf[index++] = this.read4Buffer[nextAvailable++];
+						--n;
+						--this.charsAvailable;
+					}else
+					if (this.charsAvailable == 0)
+					{
+						this.ReadFromRead4();
+					}
+					else break;
+				}
+				return index;
+			}
+
+			private void ReadFromRead4()
+			{
+				var ret = this.Read4(this.read4Buffer);
+				if (ret == 0) this.charsAvailable = -1;
+				else
+				{
+					this.nextAvailable = 0;
+					this.charsAvailable = ret;
+				} 
+			}
+		}
+	}
+}
---
+++ LeetCode158. Read N Characters Given Read4 II/LeetCode158. Read N Characters Given Read4 II/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LeetCode158. Read N Characters Given Read4 II")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode158. Read N Characters Given Read4 II")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8cffb69e-5371-4602-96a3-2b772fa82cdd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
