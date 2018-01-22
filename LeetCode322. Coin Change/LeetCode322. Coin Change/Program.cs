26f85579ca47f39a48650a8c69dd93a0aec07ab3
robertoligeiro <robertoligeiro@gmail.com>
2018-01-21 18:48:24 -0800

    coin change fix.

--- LeetCode322. Coin Change/LeetCode322. Coin Change/Program.cs
+++ LeetCode322. Coin Change/LeetCode322. Coin Change/Program.cs
@@ -13,10 +13,39 @@ namespace LeetCode322.Coin_Change
 		{
 			var s = new Solution();
 			//var r = s.CoinChange(new int[] { 1, 2, 5 }, 11);
-			var r = s.CoinChange(new int[] { 2, 5,10,1 }, 27);
+			//var r = s.CoinChange(new int[] { 2, 5,10,1 }, 27);
+			var r = s.CoinChange(new int[] { 2 }, 3);
 		}
 
 		public class Solution
+		{
+			public int CoinChange(int[] coins, int amount)
+			{
+				var memo = new int[amount+1];
+				var resp = CoinChange(coins, amount, memo);
+
+				return resp == int.MaxValue?-1:resp; //for cases there's no answer. i.e. coins:[2], amt:3
+			}
+			private int CoinChange(int[] coins, int amt, int[] memo)
+			{
+				if (amt == 0) return 0;
+				if (memo[amt] != 0) return memo[amt];
+				var min = int.MaxValue;
+				foreach(var c in coins)
+				{
+					if (amt-c >=0)
+					{
+						var localMin = CoinChange(coins, amt - c, memo);
+						if (localMin == int.MaxValue) continue;
+						min = Math.Min(localMin+1, min);
+					}
+				}
+				memo[amt] = min; 
+				return min;
+			}
+		}
+
+		public class SolutionOld
 		{
 			public int CoinChange(int[] coins, int amount)
 			{
