09cc16c90ee417d27a6d7e3794e6a49288cccf26
robertoligeiro <robertoligeiro@gmail.com>
2018-01-23 21:39:48 -0800

    LeetCode463.Island_Perimeter

---
+++ LeetCode463. Island Perimeter/LeetCode463. Island Perimeter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode463.Island_Perimeter
+{
+	class Program
+	{
+		//https://leetcode.com/problems/island-perimeter/description/
+		static void Main(string[] args)
+		{
+		}
+
+		public class Solution
+		{
+			public int IslandPerimeter(int[,] grid)
+			{
+				var resp = 0;
+				for (int row = 0; row < grid.GetLength(0); ++row)
+				{
+					for (int col = 0; col < grid.GetLength(1); ++col)
+					{
+						if (grid[row, col] == 1)
+						{
+							if (row == 0 || grid[row - 1, col] == 0) resp++;
+							if (row == grid.GetLength(0) - 1 || grid[row + 1, col] == 0) resp++;
+							if (col == 0 || grid[row, col - 1] == 0) resp++;
+							if (col == grid.GetLength(1) - 1 || grid[row, col + 1] == 0) resp++;
+						}
+					}
+				}
+				return resp;
+			}
+		}
+	}
+}
