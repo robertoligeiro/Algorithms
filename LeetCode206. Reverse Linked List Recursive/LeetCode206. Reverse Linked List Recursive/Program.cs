9c2d4f04980daf463eb4f8659bd1ba2b1de4f24f
robertoligeiro <robertoligeiro@gmail.com>
2018-01-25 08:14:09 -0800

    LeetCode63.Unique_Paths_II_UsingDP

---
+++ LeetCode206. Reverse Linked List Recursive/LeetCode206. Reverse Linked List Recursive/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode206.Reverse_Linked_List_Recursive
+{
+	class Program
+	{
+		//https://leetcode.com/problems/reverse-linked-list/
+		static void Main(string[] args)
+		{
+		}
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int x) { val = x; }
+		}
+		public class Solution
+		{
+			public ListNode ReverseList(ListNode head)
+			{
+				if (head == null) return null;
+				return Reverse(head, null);
+			}
+
+			private ListNode Reverse(ListNode n, ListNode prev)
+			{
+				var next = n.next;
+				n.next = prev;
+				if (next == null) return n;
+				return Reverse(next, n);
+			}
+		}
+	}
+}
