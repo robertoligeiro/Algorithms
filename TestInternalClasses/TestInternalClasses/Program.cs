33fb465657dc6ca233eb5ae925c314e20ed692d2
robertoligeiro <robertoligeiro@gmail.com>
2018-01-26 19:25:08 -0800

    TestInternalClasses

---
+++ TestInternalClasses/TestInternalClasses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinternalNm;
+
+namespace TestInternalClasses
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var t = new Tinternal();
+			t.printInternal();
+		}
+	}
+}
---
+++ TestInternalClasses/TestInternalClasses/Tinternal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinternalNm
+{
+	public class Tinternal
+	{
+		internal class TinternalInternal
+		{
+			public int v;
+			public TinternalInternal(int v)
+			{
+				this.v = v;
+			}
+		}
+
+		private TinternalInternal t = new TinternalInternal(2);
+		public void printInternal()
+		{
+			Console.WriteLine(this.t.v);
+		}
+	}
+}
