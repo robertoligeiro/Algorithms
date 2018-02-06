b9b7f10593ea338b3f864424175591ca2e4cd28d
robertoligeiro <robertoligeiro@gmail.com>
2018-02-06 08:22:45 -0800

    LeetCode157.Read_N_Characters_Given_Read4

---
+++ LeetCode157. Read N Characters Given Read4/LeetCode157. Read N Characters Given Read4/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode157.Read_N_Characters_Given_Read4
+{
+	class Program
+	{
+		//https://leetcode.com/problems/read-n-characters-given-read4/description/
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var b = new char[1];
+			var r = s.Read(b, 1);
+		}
+
+		/* The Read4 API is defined in the parent class Reader4.
+      int Read4(char[] buf); */
+
+		public class Solution //: Reader4
+		{
+			private char[] intBuf = new char[4];
+			private int available;
+			private int next = 0;
+			int Read4(char[] buf)
+			{
+				intBuf = new char[] { 'a' };
+				return 1;
+			}
+			/**
+			 * @param buf Destination buffer
+			 * @param n   Maximum number of characters to read
+			 * @return    The number of characters read
+			 */
+			public int Read(char[] buf, int n)
+			{
+				int i = 0;
+				while (n > 0)
+				{
+					--n;
+					char c = ' ';
+					if (ReadFromIntBuf(ref c))
+					{
+						buf[i++] = c;
+					}
+					else break;
+				}
+				return i;
+			}
+
+			private bool ReadFromIntBuf(ref char c)
+			{
+				if (available > 0)
+				{
+					available--;
+					c = intBuf[next++];
+					return true;
+				}
+				else
+				{
+					available = Read4(intBuf);
+					if (available > 0)
+					{
+						next = 0;
+						return ReadFromIntBuf(ref c);
+					}
+				}
+				return false;
+			}
+		}
+	}
+}
---
+++ LeetCode157. Read N Characters Given Read4/LeetCode157. Read N Characters Given Read4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LeetCode157. Read N Characters Given Read4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode157. Read N Characters Given Read4")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("850f0648-595a-46b2-a484-37a464c962c1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
