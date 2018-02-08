b8ae7f956d0778579fc166aa5e695fe80c7ffd36
robertoligeiro <robertoligeiro@gmail.com>
2018-02-08 08:53:40 -0800

    LeetCode285.Inorder_Successor_in_BST

---
+++ LeetCode285. Inorder Successor in BST/LeetCode285. Inorder Successor in BST/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode285.Inorder_Successor_in_BST
+{
+	class Program
+	{
+		//https://leetcode.com/problems/inorder-successor-in-bst/description/
+		static void Main(string[] args)
+		{
+		}
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int x) { val = x; }
+		}
+		public class Solution
+		{
+			public TreeNode InorderSuccessor(TreeNode root, TreeNode p)
+			{
+				if (p == null) return null;
+				TreeNode resp = null;
+				while(root != null)
+				{
+					if (root.val > p.val)
+					{
+						resp = root;
+						root = root.left;
+					}
+					else
+					{
+						root = root.right;
+					}
+				}
+				return resp;
+			}
+		}
+	}
+}
