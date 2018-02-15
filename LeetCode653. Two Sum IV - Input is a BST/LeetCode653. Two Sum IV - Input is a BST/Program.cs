da22958f54c21e27d257d316a1b19566e4a9488a
robertoligeiro <robertoligeiro@gmail.com>
2018-02-14 16:55:14 -0800

    LeetCode653.Two_Sum_IV___Input_is_a_BST

---
+++ LeetCode653. Two Sum IV - Input is a BST/LeetCode653. Two Sum IV - Input is a BST/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode653.Two_Sum_IV___Input_is_a_BST
+{
+	class Program
+	{
+		//https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
+		static void Main(string[] args)
+		{
+		}
+
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int x) { val = x; }
+		}
+		public class Solution
+		{
+			public bool FindTarget(TreeNode root, int k)
+			{
+				var h = new HashSet<int>();
+				return FindTarget(root, k, h);
+			}
+
+			private bool FindTarget(TreeNode n, int k, HashSet<int> h)
+			{
+				if (n == null) return false;
+				if (h.Contains(n.val)) return true;
+
+				h.Add(k - n.val);
+				return FindTarget(n.left, k, h) || FindTarget(n.right, k, h);
+			}
+		}
+	}
+}
