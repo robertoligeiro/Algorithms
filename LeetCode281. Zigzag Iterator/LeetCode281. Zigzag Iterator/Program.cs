689237801c562f9ae839173a200e62666a8a6d8e
robertoligeiro <robertoligeiro@gmail.com>
2018-02-19 20:21:14 -0800

    LeetCode281.Zigzag_Iterator

---
+++ LeetCode281. Zigzag Iterator/LeetCode281. Zigzag Iterator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode281.Zigzag_Iterator
+{
+	class Program
+	{
+		//https://leetcode.com/problems/zigzag-iterator/
+		static void Main(string[] args)
+		{
+		}
+
+		public class ZigzagIterator
+		{
+			private int indexV1;
+			private int indexV2;
+			private IList<int> v1;
+			private IList<int> v2;
+			private Queue<int> items = new Queue<int>();
+			private void AddToQueue()
+			{
+				if (indexV1 < v1.Count) items.Enqueue(v1[indexV1++]);
+				if (indexV2 < v2.Count) items.Enqueue(v2[indexV2++]);
+			}
+			public ZigzagIterator(IList<int> v1, IList<int> v2)
+			{
+				this.v1 = v1;
+				this.v2 = v2;
+				AddToQueue();
+			}
+
+			public bool HasNext()
+			{
+				return items.Count > 0;
+			}
+
+			public int Next()
+			{
+				AddToQueue();
+				return items.Dequeue();
+			}
+		}
+	}
+}
---
+++ LeetCode418. Sentence Screen Fitting/LeetCode418. Sentence Screen Fitting/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode418.Sentence_Screen_Fitting
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			//var r = s.WordsTyping(new string[] { "hello","world"}, 2,8);
+			var r = s.WordsTyping(new string[] { "a", "bcd", "e" }, 3, 6);
+			//var r = s.WordsTyping(new string[] { "I", "had", "apple", "pie" }, 4, 5);
+			//var r = s.WordsTyping(new string[] { "ab", "c"}, 2, 4);
+			//var r = s.WordsTyping(new string[] { "abc", "de", "f" }, 4, 6);
+		}
+
+		public class Solution
+		{
+			public int WordsTyping(string[] sentence, int rows, int cols)
+			{
+				int index = 0;
+				var h = new HashSet<string>();
+				int lines = 0;
+				for (int rowsCount = 0; rowsCount < rows; ++rowsCount)
+				{
+					var freeSpace = cols;
+					var line = string.Empty;
+					while (true)
+					{
+						freeSpace -= sentence[index % sentence.Length].Length;
+						if (freeSpace < 0) break;
+						line += sentence[index % sentence.Length];
+						index++;
+						freeSpace--;
+						if (freeSpace >= 0) line += " ";
+					}
+					if (!h.Add(line))
+					{
+						break;
+					}
+					lines++;
+				}
+				return rows / lines;
+			}
+		}
+
+		//public class Solution
+		//{
+		//	public int WordsTyping(string[] sentence, int rows, int cols)
+		//	{
+		//		var s = string.Join(" ", sentence) + " ";
+		//		int start = 0, l = s.Length;
+		//		for (int i = 0; i < rows; i++)
+		//		{
+		//			start += cols;
+		//			if (s[start % l] == ' ')
+		//			{
+		//				start++;
+		//			}
+		//			else
+		//			{
+		//				while (start > 0 && s[(start - 1) % l] != ' ')
+		//				{
+		//					start--;
+		//				}
+		//			}
+		//		}
+
+		//		return start / s.Length;
+		//	}
+		//}
+	}
+}
