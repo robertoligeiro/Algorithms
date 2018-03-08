91b98d20e1500556cb5843432458b41513cdba8d
robertoligeiro <robertoligeiro@gmail.com>
2018-03-08 09:06:46 -0800

    LeetCode371.Sum_of_Two_Integers

---
+++ LeetCode371. Sum of Two Integers/LeetCode371. Sum of Two Integers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode371.Sum_of_Two_Integers
+{
+	class Program
+	{
+		//https://leetcode.com/problems/sum-of-two-integers/description/
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.GetSum(1, 2);
+		}
+		public class Solution
+		{
+			public int GetSum(int a, int b)
+			{
+				while (b > 0)
+				{
+					var prevA = a;
+					a = a ^ b;
+					b = (prevA & b) << 1;
+				}
+				return a;
+			}
+		}
+	}
+}
