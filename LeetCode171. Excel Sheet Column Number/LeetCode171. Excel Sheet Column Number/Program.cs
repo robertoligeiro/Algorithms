de7964cbf08faaada95f280556c6453f0e282971
robertoligeiro <robertoligeiro@gmail.com>
2018-03-07 22:20:42 -0800

    LeetCode171.Excel_Sheet_Column_Number

---
+++ LeetCode171. Excel Sheet Column Number/LeetCode171. Excel Sheet Column Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode171.Excel_Sheet_Column_Number
+{
+	class Program
+	{
+		//https://leetcode.com/problems/excel-sheet-column-number/description/
+		static void Main(string[] args)
+		{
+		}
+		public class Solution
+		{
+			public int TitleToNumber(string s)
+			{
+				var resp = 0;
+				for (var i = 0; i < s.Length; ++i)
+				{
+					resp = resp * 26 + (s[i] - 'A' + 1);
+				}
+				return resp;
+			}
+		}
+	}
+}
