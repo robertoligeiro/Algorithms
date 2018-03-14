6fca69be945f9e69be2579b4871959dc808b767c
robertoligeiro <robertoligeiro@gmail.com>
2018-03-13 21:29:19 -0700

    LeetCode36.Valid_Sudoku

--- LeetCode36. Valid Sudoku/LeetCode36. Valid Sudoku/Program.cs
+++ LeetCode36. Valid Sudoku/LeetCode36. Valid Sudoku/Program.cs
@@ -30,49 +30,25 @@ namespace LeetCode36.Valid_Sudoku
 		{
 			public bool IsValidSudoku(char[,] board)
 			{
-				for (int row = 0; row < board.GetLength(0); ++row)
+				for (int row = 0; row < 9; row++)
 				{
-					for (int col = 0; col < board.GetLength(1); ++col)
+					var rowHash = new HashSet<char>();
+					var colHash = new HashSet<char>();
+					var regHash = new HashSet<char>();
+					for (int col = 0; col < 9; col++)
 					{
-						if (board[row, col] != '.')
-						{
-							if (!CanAdd(board, row, col)) return false;
-						}
-					}
-				}
-				return true;
-			}
+						//get row
+						if (board[row, col] != '.' && !rowHash.Add(board[row, col])) return false;
 
-			public bool CanAdd(char[,] b, int row, int col)
-			{
-				return CanAddRow(b,row) && CanAddCol(b, col) && CanAddSqr(b, row - row%3, col - col%3);
-			}
-			public bool CanAddRow(char[,] b, int row)
-			{
-				var h = new HashSet<char>();
-				for (int col = 0; col < b.GetLength(1); ++col)
-				{
-					if (b[row, col] != '.' && !h.Add(b[row, col])) return false;
-				}
-				return true;
-			}
-			public bool CanAddCol(char[,] b, int col)
-			{
-				var h = new HashSet<char>();
-				for (int row = 0; row < b.GetLength(1); ++row)
-				{
-					if (b[row, col] != '.' && !h.Add(b[row, col])) return false;
-				}
-				return true;
-			}
-			public bool CanAddSqr(char[,] b, int rowStart, int colStart)
-			{
-				var h = new HashSet<char>();
-				for (int row = rowStart; row < rowStart + 3; row++)
-				{
-					for (int col = colStart; col < colStart + 3; col++)
-					{
-						if (b[row, col] != '.' && !h.Add(b[row, col])) return false;
+						//get col
+						if (board[col, row] != '.' && !colHash.Add(board[col, row])) return false;
+
+						//get reg
+						var rowIndex = 3 * (row / 3);
+						var colIndex = 3 * (row % 3);
+						var rowReg = rowIndex + (col / 3);
+						var colReg = colIndex + (col % 3);
+						if (board[rowReg, colReg] != '.' && !regHash.Add(board[rowReg, colReg])) return false;
 					}
 				}
 				return true;
