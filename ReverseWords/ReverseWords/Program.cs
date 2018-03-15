264af2770b991563e6e1a3d6c90571a30bb9603d
robertoligeiro <robertoligeiro@gmail.com>
2018-03-14 21:09:51 -0700

    reverse words in string.

---
+++ ReverseWords/ReverseWords/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWords
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var s = Reverse("The Blue Sky");
+		}
+
+		public static string Reverse(string s)
+		{
+			var sArray = s.ToArray();
+			var start = 0;
+			var end = 0;
+			Array.Reverse(sArray);
+			while (end < s.Length)
+			{
+				while (end < s.Length && s[end] != ' ')
+				{
+					end++;
+				}
+				Array.Reverse(sArray, start, end - start);
+				end++;
+				start = end;
+			}
+			return new string(sArray);
+		}
+	}
+}
