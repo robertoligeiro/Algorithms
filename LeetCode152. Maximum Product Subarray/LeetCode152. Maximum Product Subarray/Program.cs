8ae6f717640ad013ceff760f7737a50a37bf94b7
robertoligeiro <robertoligeiro@gmail.com>
2018-03-15 08:47:38 -0700

    LeetCode152.Maximum_Product_Subarray

---
+++ LeetCode152. Maximum Product Subarray/LeetCode152. Maximum Product Subarray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode152.Maximum_Product_Subarray
+{
+	//https://leetcode.com/problems/maximum-product-subarray/description/
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+		public class Solution
+		{
+			public int MaxProduct(int[] nums)
+			{
+				var maxSoFar = nums[0];
+				var maxHerePre = 0;
+				var minHerePre = 0;
+				var maxHere = nums[0];
+				var minHere = nums[0];
+				for (int i = 1; i < nums.Length; ++i)
+				{
+					maxHerePre = Math.Max(nums[i], Math.Max(maxHere * nums[i], minHere * nums[i]));
+					minHerePre = Math.Min(nums[i], Math.Min(minHere * nums[i], maxHere * nums[i]));
+					maxHere = maxHerePre;
+					minHere = minHerePre;
+					maxSoFar = Math.Max(maxSoFar, maxHere);
+				}
+				return maxSoFar;
+			}
+		}
+	}
+}
