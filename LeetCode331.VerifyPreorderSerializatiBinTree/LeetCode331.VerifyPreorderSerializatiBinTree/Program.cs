e98f6ce0a0adce805f1918e765babc75f8901af2
robertoligeiro <robertoligeiro@gmail.com>
2018-03-15 20:41:35 -0700

    fix LeetCode331.VerifyPreorderSerializatiBinTree

--- LeetCode331.VerifyPreorderSerializatiBinTree/LeetCode331.VerifyPreorderSerializatiBinTree/Program.cs
+++ LeetCode331.VerifyPreorderSerializatiBinTree/LeetCode331.VerifyPreorderSerializatiBinTree/Program.cs
@@ -12,31 +12,51 @@ namespace LeetCode331.VerifyPreorderSerializatiBinTree
 		static void Main(string[] args)
 		{
 			//my testcases
-			//var s = new Solution();
-			//var r = s.IsValidSerialization("1,2,4,#,#,#,3,#,#");
-			//var r = s.IsValidSerialization("1,#,#,1");		
+			var s = new Solution();
+			var r = s.IsValidSerialization("1,2,4,#,#,#,3,#,#");
+			//var r = s.IsValidSerialization("1,#,#,1");
 		}
 		public class Solution
 		{
 			public bool IsValidSerialization(string preorder)
 			{
-				int pos = 0;
-				var preOrderArray = preorder.Split(',');
-				return IsValidSerialization(preOrderArray, ref pos) && pos == preOrderArray.Length - 1;
+				var q = new Queue<string>(preorder.Split(','));
+				return IsValidSerialization(q) && q.Count == 0;
 			}
 
-			public bool IsValidSerialization(string[] preorder, ref int pos)
+			public bool IsValidSerialization(Queue<string> q)
 			{
-				if (pos >= preorder.Length) return false;
-				if (preorder[pos] == "#")
+				if (q.Count == 0)
 				{
-					return true;
+					return false;
 				}
-				++pos;
-				if (!IsValidSerialization(preorder, ref pos)) return false;
-				++pos;
-				return IsValidSerialization(preorder, ref pos);
+				var s = q.Dequeue();
+				if (s == "#") return true;
+				if(!IsValidSerialization(q)) return false;
+				return IsValidSerialization(q);
 			}
 		}
+		//public class Solution
+		//{
+		//	public bool IsValidSerialization(string preorder)
+		//	{
+		//		int pos = 0;
+		//		var preOrderArray = preorder.Split(',');
+		//		return IsValidSerialization(preOrderArray, ref pos) && pos == preOrderArray.Length - 1;
+		//	}
+
+		//	public bool IsValidSerialization(string[] preorder, ref int pos)
+		//	{
+		//		if (pos >= preorder.Length) return false;
+		//		if (preorder[pos] == "#")
+		//		{
+		//			return true;
+		//		}
+		//		++pos;
+		//		if (!IsValidSerialization(preorder, ref pos)) return false;
+		//		++pos;
+		//		return IsValidSerialization(preorder, ref pos);
+		//	}
+		//}
 	}
 }
