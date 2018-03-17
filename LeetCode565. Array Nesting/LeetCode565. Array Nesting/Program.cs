30342bfbca3ee45a72cb4e3a27ea590cbf015fcd
robertoligeiro <robertoligeiro@gmail.com>
2018-03-17 10:32:30 -0700

    LeetCode565.Array_Nesting

--- LeetCode565. Array Nesting/LeetCode565. Array Nesting/Program.cs
+++ LeetCode565. Array Nesting/LeetCode565. Array Nesting/Program.cs
@@ -14,7 +14,6 @@ namespace LeetCode565.Array_Nesting
 			var s = new Solution();
 			var r = s.ArrayNesting(new int[] { 5,4,0,3,1,6,2});
 		}
-
 		public class Solution
 		{
 			public int ArrayNesting(int[] nums)
@@ -23,17 +22,14 @@ namespace LeetCode565.Array_Nesting
 				var resp = 0;
 				for (int i = 0; i < nums.Length; ++i)
 				{
-					if (!h.Contains(nums[i]))
+					var seed = i;
+					var count = 0;
+					while (h.Add(seed))
 					{
-						var seed = nums[i];
-						var count = 0;
-						while (h.Add(seed))
-						{
-							++count;
-							seed = nums[seed];
-						}
-						resp = Math.Max(resp, count);
+						++count;
+						seed = nums[seed];
 					}
+					resp = Math.Max(resp, count);
 				}
 				return resp;
 			}
