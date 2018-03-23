f036cc1d3374328804225bdcbdf40605df1b83da
robertoligeiro <robertoligeiro@gmail.com>
2018-03-23 10:35:09 -0700

    LeetCode662.Maximum_Width_of_Binary_Tree LeetCode89.Gray_Code

---
+++ LeetCode535. Encode and Decode TinyURL/LeetCode535. Encode and Decode TinyURL/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode535.Encode_and_Decode_TinyURL
+{
+	class Program
+	{
+		//https://leetcode.com/problems/encode-and-decode-tinyurl
+		static void Main(string[] args)
+		{
+		}
+		public class Codec
+		{
+			private Dictionary<string, string> map = new Dictionary<string, string>();
+
+			// Encodes a URL to a shortened URL
+			public string encode(string longUrl)
+			{
+				var key = "http://tinyurl.com/" + Base64Encode(longUrl).Substring(0, 6);
+				if (!map.ContainsKey(key))
+				{
+					map.Add(key, longUrl);
+				}
+				return key;
+			}
+
+			// Decodes a shortened URL to its original URL.
+			public string decode(string shortUrl)
+			{
+				if (map.ContainsKey(shortUrl)) return map[shortUrl];
+				return string.Empty;
+			}
+
+			public static string Base64Encode(string plainText)
+			{
+				var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+				return System.Convert.ToBase64String(plainTextBytes);
+			}
+		}
+
+	}
+}
---
+++ LeetCode89. Gray Code/LeetCode89. Gray Code/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode89.Gray_Code
+{
+	class Program
+	{
+		//https://leetcode.com/problems/gray-code/description/
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.GrayCode(2);
+		}
+
+		public class Solution
+		{
+			public IList<int> GrayCode(int n)
+			{
+				var resp = new List<int>();
+				resp.Add(0);
+				for (int i = 0; i < n; ++i)
+				{
+					var size = resp.Count-1;
+					for (; size >= 0; size--)
+					{
+						var v = resp[size] | 1 << i;
+						resp.Add(v);
+					}
+				}
+				return resp;
+			}
+		}
+	}
+}
