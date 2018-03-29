37143aa5e89f55fdc27411ba5f689ea58439b93e
robertoligeiro <robertoligeiro@gmail.com>
2018-03-29 08:55:45 -0700

    LeetCode791.Custom_Sort_String

---
+++ LeetCode791. Custom Sort String/LeetCode791. Custom Sort String/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode791.Custom_Sort_String
+{
+	class Program
+	{
+		//https://leetcode.com/problems/custom-sort-string/description/
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.CustomSortString("cba", "abcd");
+		}
+
+		public class Solution
+		{
+			public string CustomSortString(string S, string T)
+			{
+				var mT = new Dictionary<char, int>();
+				foreach (var c in T)
+				{
+					var count = 0;
+					if (mT.TryGetValue(c, out count))
+					{
+						mT[c] = ++count;
+					}
+					else
+					{
+						mT.Add(c, 1);
+					}
+				}
+
+				var sb = new StringBuilder();
+				foreach (var c in S)
+				{
+					var count = 0;
+					if (mT.TryGetValue(c, out count))
+					{
+						sb.Append(new string(Enumerable.Repeat(c, count).ToArray()));
+						mT[c] = -1;
+					}
+				}
+				foreach (var c in T)
+				{
+					if (mT[c] > 0) sb.Append(c);
+				}
+				return sb.ToString();
+			}
+		}
+	}
+}
