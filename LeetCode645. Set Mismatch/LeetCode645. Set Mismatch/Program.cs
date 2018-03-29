b2536e2dcd606e4da945e9b1d39a887ec76f78d0
robertoligeiro <robertoligeiro@gmail.com>
2018-03-29 08:40:51 -0700

    LeetCode645.Set_Mismatch

---
+++ LeetCode645. Set Mismatch/LeetCode645. Set Mismatch/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode645.Set_Mismatch
+{
+	class Program
+	{
+		//https://leetcode.com/problems/set-mismatch/description/
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.FindErrorNums(new int[] { 1, 2, 2, 4 }); 
+		}
+		public class Solution
+		{
+			public int[] FindErrorNums(int[] nums)
+			{
+				var resp = new int[2];
+				for (int i = 0; i < nums.Length; ++i)
+				{
+					var index = Math.Abs(nums[i]) - 1;
+					if (nums[index] < 0)
+					{
+						resp[0] = index + 1;
+					}
+					else
+					{
+						nums[index] = -nums[index];
+					}
+				}
+
+				for (int i = 0; i < nums.Length; ++i)
+				{
+					if (nums[i] > 0)
+					{
+						resp[1] = i + 1;
+						return resp;
+					}
+				}
+				return resp;
+			}
+		}
+	}
+}
