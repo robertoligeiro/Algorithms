2970725dbc244d61d71be854b12bb3706894ed90
robertoligeiro <robertoligeiro@gmail.com>
2018-03-28 22:15:19 -0700

    LeetCode536.ConstructBinaryTreefromString

---
+++ LeetCode536.ConstructBinaryTreefromString/LeetCode536.ConstructBinaryTreefromString/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode536.ConstructBinaryTreefromString
+{
+	class Program
+	{
+		//https://leetcode.com/problems/construct-binary-tree-from-string/description/
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.Str2tree("4(2(3)(1))(6(5))");
+			//var r = s.Str2tree("4");
+			//var r = s.Str2tree("4(2(3)(1))(6(5))");
+		}
+
+		public class TreeNode
+		{
+			public int val;
+			public TreeNode left;
+			public TreeNode right;
+			public TreeNode(int x) { val = x; }
+		}
+		public class Solution
+		{
+			public TreeNode Str2tree(string s)
+			{
+				if (string.IsNullOrEmpty(s)) return null;
+				var pos = 0;
+				return Str2tree(s, ref pos);
+			}
+
+			private TreeNode Str2tree(string s, ref int pos)
+			{
+				var sb = new StringBuilder();
+				var isNeg = false;
+				if (s[pos] == '-')
+				{
+					++pos;
+					isNeg = true;
+				}
+				while (pos < s.Length && char.IsDigit(s[pos]))
+				{
+					sb.Append(s[pos++]);
+				}
+				var v = int.Parse(sb.ToString());
+				if (isNeg) v = -v;
+				var n = new TreeNode(v);
+				if (pos >= s.Length) return n;
+				if (pos < s.Length && s[pos++] == ')')
+				{
+					return n;
+				}
+				n.left = Str2tree(s, ref pos);
+				if (pos < s.Length && s[pos++] == '(')
+				{
+					n.right = Str2tree(s, ref pos);
+					pos++;
+				}
+				return n;
+			}
+		}
+	}
+}
---
+++ LeetCode801.MinSwapsToMakeSequencesIncreasing/LeetCode801.MinSwapsToMakeSequencesIncreasing/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode801.MinSwapsToMakeSequencesIncreasing
+{
+	class Program
+	{
+		// code is wrong, problem is DP, not implemented.
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.MinSwap(new int[] { 1,3,5,4}, new int[] { 1,2,3,7});
+		}
+		public class Solution
+		{
+			public int MinSwap(int[] A, int[] B)
+			{
+				var resp= 0;
+				for (int i = 1; i < A.Length; ++i)
+				{
+					if (A[i] < A[i - 1])
+					{
+						var temp = A[i];
+						A[i] = B[i];
+						B[i] = temp;
+						resp++;
+					}
+				}
+				for (int i = 1; i < B.Length; ++i)
+				{
+					if (B[i] < B[i - 1])
+					{
+						var temp = B[i];
+						B[i] = A[i];
+						A[i] = temp;
+						resp++;
+					}
+				}
+				return resp;
+			}
+		}
+	}
+}
