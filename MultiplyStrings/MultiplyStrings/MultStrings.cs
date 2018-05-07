69edfd322cbf8689ac6ba6976c42ba23ee18019c
robertoligeiro <robertoligeiro@gmail.com>
2018-05-06 20:02:53 -0700

    mult strings.

---
+++ MultiplyStrings/MultiplyStrings/MultStrings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyStrings
+{
+	public class MultStrings
+	{
+		public static string Mult(string x, string y)
+		{
+			var resp = new int[x.Length + y.Length];
+
+			for (int i = x.Length-1; i >=0 ; --i)
+			{
+				for (int j = y.Length-1; j >= 0; --j)
+				{
+					var p0 = i + j + 1;
+					var p1 = i + j;
+					var mult = (x[i] - '0') * (y[j] - '0');
+					var sum = resp[p0] + mult;
+					resp[p0] = sum % 10;
+					resp[p1] += (sum / 10);
+				}
+			}
+
+			var stringResp = string.Join("", resp);
+			return stringResp.TrimStart('0');
+		}
+	}
+}
---
+++ MultiplyStrings/MultiplyStrings/TestMultStrings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyStrings
+{
+	public class TestMultStrings
+	{
+		public static void TestWithStatic()
+		{
+			Console.WriteLine("Mutl: 123 x 34 = " + 123 * 34 +" returned: "+  MultStrings.Mult("123", "34") );
+			Console.WriteLine("Mutl: 111 x 11 = " + 111 * 11 + " returned: " + MultStrings.Mult("111", "11"));
+			Console.WriteLine("Mutl: 1000 x 1000 = " + 1000 * 1000 + " returned: " + MultStrings.Mult("1000", "1000"));
+		}
+	}
+}
