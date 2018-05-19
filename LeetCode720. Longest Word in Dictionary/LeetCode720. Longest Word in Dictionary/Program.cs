816bc5fc38fff213dae36a7a5aa2dbac6bb60ffd
robertoligeiro <robertoligeiro@gmail.com>
2018-05-19 10:34:45 -0700

    LeetCode720.Longest_Word_in_Dictionary

---
+++ LeetCode720. Longest Word in Dictionary/LeetCode720. Longest Word in Dictionary/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode720.Longest_Word_in_Dictionary
+{
+	//https://leetcode.com/problems/longest-word-in-dictionary/description/
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var s = new Solution();
+			var r = s.LongestWord(new string[] { "a", "banana", "app", "appl", "ap", "apply", "apple" });
+			//var r = s.LongestWord(new string[] { "d", "do", "dog", "p", "pe", "pen", "peng", "pengu", "pengui", "penguin", "e", "el", "ele", "elep", "eleph", "elepha", "elephan", "elephant"});
+		}
+
+		public class Solution
+		{
+			public class TrieNode
+			{
+				public Dictionary<char, TrieNode> children = new Dictionary<char, TrieNode>();
+				public int end;
+			}
+
+			public class Trie
+			{
+				private TrieNode root = new TrieNode();
+				private string[] words;
+				public Trie(string[] words)
+				{
+					this.words = words;
+				}
+				public void Insert(string w, int index)
+				{
+					var curr = this.root;
+					foreach (var c in w)
+					{
+						TrieNode next = null;
+						if (!curr.children.TryGetValue(c, out next))
+						{
+							next = new TrieNode();
+							curr.children.Add(c, next);
+						}
+						curr = next;
+					}
+					curr.end = index;
+				}
+
+				public string Dfs()
+				{
+					var resp = string.Empty;
+					var s = new Stack<TrieNode>();
+					s.Push(root);
+
+					while (s.Count > 0)
+					{
+						var curr = s.Pop();
+						if (curr.end > 0 || curr == this.root)
+						{
+							if (curr != this.root)
+							{
+								var candidate = this.words[curr.end - 1];
+								if (candidate.Length > resp.Length || (candidate.Length == resp.Length && candidate.CompareTo(resp) < 0))
+								{
+									resp = candidate;
+								}
+							}
+
+							foreach (var c in curr.children.Values)
+							{
+								s.Push(c);
+							}
+						}
+					}
+					return resp;
+				}
+			}
+			public string LongestWord(string[] words)
+			{
+				var trie = new Trie(words);
+				var index = 0;
+				foreach (var w in words)
+				{
+					trie.Insert(w, ++index);
+				}
+				return trie.Dfs();
+			}
+		}
+	}
+}
---
+++ LeetCode720. Longest Word in Dictionary/LeetCode720. Longest Word in Dictionary/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LeetCode720. Longest Word in Dictionary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode720. Longest Word in Dictionary")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5c6c3fce-979e-4dcf-89e8-05e50aa4e340")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
