af1327f33dd6a94bfc1a34bc4693f7a2328e769e
robertoligeiro <robertoligeiro@gmail.com>
2018-05-29 09:57:48 -0700

    LeetCodeMockIs_Subsequence

---
+++ LeetCodeMockIs Subsequence/LeetCodeMockIs Subsequence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeMockIs_Subsequence
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		public class Solution
+		{
+			public bool IsSubsequence(string s, string t)
+			{
+				if (string.IsNullOrEmpty(s)) return true;
+				if (string.IsNullOrEmpty(t)) return false;
+				var count = 0;
+				foreach (var c in t)
+				{
+					if (c == s[count])
+					{
+						count++;
+						if (count == s.Length) return true;
+					}
+				}
+				return false;
+			}
+		}
+	}
+}
