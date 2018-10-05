b550520c6e46f4ffb2c6bc043a33faa07d1afa1b
robertoligeiro <robertoligeiro@gmail.com>
2018-10-05 10:03:54 -0700

    fix 776

--- LeetCode776. Split BST/LeetCode776. Split BST/Program.cs
+++ LeetCode776. Split BST/LeetCode776. Split BST/Program.cs
@@ -11,6 +11,9 @@ namespace LeetCode776.Split_BST
 	{
 		static void Main(string[] args)
 		{
+			var t = new TreeNode(4) { left = new TreeNode(2) { left = new TreeNode(1), right = new TreeNode(3) }, right = new TreeNode(6) };
+			var s = new Solution();
+			var r = s.SplitBST(t, 2);
 		}
 		public class TreeNode
 		{
@@ -24,18 +27,18 @@ namespace LeetCode776.Split_BST
 			public TreeNode[] SplitBST(TreeNode root, int V)
 			{
 				if (root == null) return new TreeNode[2];
-				if (root.val >= V) // go left
+				if (root.val > V) // go left
 				{
 					var resp = SplitBST(root.left, V);
-					root.left = resp[1];
-					resp[1] = root;
+					root.left = resp[0];
+					resp[0] = root;
 					return resp;
 				}
 				else // go right
 				{
 					var resp = SplitBST(root.right, V);
-					root.right = resp[0];
-					resp[0] = root;
+					root.right = resp[1];
+					resp[1] = root;
 					return resp;
 				}
 			}
