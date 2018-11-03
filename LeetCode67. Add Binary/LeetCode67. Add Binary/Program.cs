8e97a8444b63cd720569c3d510b6fdb739967109
robertoligeiro <robertoligeiro@gmail.com>
2018-11-02 21:45:05 -0700

    fix https://leetcode.com/problems/add-binary/

--- LeetCode67. Add Binary/LeetCode67. Add Binary/Program.cs
+++ LeetCode67. Add Binary/LeetCode67. Add Binary/Program.cs
@@ -20,6 +20,36 @@ namespace LeetCode67.Add_Binary
 			{
 				var indexA = a.Length - 1;
 				var indexB = b.Length - 1;
+				var resp = new StringBuilder();
+				var carry = 0;
+				while (indexA >= 0 || indexB >= 0)
+				{
+					var sum = carry;
+					if (indexA >= 0)
+					{
+						sum += a[indexA--] - '0';
+					}
+					if (indexB >= 0)
+					{
+						sum += b[indexB--] - '0';
+					}
+
+					var digit = sum % 2 == 0 ? '0' : '1';
+					resp.Insert(0, digit);
+					carry = sum >= 2 ? 1 : 0;
+				}
+				if (carry == 1) resp.Insert(0,'1');
+				
+				return resp.ToString();
+			}
+		}
+
+		public class Solution1
+		{
+			public string AddBinary(string a, string b)
+			{
+				var indexA = a.Length - 1;
+				var indexB = b.Length - 1;
 				var resp = new List<char>();
 				var carry = 0;
 				while (indexA >= 0 || indexB >= 0)
