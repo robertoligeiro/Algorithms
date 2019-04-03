13c7a5b064b89d38c4bda433cf286c67188d4f10
robertoligeiro <robertoligeiro@gmail.com>
2019-04-02 22:15:27 -0700

    int to string list using linq.

---
+++ IntArrayToStringListUsingLinq/IntArrayToStringListUsingLinq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntArrayToStringListUsingLinq
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var a = new int[] { 1, 2, 3, 4 };
+			var s = new List<string>(a.ToList().Select(x => x.ToString()));
+			var sjoin = string.Join("", s);
+		}
+	}
+}
